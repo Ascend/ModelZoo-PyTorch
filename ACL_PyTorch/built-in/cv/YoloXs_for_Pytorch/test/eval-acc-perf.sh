c92e99011e7c9711b8efb9ab6173219ac325ea32
pengaoao <pengaoao@huawei.com>
2022-05-17 18:55:37 +0800

    fix

--- ACL_PyTorch/built-in/cv/YoloXs_for_Pytorch/test/eval-acc-perf.sh
+++ ACL_PyTorch/built-in/cv/YoloXs_for_Pytorch/test/eval-acc-perf.sh
@@ -29,17 +29,14 @@ fi
 source env.sh
 rm -rf result/dumpOutput_device*
 
-./benchmark.${arch} -model_type=vision -device_id=0 -batch_size=1 -om_path=./models/yolox_x_bs1.om -input_text_path=./prep_bin.info -input_width=640 -input_height=640 -output_binary=True -useDvpp=False
+./benchmark.${arch} -model_type=vision -device_id=0 -batch_size=4 -om_path=./models/yolox.om -input_text_path=./prep_bin.info -input_width=640 -input_height=640 -output_binary=True -useDvpp=False
 if [ $? != 0 ]; then
     echo "fail!"
     exit -1
 fi
 
-mv result/dumpOutput_device0 result/dumpOutput_device0_bs1
 
-
-
-python Yolox_postprocess.py --dataroot ${datasets_path} --dump_dir 'result/dumpOutput_device0_bs1'
+python Yolox_postprocess.py --dataroot ${datasets_path} --dump_dir 'result/dumpOutput_device0'
 if [ $? != 0 ]; then
     echo "fail!"
     exit -1
@@ -47,7 +44,7 @@ fi
 
 
 echo "====performance data===="
-python test/parse.py result/perf_vision_batchsize_1_device_0.txt
+python test/parse.py result/perf_vision_batchsize_4_device_0.txt
 
 if [ $? != 0 ]; then
     echo "fail!"
--- ACL_PyTorch/built-in/cv/YoloXs_for_Pytorch/test/pth2om.sh
+++ ACL_PyTorch/built-in/cv/YoloXs_for_Pytorch/test/pth2om.sh
@@ -6,22 +6,23 @@ mkdir -p models
 rm -rf models/*.onnx
 cd YOLOX
 
-python tools/export_onnx.py --output-name yolox_x_bs1.onnx -n yolox-x -c ../yolox_x.pth
+python tools/export_onnx.py -c ../yolox_s.pth -f exps/default/yolox_s.py --dynamic
 
 if [ $? != 0 ]; then
     echo "fail!"
     exit -1
 fi
 
-mv yolox_x_bs1.onnx ../models
+mv yolox.onnx ../models
 
 cd ..
 rm -rf models/*.om
 source env.sh
 
-atc --model=yolox_x_fix.onnx --framework=5 --output=yolox_x_fix --input_format=NCHW --input_shape='images:1,3,640,640' --log=info --soc_version=Ascend310 --out_nodes="Conv_498:0;Conv_499:0;Conv_491:0;Conv_519:0;Conv_520:0;Conv_512:0;Conv_540:0;Conv_541:0;Conv_533:0"
+atc --model=yolox.onnx --framework=5 --output=yolox --input_format=NCHW --optypelist_for_implmode="Sigmoid" \
+ --op_select_implmode=high_performance --input_shape='images:4,3,640,640'  --log=info --soc_version=Ascend710
 
-if [ -f "models/yolox_x_bs1.om" ]; then
+if [ -f "models/yolox.om" ]; then
     echo "success"
 else
     echo "fail!"
