9a691147107548397daadf248f23258e550babf5
pengaoao <pengaoao@huawei.com>
2022-05-17 17:03:35 +0800

    fix3

--- ACL_PyTorch/built-in/cv/CRAFT_for_Pytorch/craft_atc.sh
+++ ACL_PyTorch/built-in/cv/CRAFT_for_Pytorch/craft_atc.sh
@@ -1,9 +1,3 @@
-export install_path=/usr/local/Ascend/ascend-toolkit/latest
-export PATH=/usr/local/python3.7.5/bin:$PATH
-export PATH=${install_path}/atc/ccec_compiler/bin:${install_path}/atc/bin:$PATH
-export ASCEND_OPP_PATH=${install_path}/opp
-export PYTHONPATH=${install_path}/atc/python/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=${install_path}/acllib/lib64:$LD_LIBRARY_PATH
-
+source /usr/local/Ascend/ascend-toolkit/set_env.sh 
 atc --model=craft.onnx --framework=5 --output=craft --input_format=NCHW \
 --input_shape="input:1,3,640,640" --log=error --soc_version=Ascend710
