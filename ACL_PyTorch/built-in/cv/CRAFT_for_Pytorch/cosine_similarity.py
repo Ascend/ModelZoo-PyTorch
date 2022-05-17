ae5a76bf14531211c31d1fe7a37840358c80882c
pengaoao <pengaoao@huawei.com>
2022-05-17 10:20:51 +0800

    add craft

---
+++ ACL_PyTorch/built-in/cv/CRAFT_for_Pytorch/cosine_similarity.py
@@ -0,0 +1,46 @@
+# Copyright 2020 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+from pyacl.acl_infer import AclNet, init_acl, release_acl
+import acl
+import numpy as np
+import onnxruntime
+
+def cos_sim(a, b):
+    a_norm = np.linalg.norm(a)
+    b_norm = np.linalg.norm(b)
+    cos = np.dot(a, b)/(a_norm * b_norm)
+    return cos
+
+if __name__ == '__main__':
+    DEVICE = 0
+    init_acl(DEVICE)
+    input = np.random.random((1, 3, 640, 640)).astype("float32")
+    net = AclNet(model_path="craft.om", device_id=DEVICE)
+    output_data, exe_time = net([input])
+    y = output_data[0].flatten()
+    feature = output_data[1].flatten()
+    onnx_model = onnxruntime.InferenceSession("craft.onnx")
+    inputs = {
+        onnx_model.get_inputs()[0].name: input,
+    }
+    onnx_output = onnx_model.run(None, inputs)
+    onnx_y = onnx_output[0].flatten()
+    onnx_feature = onnx_output[1].flatten()
+    cos_1 = cos_sim(y, onnx_y)
+    cos_2 = cos_sim(feature, onnx_feature)
+    print("cosine_1:", cos_1)
+    print("cosine_2:", cos_2)
+    del net
+    release_acl(DEVICE)
---
+++ ACL_PyTorch/built-in/cv/CRAFT_for_Pytorch/export_onnx.py
@@ -0,0 +1,41 @@
+"""  
+Copyright (c) 2019-present NAVER Corp.
+MIT License
+"""
+
+# -*- coding: utf-8 -*-
+import sys
+import argparse
+import torch
+from craft import CRAFT
+
+from collections import OrderedDict
+def copyStateDict(state_dict):
+    if list(state_dict.keys())[0].startswith("module"):
+        start_idx = 1
+    else:
+        start_idx = 0
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = ".".join(k.split(".")[start_idx:])
+        new_state_dict[name] = v
+    return new_state_dict
+
+def str2bool(v):
+    return v.lower() in ("yes", "y", "true", "t", "1")
+
+parser = argparse.ArgumentParser(description='CRAFT Text Detection')
+parser.add_argument('--trained_model', default='weights/craft_mlt_25k.pth', type=str, help='pretrained model')
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    # load net
+    net = CRAFT()     # initialize
+    print('Loading weights from checkpoint (' + args.trained_model + ')')
+    net.load_state_dict(copyStateDict(torch.load(args.trained_model, map_location='cpu')))
+    net.eval()
+    x = torch.randn(1, 3, 640, 640, requires_grad=False)
+    dynamic_axes = {'input': {0, '-1'}}
+    torch.onnx.export(net, (x), "craft.onnx", input_names=['input'], output_names=['y', 'feature'],
+                      opset_version=11, verbose=False)
