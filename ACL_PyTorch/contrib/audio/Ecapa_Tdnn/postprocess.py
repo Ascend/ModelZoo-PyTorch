5b17124896f8b7258f6891546c7f294a899e9a33
曾昊楠 <1114560730@qq.com>
2022-05-18 09:33:23 +0000

    update ACL_PyTorch/contrib/audio/Ecapa_Tdnn/postprocess.py.

--- ACL_PyTorch/contrib/audio/Ecapa_Tdnn/postprocess.py
+++ ACL_PyTorch/contrib/audio/Ecapa_Tdnn/postprocess.py
@@ -2,32 +2,38 @@ from collections import defaultdict
 
 import torch
 
-from ECAPA-TDNN.main import inference_embeddings_to_plt_hist_and_roc
+from ECAPA_TDNN.main import inference_embeddings_to_plt_hist_and_roc
 import sys
 from tqdm import tqdm
 
 if __name__ == "__main__":
     result_path = sys.argv[1]
     speakers_path = sys.argv[2]
+    batch_size = int(sys.argv[3])
+    total_nums = int(sys.argv[4])
+    result_nums = int(total_nums/batch_size)
+    print(result_nums)
     embedding_holder = defaultdict(list)
-    for i in tqdm(range(290)):
+    #样本总数为4648，bs为16,drop_last=True，所以循环4648/16且向下取整即290
+    for i in tqdm(range(result_nums)):
         index = i+1
         speakers = torch.load(speakers_path+ 'speakers'+str(index)+".pt")
-#        print(speakers)
+
         batch = []
         with open(result_path+ "mels"+str(index)+"_output_0.txt", 'r') as file:
             data_read = file.read()
             data_line = str(data_read).split('\n')
-#            print(data_line[0])
+
+
             for j in range(16):
                 data_list = []
                 num = data_line[j].split(' ')
-#                print(float(num[191]))
+
                 for k in range(192):
                     data_list.append(float(num[k]))
                 batch.append(data_list)
             h_tensor = torch.Tensor(batch)
-#        print(h_tensor)
+
         for h, s in zip(h_tensor.detach().cpu(), speakers):
             embedding_holder[s.item()].append(h.numpy())
     infer_hist, infer_roc, scores = inference_embeddings_to_plt_hist_and_roc(embedding_holder, 88600)
