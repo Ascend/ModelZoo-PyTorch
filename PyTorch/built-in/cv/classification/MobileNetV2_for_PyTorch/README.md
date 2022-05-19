44ef08d6a648740f4f3f66ac9bd6bd44e72c9cbb
白浪 <bailang12@h-partners.com>
2022-05-19 08:26:54 +0000

    !397 [自研][PyTorch]MobileNetV2_for_PyTorch模型训练启动脚本只留test下shell脚本，其余删除 * update PyTorch/built-in/cv/classification/MobileNetV2_for_PyTorch/README.md. * [自研][PyTorch]MobileNetV2_for_PyTorch模型训练启动脚本只留test下shell脚本，其余删除

--- PyTorch/built-in/cv/classification/MobileNetV2_for_PyTorch/README.md
+++ PyTorch/built-in/cv/classification/MobileNetV2_for_PyTorch/README.md
@@ -1,12 +1,12 @@
 ## 一、训练流程：
-    
+注意：data_path为数据集imagenet所在的路径 
+   
 ### 单卡训练流程：
 
 ```shell
 	1. 安装环境
-	2. 修改run_1p.sh字段"data"为当前磁盘的数据集路径
-	3. 修改字段device_id（单卡训练所使用的device id），为训练配置device_id，比如device_id=0
-	4. cd中run1p.sh文件的目录(train文件夹)，执行bash run1p.sh单卡脚本， 进行单卡训练
+	2. 开始训练
+	    bash test/train_full_1p.sh  --data_path=/data/imagenet
 ```
 
 	
@@ -14,9 +14,8 @@
 
 ```shell
 	1. 安装环境
-	2. 修改多P脚本中字段"data"为当前磁盘的数据集路径
-	3. 修改run8p.sh字段"addr"为当前主机ip地址
-	4. cd到run8p.sh文件的目录(train文件夹)，执行bash run8p.sh等多卡脚本， 进行多卡训练	
+	2. 开始训练
+	    bash test/train_full_8p.sh  --data_path=/data/imagenet
 ```
 
 
@@ -38,7 +37,7 @@
     
 - 训练日志路径：在训练脚本的同目录下result文件夹里，如：
 
-        /home/MobileNet/result/training_8p_job_20201121023601
+        /home/MobileNet/test/output/
         
 	
 
--- PyTorch/built-in/cv/classification/MobileNetV2_for_PyTorch/test/train_full_1p.sh
+++ PyTorch/built-in/cv/classification/MobileNetV2_for_PyTorch/test/train_full_1p.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-
-#当前路径,不需要修改
+###############指定训练脚本执行路径###############
+# cd到与test文件夹同层级目录下执行脚本，提高兼容性；test_path_dir为包含test文件夹的路径
 cur_path=`pwd`
+cur_path_last_dirname=${cur_path##*/}
+if [ x"${cur_path_last_dirname}" == x"test" ];then
+    test_path_dir=${cur_path}
+    cd ..
+    cur_path=`pwd`
+else
+    test_path_dir=${cur_path}/test
+fi
+
 
 
 #集合通信参数,不需要修改
 export HCCL_WHITELIST_DISABLE=1
-export RANK_SIZE=8
 export JOB_ID=10087
 RANK_ID_START=0
-# source env.sh
-RANK_SIZE=8
+
 # 数据集路径,保持为空,不需要修改
 data_path=""
 
@@ -21,14 +28,13 @@ data_path=""
 #网络名称，同目录名称
 Network="DeepMar_for_PyTorch"
 #训练epoch
-train_epochs=1
+train_epochs=600
 #训练batch_size
 batch_size=512
 #训练step
 train_steps=`expr 1281167 / ${batch_size}`
 #学习率
 learning_rate=0.045
-
 
 
 #维测参数，precision_mode需要模型审视修改
@@ -68,26 +74,35 @@ if [[ $data_path == "" ]];then
     echo "[Error] para \"data_path\" must be confing"
     exit 1
 fi
- 
-cd $cur_path
 
+#非平台场景时source 环境变量
+check_etp_flag=`env | grep etp_running_flag`
+etp_flag=`echo ${check_etp_flag#*=}`
+if [ x"${etp_flag}" != x"true" ];then
+    source  ${test_path_dir}/env_npu.sh
+fi
+
+
+cd $cur_path
+ASCEND_DEVICE_ID=0
 #设置环境变量，不需要修改
 echo "Device ID: $ASCEND_DEVICE_ID"
 export RANK_ID=$RANK_ID
 
-if [ -d $cur_path/output ];then
-   rm -rf $cur_path/output/*
-   mkdir -p $cur_path/output/$ASCEND_DEVICE_ID
+if [ -d $test_path_dir/output ];then
+   rm -rf $test_path_dir/output/*
+   mkdir -p $test_path_dir/output/$ASCEND_DEVICE_ID
 else
-   mkdir -p $cur_path/output/$ASCEND_DEVICE_ID
+   mkdir -p $test_path_dir/output/$ASCEND_DEVICE_ID
 fi
 wait
+
 
 #训练开始时间，不需要修改
 start_time=$(date +%s)
 
 # 绑核，不需要的绑核的模型删除，需要模型审视修改
-python3.7 ${cur_path}/../train/mobilenetv2_8p_main_anycard.py \
+nohup python3.7 ${cur_path}/train/mobilenetv2_8p_main_anycard.py \
     --addr=$(hostname -I |awk '{print $1}') \
     --seed 49  \
     --workers 128 \
@@ -102,10 +117,10 @@ python3.7 ${cur_path}/../train/mobilenetv2_8p_main_anycard.py \
     --batch-size 512 \
     --epochs $train_epochs \
     --rank 0 \
-    --device-list '$ASCEND_DEVICE_ID' \
+    --device-list $ASCEND_DEVICE_ID \
     --amp \
     --benchmark 0 \
-    --data $data_path > $cur_path/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log 2>&1 &
+    --data $data_path > $test_path_dir/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log 2>&1 &
 wait
 
 #训练结束时间，不需要修改
@@ -116,13 +131,12 @@ e2e_time=$(( $end_time - $start_time ))
 echo "------------------ Final result ------------------"
 #输出性能FPS，需要模型审视修改
 #FPS=`grep -a 'FPS'  $cur_path/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F " " '{print $NF}'|awk 'END {print}'`
-FPS=`grep Epoch: ${cur_path}/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log|grep -v Test|awk -F "FPS" '{print $2}'|awk -F " " '{print $1}' | tail -n +2|awk '{sum+=$1} END {print sum/NR}' | sed s/[[]:space:]//g `
-
+FPS=`grep FPS ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log|awk '{print $NF}'|awk '{sum+=$1} END {print  sum/NR}'`
 #打印，不需要修改
 echo "Final Performance images/sec : $FPS"
 
 #输出训练精度,需要模型审视修改
-train_accuracy=`grep -a '* Acc@1' $cur_path/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk 'END {print}'|awk -F "Acc@1" '{print $NF}'|awk -F " " '{print $1}'`
+train_accuracy=`grep -a '* Acc@1' $test_path_dir/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk 'END {print}'|awk -F "Acc@1" '{print $NF}'|awk -F " " '{print $1}'`
 
 #打印，不需要修改
 echo "Final Train Accuracy : ${train_accuracy}"
@@ -141,18 +155,18 @@ ActualFPS=${FPS}
 TrainingTime=`awk 'BEGIN{printf "%.2f\n", '${batch_size}'*1000/'${FPS}'}'`
 
 #从train_$ASCEND_DEVICE_ID.log提取Loss到train_${CaseName}_loss.txt中，需要根据模型审视
-grep Epoch: $cur_path/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log|grep -v Test|awk -F "Loss" '{print $NF}' | awk -F " " '{print $1}' > $cur_path/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt
+grep Epoch: $test_path_dir/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log|grep -v Test|awk -F "Loss" '{print $NF}' | awk -F " " '{print $1}' > $test_path_dir/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt
 
 #最后一个迭代loss值，不需要修改
-ActualLoss=`awk 'END {print}' $cur_path/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt`
+ActualLoss=`awk 'END {print}' $test_path_dir/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt`
 
 #关键信息打印到${CaseName}.log中，不需要修改
-echo "Network = ${Network}" > $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "RankSize = ${RANK_SIZE}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "BatchSize = ${BatchSize}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "DeviceType = ${DeviceType}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "CaseName = ${CaseName}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "ActualFPS = ${ActualFPS}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "TrainingTime = ${TrainingTime}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "ActualLoss = ${ActualLoss}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
-echo "E2ETrainingTime = ${e2e_time}" >> $cur_path/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "Network = ${Network}" > $test_path_dir/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RankSize = ${RANK_SIZE}" >> $test_path_dir/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "BatchSize = ${BatchSize}" >> $test_path_dir/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "DeviceType = ${DeviceType}" >> $test_path_dir/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "CaseName = ${CaseName}" >> $test_path_dir/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualFPS = ${ActualFPS}" >> $test_path_dir/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "TrainingTime = ${TrainingTime}" >> $test_path_dir/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualLoss = ${ActualLoss}" >> $test_path_dir/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "E2ETrainingTime = ${e2e_time}" >> $test_path_dir/output/$ASCEND_DEVICE_ID/${CaseName}.log
