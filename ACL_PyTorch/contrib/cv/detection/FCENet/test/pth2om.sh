5c9786f5fb2a291e9224593078d2810bcb9da96d
张艺凡 <2803289563@qq.com>
2022-05-22 05:48:08 +0000

    update ACL_PyTorch/contrib/cv/detection/FCENet/test/pth2om.sh.

--- ACL_PyTorch/contrib/cv/detection/FCENet/test/pth2om.sh
+++ ACL_PyTorch/contrib/cv/detection/FCENet/test/pth2om.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-export profile_path=/home/zhangyifan/mmocr
 
 for para in $*
 do
@@ -8,11 +7,11 @@ do
     fi
 done
 
-python ${profile_path}/tools/deployment/pytorch2onnx.py \
-${profile_path}/configs/textdet/fcenet/fcenet_r50_fpn_1500e_icdar2015.py \
-${profile_path}/fcenet_r50_fpn_1500e_icdar2015_20211022-daefb6ed.pth \
+python ./tools/deployment/pytorch2onnx.py \
+./configs/textdet/fcenet/fcenet_r50_fpn_1500e_icdar2015.py \
+./fcenet_r50_fpn_1500e_icdar2015_20211022-daefb6ed.pth \
 det \
-${profile_path}/data/icdar2015/imgs/test/img_1.jpg \
+./data/icdar2015/imgs/test/img_1.jpg \
 --dynamic-export \
 --output-file ./fcenet.onnx
 
@@ -25,7 +24,7 @@ rm -f fcenet.om
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 
 atc --framework=5 --model=./fcenet.onnx --output=./fcenet  --input_format=NCHW \
---input_shape="input:$batch_size,3,1280,2272" --log=error --soc_version=Ascend310
+--input_shape="input:$batch_size,3,1280,2272" --log=error --soc_version=Ascend710
 
 if [ -f "fcenet.om" ]; then
     echo "success"
