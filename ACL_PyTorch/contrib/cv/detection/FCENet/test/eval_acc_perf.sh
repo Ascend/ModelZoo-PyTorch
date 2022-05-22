af24de7256e56eba142c14ae7eb1e2b4c365380d
张艺凡 <2803289563@qq.com>
2022-05-22 13:07:05 +0000

    update ACL_PyTorch/contrib/cv/detection/FCENet/test/eval_acc_perf.sh.

--- ACL_PyTorch/contrib/cv/detection/FCENet/test/eval_acc_perf.sh
+++ ACL_PyTorch/contrib/cv/detection/FCENet/test/eval_acc_perf.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
-#export profile_path=/home/zhangyifan/mmocr
-export msame_path=/home/zhangyifan/tools/msame/out
 
 rm -rf preprocessed_imgs
 mkdir preprocessed_imgs
-
-python ./mmocr/utils/fcenet_preprocess.py ./data/icdar2015/imgs/test/
+echo "start preprocess..."
+python fcenet_preprocess.py ./mmocr/data/icdar2015/imgs/test/
 
 python ./gen_dataset_info.py \
 bin \
@@ -14,18 +12,18 @@ fcenet_prep_bin.info \
 1280 2272
 
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
-
+echo "start inference..."
 rm -rf result
-${msame_path}/msame \
+./tools/msame/out/msame \
 --model fcenet.om \
 --input preprocessed_imgs \
 --output result \
 --outfmt TXT
-
+echo "start postprocess..."
 rm boundary_results.txt
-python ./mmocr/models/textdet/postprocess/fcenet_postprocess.py
-
-python ./tools/eval.py \
-./configs/textdet/fcenet/fcenet_r50_fpn_1500e_icdar2015.py \
-./fcenet_r50_fpn_1500e_icdar2015_20211022-daefb6ed.pth \
+python fcenet_postprocess.py
+echo "start evaluate metric..."
+python eval.py \
+./mmocr/configs/textdet/fcenet/fcenet_r50_fpn_1500e_icdar2015.py \
+fcenet_r50_fpn_1500e_icdar2015_20211022-daefb6ed.pth \
 --eval hmean-iou
