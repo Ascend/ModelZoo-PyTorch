0132548f58b2f1821f3bff4c568023c8a93779d6
曾昊楠 <1114560730@qq.com>
2022-05-23 17:46:33 +0000

    update ACL_PyTorch/contrib/audio/Ecapa_Tdnn/to_om.sh.

--- ACL_PyTorch/contrib/audio/Ecapa_Tdnn/to_om.sh
+++ ACL_PyTorch/contrib/audio/Ecapa_Tdnn/to_om.sh
@@ -1,12 +1,13 @@
-export install_path=/usr/local/Ascend/ascend-toolkit/latest
-export PATH=/usr/local/python3.7.5/bin:${install_path}/atc/ccec_compiler/bin:${install_path}/atc/bin:$PATH
-export PYTHONPATH=${install_path}/atc/python/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=${install_path}/atc/lib64:${install_path}/acllib/lib64:$LD_LIBRARY_PATH
-export ASCEND_OPP_PATH=${install_path}/opp
-export ASCEND_AICPU_PATH=/usr/local/Ascend/ascend-toolkit/latest
+export ASCEND_DEVICE_ID=0
+export TUNE_BANK_PATH=./aoe_result_bs4
+export TE_PARALLEL_COMPILER=8
+export REPEAT_TUNE=False
 
 #export ASCEND_SLOG_PRINT_TO_STDOUT=1
 #export ASCEND_GLOBAL_LOG_LEVEL=0
 
+mkdir om
 
-atc --framework=5 --model=ecapa_tdnn_sim.onnx --output=om/ecapa_tdnn_bs1 --input_format=ND --input_shape="mel:1,80,200" --log=debug  --soc_version=Ascend710
+chmod 777 ./aoe_result_bs4
+
+atc --framework=5 --model=ecapa_tdnn_sim.onnx --output=./om/ecapa_tdnn_bs4 --input_format=ND --input_shape="mel:4,80,200" --log=debug  --soc_version=Ascend710
