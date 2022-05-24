740c3c9e4e2503f9758c51b3a144b9b00c2849c3
张艺凡 <2803289563@qq.com>
2022-05-24 06:55:47 +0000

    update ACL_PyTorch/contrib/cv/detection/FCENet/test/pth2om.sh.

--- ACL_PyTorch/contrib/cv/detection/FCENet/test/pth2om.sh
+++ ACL_PyTorch/contrib/cv/detection/FCENet/test/pth2om.sh
@@ -7,13 +7,17 @@ do
     fi
 done
 
-python ./pytorch2onnx.py \
-./mmocr/configs/textdet/fcenet/fcenet_r50_fpn_1500e_icdar2015.py \
-./fcenet_r50_fpn_1500e_icdar2015_20211022-daefb6ed.pth \
-det \
-./mmocr/data/icdar2015/imgs/test/img_1.jpg \
---dynamic-export \
---output-file ./fcenet.onnx
+if [ -f "fcenet_dynamicbs.onnx" ]; then
+    echo "onnx has existed!"
+else
+    python ./pytorch2onnx.py \
+    ./mmocr/configs/textdet/fcenet/fcenet_r50_fpn_1500e_icdar2015.py \
+    ./fcenet_r50_fpn_1500e_icdar2015_20211022-daefb6ed.pth \
+    det \
+    ./mmocr/data/icdar2015/imgs/test/img_1.jpg \
+    --dynamic-export \
+    --output-file ./fcenet_dynamicbs.onnx
+fi
 
 if [ $? != 0 ]; then
     echo "fail!"
@@ -23,10 +27,10 @@ fi
 rm -f fcenet.om
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 
-atc --framework=5 --model=./fcenet.onnx --output=./fcenet  --input_format=NCHW \
+atc --framework=5 --model=./fcenet_dynamicbs.onnx --output=./fcenet_bs$batch_size  --input_format=NCHW \
 --input_shape="input:$batch_size,3,1280,2272" --log=error --soc_version=Ascend710
 
-if [ -f "fcenet.om" ]; then
+if [ -f "fcenet_bs$batch_size.om" ]; then
     echo "success"
 else
     echo "fail!"
