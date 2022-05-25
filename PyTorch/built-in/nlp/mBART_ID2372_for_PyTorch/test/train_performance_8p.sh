dac1fb3f88a3d9bd91e6728090739b3263b30a6b
董雪莹 <dongxueying6@h-partners.com>
2022-05-25 08:30:56 +0000

    !509 [自研][PyTorch]mBART_ID2372_for_PyTorch模型，8p性能脚本精度过滤方式修改 !509 [自研][PyTorch]mBART_ID2372_for_PyTorch模型，8p性能脚本精度过滤方式修改

--- PyTorch/built-in/nlp/mBART_ID2372_for_PyTorch/test/train_performance_8p.sh
+++ PyTorch/built-in/nlp/mBART_ID2372_for_PyTorch/test/train_performance_8p.sh
@@ -149,9 +149,12 @@ echo "------------------ Final result ------------------"
 #输出性能WPS，需要模型审视修改
 WPS=`grep 'train_inner ' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "wps=" '{print $NF}'|awk -F "wps" '{print $1}'|awk -F "," '{print $1}'|awk 'END {print}'`
 train_wall=`grep 'train_inner ' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "train_wall=" '{print $NF}'|awk 'NR==1{min=$1;next}{min=min<$1?min:$1}END{print min}'|awk -F "," '{print$1}'`
+TRAIN_WALL=`grep 'train_inner ' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "train_wall=" '{print $NF}'|awk -F "," '{print $1}'|tail -n  20|awk '{sum+=$1} END {print"",sum/NR}'|sed s/[[:space:]]//g`
+
 #打印，不需要修改
 echo "Final Performance images/sec : $WPS"
 echo "E2E Training Duration sec : $e2e_time"
+echo "train_wall : $TRAIN_WALL"
 
 #性能看护结果汇总
 #训练用例信息，不需要修改
