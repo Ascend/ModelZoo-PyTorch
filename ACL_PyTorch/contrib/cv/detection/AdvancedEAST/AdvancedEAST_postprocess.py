e45d5d959aebcd18b810a7955fc6a6dd756ebfdc
zhazhafei <506806089@qq.com>
2022-06-01 16:41:27 +0800

    模型迁移 710修改

--- ACL_PyTorch/contrib/cv/detection/AdvancedEAST/AdvancedEAST_postprocess.py
+++ ACL_PyTorch/contrib/cv/detection/AdvancedEAST/AdvancedEAST_postprocess.py
@@ -17,11 +17,12 @@ import os
 import multiprocessing
 import torch
 import numpy as np
+import time
+from tqdm import tqdm
 
 sys.path.append('./AdvancedEAST-PyTorch')
 
 from utils import eval_pre_rec_f1
-
 
 def eval_func(i, out, gt_xy_list):
     eval_p_r_f = eval_pre_rec_f1()
@@ -36,14 +37,19 @@ def eval(data_dir, result_dir):
     img_list = os.listdir(result_dir)
     thread_pool = multiprocessing.Pool(multiprocessing.cpu_count())
     i = 0
+    scale = len(img_list)
+    start = time.perf_counter()
+    pbar = tqdm(total=len(img_list))
+    update = lambda *args: pbar.update()
     for img_file in img_list:
         gt_xy_list = [np.load(os.path.join(train_label_dir, img_file[:-6] + '.npy'))]
         out = np.fromfile(os.path.join(result_dir, img_file), dtype=np.float32)
         size = 736 // 4
         out.shape = (1, 7, size, size)
         out = torch.from_numpy(out)
-        thread_pool.apply_async(eval_func, args=(i, out, gt_xy_list))
+        thread_pool.apply_async(eval_func, args=(i, out, gt_xy_list), callback=update)
         i += 1
+        
     thread_pool.close()
     thread_pool.join()
 
