1c02d0fcdc99232839bc398c4e96ca694cada4f9
吴仪盈 <850763196@qq.com>
2022-06-07 04:06:05 +0000

    测试t4性能和生成onnx、om模型的sh文件

---
+++ ACL_PyTorch/contrib/cv/pose_estimation/PoseC3D/perf_t4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+rm -rf perf_bs1.log
+trtexec --onnx=posec3d.onnx --fp16 --threads > perf_bs1.log
+perf_str=`grep "GPU.* mean.*ms$" perf_bs1.log`
+if [ -n "$perf_str" ]; then
+    perf_num=`echo $perf_str | awk -F' ' '{print $16}'`
+else
+    perf_str=`grep "mean.*ms$" perf_bs1.log`
+    perf_num=`echo $perf_str | awk -F' ' '{print $4}'`
+fi
+awk 'BEGIN{printf "t4 bs1 fps:%.3f\n", 1000*1/('$perf_num'/1)}'
---
+++ ACL_PyTorch/contrib/cv/pose_estimation/PoseC3D/pth2om.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+
+batch_size=1
+not_skip_onnx=true
+
+for para in $*
+do
+    if [[ $para == --batch_size* ]]; then
+        batch_size=`echo ${para#*=}`
+    fi
+    if [[ $para == --not_skip_onnx* ]]; then
+        not_skip_onnx=`echo ${para#*=}`
+    fi
+done
+
+# ======================= convert onnx =======================================
+if [ $not_skip_onnx == true ]; then
+    rm -rf posec3d.onnx
+    python ./posec3d_pytorch2onnx.py  \
+        ./mmaction2/configs/skeleton/posec3d/slowonly_kinetics400_pretrained_r50_u48_120e_hmdb51_split1_keypoint.py \
+        ./slowonly_kinetics400_pretrained_r50_u48_120e_hmdb51_split1_keypoint-76ffdd8b.pth \
+        --shape 1 20 17 48 56 56 \
+        --verify \
+        --output-file ./posec3d.onnx
+    if [ -f "posec3d.onnx" ]; then
+      echo "==> 1. creating onnx model successfully."
+    else
+      echo "onnx export failed"
+      exit -1
+    fi
+fi
+
+
+# ======================= convert om =========================================
+rm -rf posec3d_bs${batch_size}.om
+export TUNE_BANK_PATH="./aoe_result_bs1"
+atc --framework=5 --model=./posec3d.onnx \
+    --output=./posec3d_bs${batch_size} \
+    --input_format=ND --input_shape="invals:${batch_size},20,17,48,56,56" \
+    --log=debug --soc_version=Ascend710
+if [ -f "posec3d_bs${batch_size}.om" ] ; then
+  echo "==> 2. creating om model successfully."
+else
+  echo "sim_om export failed"
+fi
+echo "==> 3. Done."
