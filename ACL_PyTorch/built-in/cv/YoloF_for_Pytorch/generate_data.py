7b9f98e4cb968bd075d4205da50495aed986aa7a
pengaoao <pengaoao@huawei.com>
2022-06-08 12:07:36 +0800

    fix

--- ACL_PyTorch/built-in/cv/YoloF_for_Pytorch/generate_data.py
+++ ACL_PyTorch/built-in/cv/YoloF_for_Pytorch/generate_data.py
@@ -18,14 +18,22 @@ import mmcv
 import numpy as np
 import argparse
 
+dataset_config = {
+    'resize': (640, 640),
+    'mean': [103.530, 116.280, 123.675],
+    'std': [1, 1, 1],
+}
 def preprocess(img_src_path, save_path, batch_size):
     image_list = ["000000455085.jpg", "000000570756.jpg", "000000446651.jpg", "000000372317.jpg",
                   "000000491216.jpg", "000000524742.jpg", "000000465585.jpg", "000000015751.jpg",
                   "000000511384.jpg", "000000434297.jpg", "000000219283.jpg", "000000473869.jpg",
                   "000000231822.jpg", "000000292225.jpg", "000000142971.jpg", "000000113720.jpg"]
     for i, file in enumerate(image_list):
-        image = mmcv.imread(os.path.join(img_src_path, file))
+        image = mmcv.imread(os.path.join(img_src_path, file), backend='cv2')
         image, scalar = mmcv.imrescale(image, (args.model_input_height, args.model_input_width), return_scale=True)
+        mean = np.array(dataset_config['mean'], dtype=np.float32)
+        std = np.array(dataset_config['std'], dtype=np.float32)
+        image = mmcv.imnormalize(image, mean, std, to_rgb=False)
         image = mmcv.impad(image, shape=(args.model_input_height, args.model_input_width),
                            pad_val=(args.model_pad_val, args.model_pad_val, args.model_pad_val))
         image = image.transpose(2, 0, 1)
