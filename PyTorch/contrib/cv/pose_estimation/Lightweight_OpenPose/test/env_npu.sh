e1be4f1214473a0de2987abbc5ff1dbe9e4c9aad
冯王磊 <2398672897@qq.com>
2022-06-09 01:55:34 +0000

    !436 [浙江工业大学][高校贡献][Pytorch][Lightweight_OpenPose]--pt-1.8初次提交 !436 [浙江工业大学][高校贡献][Pytorch][Lightweight_OpenPose]--pt-1.8初次提交

--- PyTorch/contrib/cv/pose_estimation/Lightweight_OpenPose/test/env_npu.sh
+++ PyTorch/contrib/cv/pose_estimation/Lightweight_OpenPose/test/env_npu.sh
@@ -2,21 +2,21 @@
 export install_path=/usr/local/Ascend
 
 if [ -d ${install_path}/toolkit ]; then
-    export LD_LIBRARY_PATH=/usr/include/hdf5/lib/:/usr/local/:/usr/local/lib/:/usr/lib/:${install_path}/fwkacllib/lib64/:${install_path}/driver/lib64/common/:${install_path}/driver/lib64/driver/:${install_path}/add-ons:${path_lib}:${LD_LIBRARY_PATH}
+    export LD_LIBRARY_PATH=${install_path}/fwkacllib/lib64/:/usr/include/hdf5/lib/:/usr/local/:/usr/local/lib/:/usr/lib/:${install_path}/driver/lib64/common/:${install_path}/driver/lib64/driver/:${install_path}/add-ons:${path_lib}:${LD_LIBRARY_PATH}
     export PATH=${install_path}/fwkacllib/ccec_compiler/bin:${install_path}/fwkacllib/bin:$PATH
     export PYTHONPATH=${install_path}/fwkacllib/python/site-packages:${install_path}/tfplugin/python/site-packages:${install_path}/toolkit/python/site-packages:$PYTHONPATH
     export PYTHONPATH=/usr/local/python3.7.5/lib/python3.7/site-packages:$PYTHONPATH
     export ASCEND_OPP_PATH=${install_path}/opp
 else
     if [ -d ${install_path}/nnae/latest ];then
-        export LD_LIBRARY_PATH=/usr/local/:/usr/local/python3.7.5/lib/:/usr/local/openblas/lib:/usr/local/lib/:/usr/lib64/:/usr/lib/:${install_path}/nnae/latest/fwkacllib/lib64/:${install_path}/driver/lib64/common/:${install_path}/driver/lib64/driver/:${install_path}/add-ons/:/usr/lib/aarch64_64-linux-gnu:$LD_LIBRARY_PATH
+        export LD_LIBRARY_PATH=${install_path}/nnae/latest/fwkacllib/lib64/:/usr/local/:/usr/local/python3.7.5/lib/:/usr/local/openblas/lib:/usr/local/lib/:/usr/lib64/:/usr/lib/:${install_path}/driver/lib64/common/:${install_path}/driver/lib64/driver/:${install_path}/add-ons/:/usr/lib/aarch64_64-linux-gnu:$LD_LIBRARY_PATH
         export PATH=$PATH:${install_path}/nnae/latest/fwkacllib/ccec_compiler/bin/:${install_path}/nnae/latest/toolkit/tools/ide_daemon/bin/
         export ASCEND_OPP_PATH=${install_path}/nnae/latest/opp/
         export OPTION_EXEC_EXTERN_PLUGIN_PATH=${install_path}/nnae/latest/fwkacllib/lib64/plugin/opskernel/libfe.so:${install_path}/nnae/latest/fwkacllib/lib64/plugin/opskernel/libaicpu_engine.so:${install_path}/nnae/latest/fwkacllib/lib64/plugin/opskernel/libge_local_engine.so
         export PYTHONPATH=${install_path}/nnae/latest/fwkacllib/python/site-packages/:${install_path}/nnae/latest/fwkacllib/python/site-packages/auto_tune.egg/auto_tune:${install_path}/nnae/latest/fwkacllib/python/site-packages/schedule_search.egg:$PYTHONPATH
         export ASCEND_AICPU_PATH=${install_path}/nnae/latest
     else
-        export LD_LIBRARY_PATH=/usr/local/:/usr/local/lib/:/usr/lib64/:/usr/lib/:/usr/local/python3.7.5/lib/:/usr/local/openblas/lib:${install_path}/ascend-toolkit/latest/fwkacllib/lib64/:${install_path}/driver/lib64/common/:${install_path}/driver/lib64/driver/:${install_path}/add-ons/:/usr/lib/aarch64-linux-gnu:$LD_LIBRARY_PATH
+        export LD_LIBRARY_PATH=${install_path}/ascend-toolkit/latest/fwkacllib/lib64/:/usr/local/:/usr/local/lib/:/usr/lib64/:/usr/lib/:/usr/local/python3.7.5/lib/:/usr/local/openblas/lib:${install_path}/driver/lib64/common/:${install_path}/driver/lib64/driver/:${install_path}/add-ons/:/usr/lib/aarch64-linux-gnu:$LD_LIBRARY_PATH
         export PATH=$PATH:${install_path}/ascend-toolkit/latest/fwkacllib/ccec_compiler/bin/:${install_path}/ascend-toolkit/latest/toolkit/tools/ide_daemon/bin/
         export ASCEND_OPP_PATH=${install_path}/ascend-toolkit/latest/opp/
         export OPTION_EXEC_EXTERN_PLUGIN_PATH=${install_path}/ascend-toolkit/latest/fwkacllib/lib64/plugin/opskernel/libfe.so:${install_path}/ascend-toolkit/latest/fwkacllib/lib64/plugin/opskernel/libaicpu_engine.so:${install_path}/ascend-toolkit/latest/fwkacllib/lib64/plugin/opskernel/libge_local_engine.so
@@ -25,24 +25,33 @@ else
     fi
 fi
 
+${install_path}/driver/tools/msnpureport -g error -d 0
+${install_path}/driver/tools/msnpureport -g error -d 1
+${install_path}/driver/tools/msnpureport -g error -d 2
+${install_path}/driver/tools/msnpureport -g error -d 3
+${install_path}/driver/tools/msnpureport -g error -d 4
+${install_path}/driver/tools/msnpureport -g error -d 5
+${install_path}/driver/tools/msnpureport -g error -d 6
+${install_path}/driver/tools/msnpureport -g error -d 7
 
-#å°†Hostæ—¥å¿—è¾“å‡ºåˆ°ä¸²å£,0-å…³é—­/1-å¼€å¯
+#½«HostÈÕÖ¾Êä³öµ½´®¿Ú,0-¹Ø±Õ/1-¿ªÆô
 export ASCEND_SLOG_PRINT_TO_STDOUT=0
-#è®¾ç½®é»˜è®¤æ—¥å¿—çº§åˆ«,0-debug/1-info/2-warning/3-error
-export ASCEND_GLOBAL_LOG_LEVEL=3
-#è®¾ç½®Eventæ—¥å¿—å¼€å¯æ ‡å¿—,0-å…³é—­/1-å¼€å¯
+#ÉèÖÃÄ¬ÈÏÈÕÖ¾¼¶±ğ,0-debug/1-info/2-warning/3-error
+export ASCEND_GLOBAL_LOG_LEVEL==3
+#ÉèÖÃEventÈÕÖ¾¿ªÆô±êÖ¾,0-¹Ø±Õ/1-¿ªÆô
 export ASCEND_GLOBAL_EVENT_ENABLE=0
-#è®¾ç½®æ˜¯å¦å¼€å¯taskque,0-å…³é—­/1-å¼€å¯
+#ÉèÖÃÊÇ·ñ¿ªÆôtaskque,0-¹Ø±Õ/1-¿ªÆô
 export TASK_QUEUE_ENABLE=1
-#è®¾ç½®æ˜¯å¦å¼€å¯PTCopy,0-å…³é—­/1-å¼€å¯
+#ÉèÖÃÊÇ·ñ¿ªÆôPTCopy,0-¹Ø±Õ/1-¿ªÆô
 export PTCOPY_ENABLE=1
-#è®¾ç½®æ˜¯å¦å¼€å¯combinedæ ‡å¿—,0-å…³é—­/1-å¼€å¯
-export COMBINED_ENABLE=1
-#è®¾ç½®ç‰¹æ®Šåœºæ™¯æ˜¯å¦éœ€è¦é‡æ–°ç¼–è¯‘,ä¸éœ€è¦ä¿®æ”¹
+#ÉèÖÃÊÇ·ñ¿ªÆô2¸ö·ÇÁ¬Ğøcombined±êÖ¾,0-¹Ø±Õ/1-¿ªÆô
+export COMBINED_ENABLE=0
+#ÉèÖÃÌØÊâ³¡¾°ÊÇ·ñĞèÒªÖØĞÂ±àÒë,²»ĞèÒªĞŞ¸Ä
 export DYNAMIC_OP="ADD#MUL"
-#HCCLç™½åå•å¼€å…³,1-å…³é—­/0-å¼€å¯
+# HCCL°×Ãûµ¥¿ª¹Ø,1-¹Ø±Õ/0-¿ªÆô
 export HCCL_WHITELIST_DISABLE=1
-export HCCL_IF_IP=$(hostname -I |awk '{print $1}')
+# HCCLÄ¬ÈÏ³¬Ê±Ê±¼ä120s½ÏÉÙ£¬ĞŞ¸ÄÎª1800s¶ÔÆëPyTorchÄ¬ÈÏÉèÖÃ
+export HCCL_CONNECT_TIMEOUT=1800
 
 ulimit -SHn 512000
 
