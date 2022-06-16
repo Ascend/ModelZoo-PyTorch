fd736a17cb3f46721709aa2bfcdbbb0bb08425b0
初心 <1280190261@qq.com>
2022-06-16 02:41:01 +0000

    update ACL_PyTorch/contrib/cv/detection/Retinanet/test/pth2om.sh.

--- ACL_PyTorch/contrib/cv/detection/Retinanet/test/pth2om.sh
+++ ACL_PyTorch/contrib/cv/detection/Retinanet/test/pth2om.sh
@@ -3,7 +3,7 @@ python3.7 ./detectron2/tools/deploy/export_model.py --config-file ./detectron2/c
 mv ./output/model.onnx model_py1.8.onnx
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 rm -rf retinanet_detectron2_npu.om 
-atc --model=model_py1.8.onnx --framework=5 --output=retinanet_detectron2_npu --input_format=NCHW --input_shape="input0:1,3,1344,1344"  --log=debug --soc_version=Ascend310
+atc --model=model_py1.8.onnx --framework=5 --output=retinanet_detectron2_npu --input_format=NCHW --input_shape="input0:1,3,1344,1344" --out_nodes="Cast_1229:0;Reshape_1223:0;Gather_1231:0" --log=debug --soc_version=Ascend310
 if [ -f "retinanet_detectron2_npu.om" ] ; then
     echo "success"
 else
