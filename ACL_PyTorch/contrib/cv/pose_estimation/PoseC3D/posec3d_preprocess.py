157c84e523a444ed5c90a935e20ecfd19c948fd7
吴仪盈 <850763196@qq.com>
2022-06-17 19:59:44 +0000

    update ACL_PyTorch/contrib/cv/pose_estimation/PoseC3D/posec3d_preprocess.py.

--- ACL_PyTorch/contrib/cv/pose_estimation/PoseC3D/posec3d_preprocess.py
+++ ACL_PyTorch/contrib/cv/pose_estimation/PoseC3D/posec3d_preprocess.py
@@ -1,23 +1,37 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 # Copyright 2021 Huawei Technologies Co., Ltd
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # ============================================================================
 
 import os
 import torch
 import numpy as np
 import argparse
+from tqdm import tqdm
 from mmcv import Config
 import torch.nn.functional as F
 from mmaction.datasets import build_dataloader, build_dataset
@@ -32,7 +46,7 @@ def parse_args():
     parser.add_argument('--num_worker', default=8, type=int, help='Number of workers for inference')
     parser.add_argument('--data_root', default='/opt/npu/hmdb51/rawframes/', type=str)
     parser.add_argument('--ann_file', default='/opt/npu/hmdb51/hmdb51.pkl', type=str)
-    parser.add_argument('--name', default='prep_hmdb51_bs', type=str)
+    parser.add_argument('--name', default='./prep_hmdb51_bs1', type=str)
 
     args = parser.parse_args()
 
@@ -57,16 +71,18 @@ def main():
     data_loader = build_dataloader(dataset, **dataloader_setting)
 
     root_path = os.path.dirname(args.ann_file)
-    out_path = './prep_hmdb51_bs1'
+    out_path = args.name
+    
     if not os.path.exists(out_path):
         os.mkdir(out_path)
     info_file = open(os.path.join(root_path, 'hmdb51.info'), 'w')
 
-    for i, data in enumerate(data_loader):
-        print('Preprocessing video {}/{}'.format(i, len(data_loader)))
+    pbar = tqdm(data_loader)
+    i = 0
+    for data in pbar:
+        pbar.set_description('Preprocessing ')
         imgs = data['imgs']
         label = data['label']
-        print(imgs.shape)
 
         for batch in range(imgs.shape[0]):
             l = label.cpu().numpy()[batch]
@@ -77,11 +93,8 @@ def main():
             imgs = F.pad(imgs, (0,0,0,0,0,0,0,0,0,args.batch_size-imgs.shape[0]))
 
         bin_info = imgs.cpu().numpy()
-        print(bin_info.shape)
-        preprocess = torch.from_numpy(bin_info)
-        print(preprocess.shape)
         bin_info.tofile(out_path + '/' + str(i) + '.bin')
-
+        i += 1
 
 if __name__ == '__main__':
     main()
