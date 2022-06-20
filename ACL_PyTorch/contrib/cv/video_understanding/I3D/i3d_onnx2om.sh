e129e3259f0b1c30e3131d6a2301354d5c8c8e28
Nagaloop <chenjian244@huawei.com>
2022-06-20 03:42:50 +0000

    update ACL_PyTorch/contrib/cv/video_understanding/I3D/i3d_onnx2om.sh.

--- ACL_PyTorch/contrib/cv/video_understanding/I3D/i3d_onnx2om.sh
+++ ACL_PyTorch/contrib/cv/video_understanding/I3D/i3d_onnx2om.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ${install_path}/atc/bin/atc --framework=5 --output=./i3d_nl_dot_bs1  --input_format=NCHW  --soc_version=$1 \
-    --model=./i3d_nl_dot.onnx --input_shape="0:1,10,3,32,256,256"
+    --model=./i3d_nl_dot.onnx --input_shape="0:1,10,3,32,256,256" --output_type=FP16
 
 
