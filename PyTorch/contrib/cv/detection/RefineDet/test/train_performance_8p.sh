16eaeeb742d1621e2eecdf4e9e9e694692ec959c
白浪 <bailang12@h-partners.com>
2022-06-22 01:26:19 +0000

    !848 [众智][PyTorch]RefineDet模型训练脚本整改 * update PyTorch/contrib/cv/detection/RefineDet/test/train_full_1p.sh. * [众智][PyTorch]RefineDet模型训练脚本整改

--- PyTorch/contrib/cv/detection/RefineDet/test/train_performance_8p.sh
+++ PyTorch/contrib/cv/detection/RefineDet/test/train_performance_8p.sh
@@ -12,7 +12,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-data_path="/root/data/VOCdevkit"
+###############指定训练脚本执行路径###############
+# cd到与test文件夹同层级目录下执行脚本，提高兼容性；test_path_dir为包含test文件夹的路径
+cur_path=`pwd`
+cur_path_last_dirname=${cur_path##*/}
+if [ x"${cur_path_last_dirname}" == x"test" ];then
+    test_path_dir=${cur_path}
+    cd ..
+    cur_path=`pwd`
+else
+    test_path_dir=${cur_path}/test
+fi
+# 指定训练所使用的npu device卡id
+device_id=0
+
+# 数据集路径,保持为空,不需要修改
+data_path=""
 
 for para in $*
 do
@@ -21,10 +36,48 @@ do
     fi
 done
 
+#校验是否传入data_path,不需要修改
+if [[ $data_path == "" ]];then
+    echo "[Error] para \"data_path\" must be confing"
+    exit 1
+fi
+
 echo ${data_path}
 
-rm -rf nohup.out
+# 校验是否指定了device_id,分动态分配device_id与手动指定device_id,此处不需要修改
+if [ $ASCEND_DEVICE_ID ];then
+    echo "device id is ${ASCEND_DEVICE_ID}"
+elif [ ${device_id} ];then
+    export ASCEND_DEVICE_ID=${device_id}
+    echo "device id is ${ASCEND_DEVICE_ID}"
+else
+    "[Error] device id must be config"
+    exit 1
+fi
 
+#################创建日志输出目录，不需要修改#################
+if [ -d ${test_path_dir}/output/${ASCEND_DEVICE_ID} ];then
+    rm -rf ${test_path_dir}/output/${ASCEND_DEVICE_ID}
+    mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+else
+    mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+fi
+
+#非平台场景时source 环境变量
+check_etp_flag=`env | grep etp_running_flag`
+etp_flag=`echo ${check_etp_flag#*=}`
+if [ x"${etp_flag}" != x"true" ];then
+    source  ${test_path_dir}/env_npu.sh
+fi
+#训练开始时间，不需要修改
+start_time=$(date +%s)
+
+rm -rf nohup.out
+#训练batch_size
+batch_size=32
+#网络名称,同目录名称,需要模型审视修改
+Network="RefineDet"
+#集合通信参数,不需要修改
 RANK_ID_START=0
 RANK_SIZE=8
 
@@ -37,18 +90,48 @@ PID_END=$((PID_START + KERNEL_NUM - 1))
 
 
 
-nohup \
-taskset -c $PID_START-$PID_END python -u train_8p.py "$@"  \
+nohup taskset -c $PID_START-$PID_END python3.7 -u train_8p.py "$@"  \
     --dataset_root ${data_path} \
     --save_folder ./RefineDet320_bn/ \
     --num_workers $KERNEL_NUM \
     --world_size 8 \
-    --batch_size 32 \
+    --batch_size $batch_size \
     --bn True \
     --local_rank $RANK_ID \
     --lr 0.002 \
-    --num_epochs 1  &
+    --num_epochs 1   > ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
 done
+wait
+##################获取训练数据################
+#训练结束时间，不需要修改
+end_time=$(date +%s)
+e2e_time=$(( $end_time - $start_time ))
 
+#结果打印，不需要修改
+echo "------------------ Final result ------------------"
+#输出性能FPS，需要模型审视修改
+FPS=`grep -a 'FPS'  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F " " '{print $3}'|tail -1`
+#打印，不需要修改
+echo "Final Performance images/sec : $FPS"
+echo "E2E Training Duration sec : $e2e_time"
 
+#性能看护结果汇总
+#训练用例信息，不需要修改
+BatchSize=${batch_size}
+DeviceType=`uname -m`
+CaseName=${Network}_bs${BatchSize}_${RANK_SIZE}'p'_'perf'
 
+##获取性能数据，不需要修改
+#吞吐量
+ActualFPS=${FPS}
+#单迭代训练时长
+TrainingTime=`awk -vbs=$batch_size -vfps=$FPS 'BEGIN{printf "%.2f\n", bs*1000/fps}'`
+
+echo "Network = ${Network}" >  ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RankSize = ${RANK_SIZE}" >>  ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "BatchSize = ${BatchSize}" >>  ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "DeviceType = ${DeviceType}" >>  ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "CaseName = ${CaseName}" >>  ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualFPS = ${ActualFPS}" >>  ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "TrainingTime = ${TrainingTime}" >>  ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RefineDetTrainingTime = ${e2e_time}" >>  ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
