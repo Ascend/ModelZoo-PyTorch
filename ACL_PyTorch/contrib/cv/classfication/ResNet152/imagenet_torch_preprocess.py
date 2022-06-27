78bb2c6d17864dafed6cc1782ff73393ca7c3462
董展硕 <10828577+dong-zhanshuo@user.noreply.gitee.com>
2022-06-27 07:35:13 +0000

    !380 [浙江工业大学][高校贡献][PyTorch离线推理][ResNet152]-初次提交 * 添加710修改 * 删除文件 README1.md * 删除文件 gen_dataset_info.py * 删除文件 imagenet_acc_eval.py * 删除文件 imagenet_torch_preprocess.py * 删除文件 resnet152_pth2onnx.py * 添加pyramidbox 710修改 * 添加pyramidbox 710修改

--- ACL_PyTorch/contrib/cv/classfication/ResNet152/imagenet_torch_preprocess.py
+++ ACL_PyTorch/contrib/cv/classfication/ResNet152/imagenet_torch_preprocess.py
@@ -18,7 +18,6 @@ from PIL import Image
 import numpy as np
 import multiprocessing
 
-
 model_config = {
     'resnet': {
         'resize': 256,
@@ -42,13 +41,15 @@ model_config = {
 
 
 def center_crop(img, output_size):
-    if isinstance(output_size, int):
+    if isinstance(output_size, int):  # 可以判断一个变量的类型
         output_size = (int(output_size), int(output_size))
     image_width, image_height = img.size
     crop_height, crop_width = output_size
     crop_top = int(round((image_height - crop_height) / 2.))
+    # 一般情况也是使用四舍五入的规则，但是碰到.5的这样情况，如果要取舍的位数前的小数是奇数，则直接舍弃，如果偶数这向上取舍。
     crop_left = int(round((image_width - crop_width) / 2.))
     return img.crop((crop_left, crop_top, crop_left + crop_width, crop_top + crop_height))
+    # 你输入的大小部分截取出来，其他的没有任何变化
 
 
 def resize(img, size, interpolation=Image.BILINEAR):
@@ -65,7 +66,7 @@ def resize(img, size, interpolation=Image.BILINEAR):
             ow = int(size * w / h)
             return img.resize((ow, oh), interpolation)
     else:
-        return img.resize(size[::-1], interpolation)
+        return img.resize(size[::-1], interpolation)  # resize是原位操作，直接将原来的a3拉成一维，
 
 
 def gen_input_bin(mode_type, file_batches, batch):
@@ -76,20 +77,20 @@ def gen_input_bin(mode_type, file_batches, batch):
 
         # RGBA to RGB
         image = Image.open(os.path.join(src_path, file)).convert('RGB')
-        image = resize(image, model_config[mode_type]['resize']) # Resize
-        image = center_crop(image, model_config[mode_type]['centercrop']) # CenterCrop
+        image = resize(image, model_config[mode_type]['resize'])  # Resize
+        image = center_crop(image, model_config[mode_type]['centercrop'])  # CenterCrop
         img = np.array(image, dtype=np.float32)
-        img = img.transpose(2, 0, 1) # ToTensor: HWC -> CHW
-        img = img / 255. # ToTensor: div 255
-        img -= np.array(model_config[mode_type]['mean'], dtype=np.float32)[:, None, None] # Normalize: mean
-        img /= np.array(model_config[mode_type]['std'], dtype=np.float32)[:, None, None] # Normalize: std
-        img.tofile(os.path.join(save_path, file.split('.')[0] + ".bin"))
+        img = img.transpose(2, 0, 1)  # ToTensor: HWC -> CHW 矩阵转置
+        img = img / 255.  # ToTensor: div 255
+        img -= np.array(model_config[mode_type]['mean'], dtype=np.float32)[:, None, None]  # Normalize: mean
+        img /= np.array(model_config[mode_type]['std'], dtype=np.float32)[:, None, None]  # Normalize: std
+        img.tofile(os.path.join(save_path, file.split('.')[0] + ".bin")) # tofile()将数组中的数据以二进制格式写进文件
 
 
-def preprocess(mode_type, src_path, save_path):
+def preprocess(mode_type, src_path, save_path): # 数据预处理
     files = os.listdir(src_path)
     file_batches = [files[i:i + 500] for i in range(0, 50000, 500) if files[i:i + 500] != []]
-    thread_pool = multiprocessing.Pool(len(file_batches))
+    thread_pool = multiprocessing.Pool(len(file_batches))  # 进程是文件批的长度
     for batch in range(len(file_batches)):
         thread_pool.apply_async(gen_input_bin, args=(mode_type, file_batches, batch))
     thread_pool.close()
@@ -103,7 +104,7 @@ if __name__ == '__main__':
     mode_type = sys.argv[1]
     src_path = sys.argv[2]
     save_path = sys.argv[3]
-    src_path = os.path.realpath(src_path)
+    src_path = os.path.realpath(src_path) # 获取当前执行脚本的绝对路径。
     save_path = os.path.realpath(save_path)
     if mode_type not in model_config:
         model_type_help = "model type: "
@@ -114,4 +115,3 @@ if __name__ == '__main__':
     if not os.path.isdir(save_path):
         os.makedirs(os.path.realpath(save_path))
     preprocess(mode_type, src_path, save_path)
-
