03e0b89595258b6b8d690e77dfe982e94d969381
xingjinliang <xjl1263780481@163.com>
2022-06-27 07:23:40 +0000

    !914 【PyTorch】【dev】【st-gcn_ID2967_for_PyTorch】模型移仓，准备精度入场 !914 【PyTorch】【dev】【st-gcn_ID2967_for_PyTorch】模型移仓，准备精度入场

---
+++ PyTorch/dev/cv/multimodality/st-gcn_ID2967_for_PyTorch/tools/utils/ntu_read_skeleton.py
@@ -0,0 +1,90 @@
+#
+# BSD 3-Clause License
+#
+# Copyright (c) 2017 xxxx
+# All rights reserved.
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ============================================================================
+#
+import numpy as np
+import os
+import os
+NPU_CALCULATE_DEVICE = 0
+if os.getenv('NPU_CALCULATE_DEVICE') and str.isdigit(os.getenv('NPU_CALCULATE_DEVICE')):
+    NPU_CALCULATE_DEVICE = int(os.getenv('NPU_CALCULATE_DEVICE'))
+
+
+def read_skeleton(file):
+    with open(file, 'r') as f:
+        skeleton_sequence = {}
+        skeleton_sequence['numFrame'] = int(f.readline())
+        skeleton_sequence['frameInfo'] = []
+        for t in range(skeleton_sequence['numFrame']):
+            frame_info = {}
+            frame_info['numBody'] = int(f.readline())
+            frame_info['bodyInfo'] = []
+            for m in range(frame_info['numBody']):
+                body_info = {}
+                body_info_key = [
+                    'bodyID', 'clipedEdges', 'handLeftConfidence',
+                    'handLeftState', 'handRightConfidence', 'handRightState',
+                    'isResticted', 'leanX', 'leanY', 'trackingState'
+                ]
+                body_info = {
+                    k: float(v)
+                    for k, v in zip(body_info_key, f.readline().split())
+                }
+                body_info['numJoint'] = int(f.readline())
+                body_info['jointInfo'] = []
+                for v in range(body_info['numJoint']):
+                    joint_info_key = [
+                        'x', 'y', 'z', 'depthX', 'depthY', 'colorX', 'colorY',
+                        'orientationW', 'orientationX', 'orientationY',
+                        'orientationZ', 'trackingState'
+                    ]
+                    joint_info = {
+                        k: float(v)
+                        for k, v in zip(joint_info_key, f.readline().split())
+                    }
+                    body_info['jointInfo'].append(joint_info)
+                frame_info['bodyInfo'].append(body_info)
+            skeleton_sequence['frameInfo'].append(frame_info)
+    return skeleton_sequence
+
+
+def read_xyz(file, max_body=2, num_joint=25):
+    seq_info = read_skeleton(file)
+    data = np.zeros((3, seq_info['numFrame'], num_joint, max_body))
+    for n, f in enumerate(seq_info['frameInfo']):
+        for m, b in enumerate(f['bodyInfo']):
+            for j, v in enumerate(b['jointInfo']):
+                if m < max_body and j < num_joint:
+                    data[:, n, j, m] = [v['x'], v['y'], v['z']]
+                else:
+                    pass
+    return data
