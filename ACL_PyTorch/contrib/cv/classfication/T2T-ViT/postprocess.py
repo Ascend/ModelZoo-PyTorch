b5ff08e114d797738d8d7a51cdae8e018a3ce637
王旭 <5260362+wx1149693659@user.noreply.gitee.com>
2022-06-29 11:46:07 +0000

    update ACL_PyTorch/contrib/cv/classfication/T2T-ViT/postprocess.py.

--- ACL_PyTorch/contrib/cv/classfication/T2T-ViT/postprocess.py
+++ ACL_PyTorch/contrib/cv/classfication/T2T-ViT/postprocess.py
@@ -69,43 +69,44 @@ def post_precess(result_dir, gt_path, args):
     top5_m = AverageMeter()
 
     for res_path in Path(result_dir).iterdir():
-        batch_idx = int(res_path.stem.replace('_output_0', ''))
-        target = labels[batch_idx]
-        output = np.fromfile(res_path.__str__(), dtype=np.float32)
-        output = torch.from_numpy(output.reshape(1, args.num_classes))
+        for res in Path(res_path).iterdir():
+            batch_idx = int(res_path.stem.replace('_output_0', ''))
+            target = labels[batch_idx]
+            output = np.fromfile(res_path.__str__(), dtype=np.float32)
+            output = torch.from_numpy(output.reshape(1, args.num_classes))
 
-        reduce_factor = args.tta
-        if reduce_factor > 1:
-            output = output.unfold(0, reduce_factor, reduce_factor).mean(dim=2)
-            target = target[0:target.size(0):reduce_factor]
+            reduce_factor = args.tta
+            if reduce_factor > 1:
+                output = output.unfold(0, reduce_factor, reduce_factor).mean(dim=2)
+                target = target[0:target.size(0):reduce_factor]
 
-        loss = loss_fn(output, target)
-        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            loss = loss_fn(output, target)
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
 
-        if args.distributed:
-            reduced_loss = reduce_tensor(loss.data, args.world_size)
-            acc1 = reduce_tensor(acc1, args.world_size)
-            acc5 = reduce_tensor(acc5, args.world_size)
-        else:
-            reduced_loss = loss.data
+            if args.distributed:
+                reduced_loss = reduce_tensor(loss.data, args.world_size)
+                acc1 = reduce_tensor(acc1, args.world_size)
+                acc5 = reduce_tensor(acc5, args.world_size)
+            else:
+                reduced_loss = loss.data
 
-        losses_m.update(reduced_loss.item(), output.size(0))
-        top1_m.update(acc1.item(), output.size(0))
-        top5_m.update(acc5.item(), output.size(0))
+            losses_m.update(reduced_loss.item(), output.size(0))
+            top1_m.update(acc1.item(), output.size(0))
+            top5_m.update(acc5.item(), output.size(0))
 
-        end = time.time()
-        batch_time_m.update(time.time() - end)
-        if args.local_rank == 0 and batch_idx % args.log_interval == 0:
-            # print(target)
-            log_name = 'Test'
-            _logger.info(
-                '{0}: [{1:>4d}]  '
-                'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})  '
-                'Loss: {loss.val:>7.4f} ({loss.avg:>6.4f})  '
-                'Acc@1: {top1.val:>7.4f} ({top1.avg:>7.4f})  '
-                'Acc@5: {top5.val:>7.4f} ({top5.avg:>7.4f})'.format(
-                    log_name, batch_idx, batch_time=batch_time_m,
-                    loss=losses_m, top1=top1_m, top5=top5_m))
+            end = time.time()
+            batch_time_m.update(time.time() - end)
+            if args.local_rank == 0 and batch_idx % args.log_interval == 0:
+                # print(target)
+                log_name = 'Test'
+                _logger.info(
+                    '{0}: [{1:>4d}]  '
+                    'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})  '
+                    'Loss: {loss.val:>7.4f} ({loss.avg:>6.4f})  '
+                    'Acc@1: {top1.val:>7.4f} ({top1.avg:>7.4f})  '
+                    'Acc@5: {top5.val:>7.4f} ({top5.avg:>7.4f})'.format(
+                        log_name, batch_idx, batch_time=batch_time_m,
+                        loss=losses_m, top1=top1_m, top5=top5_m))
 
     metrics = OrderedDict([('loss', losses_m.avg), ('top1', top1_m.avg), ('top5', top5_m.avg)])
     print(f"Top-1 accuracy of the model is: {metrics['top1']:.1f}%")
