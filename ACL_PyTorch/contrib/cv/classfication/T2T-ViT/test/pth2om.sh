a0597f20fc9045a583486cab4d4b9395c38afcd0
王旭 <5260362+wx1149693659@user.noreply.gitee.com>
2022-06-29 11:49:18 +0000

    update ACL_PyTorch/contrib/cv/classfication/T2T-ViT/test/pth2om.sh.

--- ACL_PyTorch/contrib/cv/classfication/T2T-ViT/test/pth2om.sh
+++ ACL_PyTorch/contrib/cv/classfication/T2T-ViT/test/pth2om.sh
@@ -7,4 +7,4 @@ python3.7 pth2onnx.py
 source env.sh
 
 # 生成om文件
-atc --framework=5 --model=T2T_ViT_14.onnx --output=T2T_ViT_14_bs8_test --input_format=NCHW --input_shape="input:8,3,224,224" --soc_version=Ascend710 --keep_dtype=keep_dtype.cfg
+atc --framework=5 --model=T2T_ViT_14.onnx --output=T2T_ViT_14_bs1_test --input_format=NCHW --input_shape="input:1,3,224,224" --soc_version=Ascend710 --keep_dtype=keep_dtype.cfg
