44d2fcbc95bbe2472f6cdfd3c845dba1d18afff4
李世龙 <1844491768@qq.com>
2022-06-30 06:27:17 +0000

    !318 [东北大学软件学院][高校贡献][PyTorch离线推理][SKNet50]-初次提交 * add modelzoo_level.txt * update ACL_PyTorch/contrib/cv/classfication/Sknet50/sknet2onnx.py. * update ACL_PyTorch/contrib/cv/classfication/Sknet50/README.md. * 删除文件 ACL_PyTorch/contrib/cv/classfication/Sknet50/env.sh * update ACL_PyTorch/contrib/cv/classfication/Sknet50/README.md. * update ACL_PyTorch/contrib/cv/classfication/Sknet50/README.md. * update ACL_PyTorch/contrib/cv/classfication/Sknet50/sknet2onnx.py. * update ACL_PyTorch/contrib/cv/classfication/Sknet50/test/eval_acc_perf.sh. * update ACL_PyTorch/contrib/cv/classfication/Sknet50/test/pth2om.sh. * 710上ATC环境 * update ACL_PyTorch/contrib/cv/classfication/Sknet50/sknet2onnx.py. * update ACL_PyTorch/contrib/cv/classfication/Sknet50/requirements.txt. * update ACL_PyTorch/contrib/cv/classfication/Sknet50/README.md. * T4性能 * update ACL_PyTorch/contrib/cv/classfication/Sknet50/test/pth2om.sh. * update ACL_PyTorch/contrib/cv/classfication/Sknet50/test/eval_acc_perf.sh. * update ACL_PyTorch/contrib/cv/classfication/Sknet50/test/README.md.

--- ACL_PyTorch/contrib/cv/classfication/Sknet50/sknet2onnx.py
+++ ACL_PyTorch/contrib/cv/classfication/Sknet50/sknet2onnx.py
@@ -44,26 +44,6 @@ def main():
     dynamic_axes = {'image': {0: '-1'}, 'class': {0: '-1'}}
     dummy_input = torch.randn(1, 3, 224, 224)
     torch.onnx.export(model.module, dummy_input, args.onnx, input_names = input_names, dynamic_axes = dynamic_axes, output_names = output_names, opset_version=11, verbose=True)
-    
-    # delete transpose operator to improve performance
-    model = onnx.load(args.onnx)
-    graph = model.graph
-    node = graph.node
-    softmax_node_index = []
-    del_group = []
-    for i in range(len(node)):
-        if node[i].op_type == 'Softmax':
-            del_group.append((node[i-1], node[i], node[i+1], i))
-    for g in del_group:
-        new_input = g[0].input
-        new_output = g[2].output
-        new_name = g[1].name
-        new_index = g[3]
-        new_node = onnx.helper.make_node("Softmax", new_input, new_output, new_name, axis=1)
-        for n in g[:-1]:
-            graph.node.remove(n)
-        graph.node.insert(new_index, new_node)
-    onnx.save(model, args.onnx)
         
 if __name__ == '__main__':
     main()
