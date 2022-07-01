7a5ee052aca710fc84284b2353a251dfb206dba3
彭奥奥 <pengaoao@huawei.com>
2022-07-01 06:40:45 +0000

    !986 Yolox_tiny新增使用融合算子YoloxBoundingboxDecode，增强推理性能 * fix * add YoloxBoundingBoxDecode * add YoloxBoundingBoxDecode * add YoloxBoundingBoxDecode * add YoloxBoundingBoxDecode

--- ACL_PyTorch/built-in/cv/YoloX_Tiny_for_Pytorch/adaptyoloxtiny.py
+++ ACL_PyTorch/built-in/cv/YoloX_Tiny_for_Pytorch/adaptyoloxtiny.py
@@ -27,24 +27,36 @@ import argparse
 def revise_model(src_path, save_path):
     input_path = src_path
     mod2 = OXGraph(input_path)
-    NMS = mod2.get_nodes_by_optype("BatchMultiClassNMS")
-    nodes_before = mod2.get_nodes_forward_node(NMS[0].name, if_self=False)
+    Decode = mod2.get_nodes_by_optype("YoloxBoundingBoxDecode")
+    nodes_before = mod2.get_nodes_forward_node(Decode[0].name, if_self=False)
     nodes_before.discard('NULL')
-    mod2.node_remove(nodes_before)
+    Mul_node = mod2.get_node("Mul_523")
+    nodes_before_mul = mod2.get_nodes_forward_node(Mul_node.name, if_self=False)
+    nodes_before_mul.discard('NULL')
+    remove_nodes = nodes_before_mul.union(nodes_before)
+    mod2.node_remove(remove_nodes)
 
-    o2_p1_1750 = mod2.add_placeholder_node("o2_p1_1750", "float16", [1, 1000, 0, 4])
-    o2_p2_1753 = mod2.add_placeholder_node("o2_p2_1753", "float16", [1, 1000, 0])
-    NMS[0].set_input_node(0, [o2_p1_1750, o2_p2_1753])
+    o2_p1_1750 = mod2.add_placeholder_node("o2_p1_1750", "float32", [1, 1000, 0, 4])
+    o2_p2_1753 = mod2.add_placeholder_node("o2_p2_1753", "float32", [1, 1000, 0])
+    mul_p3_1113 = mod2.add_placeholder_node("mul_p3_1113", "float32", [1, 1000, 0, 4])
+    mul_p4_1119 = mod2.add_placeholder_node("mul_p4_1119", "float32", [1, 1000, 0, 4])
+    Decode[0].set_input_node(0, [o2_p1_1750, o2_p2_1753])
+    Mul_node.set_input_node(0, [mul_p3_1113, mul_p4_1119])
     mod2.save_new_model("o2.onnx")
 
     mod1 = OXGraph(input_path)
-    nodes_behind = mod1.get_nodes_behind_node(NMS[0].name, if_self=True)
-    nms_node = mod1.get_node(NMS[0].name)
+    nodes_behind = mod1.get_nodes_behind_node(Decode[0].name, if_self=True)
+    nms_node = mod1.get_node(Decode[0].name)
     mod1.mod.graph.output.remove(mod1.mod.graph.output[0])
     mod1.mod.graph.output.remove(mod1.mod.graph.output[0])
-    onnx1 = mod1.node_remove(nodes_behind)
-    new_1750 = mod1.add_output_node("1259", "float16")
-    new_1753 = mod1.add_output_node("1272", "float16")
+
+    nodes_behind_mul = mod1.get_nodes_behind_node(Mul_node.name, if_self=True)
+    remove_nodes_behind = nodes_behind.union(nodes_behind_mul)
+    onnx1 = mod1.node_remove(remove_nodes_behind)
+    new_1750 = mod1.add_output_node("1117", "float32")
+    new_1753 = mod1.add_output_node("1116", "float32")
+    new_1113 = mod1.add_output_node("1113", "float32")
+    new_1119 = mod1.add_output_node("1119", "float32")
     mod1.save_new_model("o1.onnx")
 
     cmd = "amct_onnx calibration --model ./o1.onnx --save_path ./result --input_shape " \
@@ -52,8 +64,8 @@ def revise_model(src_path, save_path):
     os.system(cmd)
 
     #change the cast int64 to int32
-    int8mod = OXGraph("result_deploy_model.onnx")
-    Expand_lists = ["Add_614", "Add_631"]
+    int8mod = OXGraph("o2.onnx")
+    Expand_lists = ["Add_574", "Add_557"]
     for i in range(len(Expand_lists)):
           now_expand = int8mod.get_node(Expand_lists[i])
           cast_node1 = int8mod.add_new_node(now_expand.name + "_cast_1", "Cast",
@@ -67,10 +79,11 @@ def revise_model(src_path, save_path):
           now_expand.set_input_node(0, [cast_node1, cast_node2])
           cast_node1.set_input_node(0, [Expand_first_input_now])
           cast_node2.set_input_node(0, [Expand_second_input_now])
-    int8mod.save_new_model("result_deploy_model_revise.onnx")
-    g1 = so.graph_from_file("result_deploy_model_revise.onnx")
-    g2 = so.graph_from_file("o2.onnx")
-    g_merge = so.merge(sg1=g1, sg2=g2, io_match=[("1259", "o2_p1_1750"), ("1272", "o2_p2_1753")], complete=False)
+    int8mod.save_new_model("o2_revise.onnx")
+    g1 = so.graph_from_file("result_deploy_model.onnx")
+    g2 = so.graph_from_file("o2_revise.onnx")
+    g_merge = so.merge(sg1=g1, sg2=g2, io_match=[("1117", "o2_p1_1750"), ("1116", "o2_p2_1753"),
+                                                 ("1113", "mul_p3_1113"), ("1119", "mul_p4_1119")], complete=False)
     so.graph_to_file(g_merge, save_path)
 
 if __name__ == '__main__':
