da71a31acc9cefb0d05b178ca854cdf669af5965
赵焱 <zhaoyan71@huawei.com>
2022-07-05 14:28:00 +0000

    !1032 「pytorch自研」yolov4 1.8 模型进异常分支问题修改 * 删除注释 * 解决部分异常问题

--- PyTorch/built-in/cv/detection/YOLOV4_ID0396_for_PyTorch/utils/torch_utils.py
+++ PyTorch/built-in/cv/detection/YOLOV4_ID0396_for_PyTorch/utils/torch_utils.py
@@ -133,14 +133,7 @@ def model_info(model, verbose=False):
             name = name.replace('module_list.', '')
             print('%5g %40s %9s %12g %20s %10.3g %10.3g' %
                   (i, name, p.requires_grad, p.numel(), list(p.shape), p.mean(), p.std()))
-
-    try:  # FLOPS
-        from thop import profile
-        flops = profile(deepcopy(model), inputs=(torch.zeros(1, 3, 64, 64),), verbose=False)[0] / 1E9 * 2
-        fs = ', %.1f GFLOPS' % (flops * 100)  # 640x640 FLOPS
-    except:
-        fs = ''
-
+    fs = ''
     print('Model Summary: %g layers, %g parameters, %g gradients%s' % (len(list(model.parameters())), n_p, n_g, fs))
 
 
