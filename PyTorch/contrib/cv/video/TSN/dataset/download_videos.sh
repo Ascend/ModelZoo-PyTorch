fc7415b8a1c5a3d6b4731ee9355a75cd0c367b6c
姚皓哲 <yaohaozhe1@huawei.com>
2022-07-06 01:51:23 +0000

    !992  [pytorch训练][TSN]解决资料问题单，修改数据路径以及训练完毕无法打印FPS问题 *  [pytorch训练][cv][video][TSN]解决资料问题单，修改数据路径以及训练完毕无法打印FPS问题

--- PyTorch/contrib/cv/video/TSN/dataset/download_videos.sh
+++ PyTorch/contrib/cv/video/TSN/dataset/download_videos.sh
@@ -12,6 +12,5 @@ cd ${DATA_DIR}
 wget https://www.crcv.ucf.edu/datasets/human-actions/ucf101/UCF101.rar --no-check-certificate
 unrar x UCF101.rar
 mv ./UCF-101 ./videos
-mv -f videos/ ucf101/
 
 cd ".."
