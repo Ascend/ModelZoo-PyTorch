14e9dd245f33c5a5b7a3f142430f70978c76fd93
danansheng <danansheng1@163.com>
2022-07-08 10:22:06 +0800

    update ACL_PyTorch/contrib/cv/segmentation/Cascade_RCNN

--- ACL_PyTorch/contrib/cv/segmentation/Cascade_RCNN/cascadercnn_atc.sh
+++ ACL_PyTorch/contrib/cv/segmentation/Cascade_RCNN/cascadercnn_atc.sh
@@ -6,4 +6,4 @@ export ASCEND_OPP_PATH=${install_path}/opp
 export ASCEND_AICPU_PATH=/usr/local/Ascend/ascend-toolkit/latest
 
 
-atc --model=model_py1.8.onnx --framework=5 --output=cascadercnn_detectron2_npu --input_format=NCHW --input_shape="0:1,3,1344,1344" --out_nodes="Cast_1853:0;Gather_1856:0;Reshape_1847:0;Slice_1886:0" --log=debug --soc_version=Ascend710
+atc --model=model_py1.8.onnx --framework=5 --output=cascadercnn_detectron2_npu --input_format=NCHW --input_shape="0:1,3,1344,1344" --out_nodes="Cast_1853:0;Gather_1856:0;Reshape_1847:0;Slice_1886:0" --log=debug --soc_version=Ascend${chip_name}
