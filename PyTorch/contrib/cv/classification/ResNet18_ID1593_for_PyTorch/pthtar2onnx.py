219bca3891ad448deb385379ae778522938224a5
宋武淦晶 <s_wgj@qq.com>
2022-07-18 08:01:08 +0000

    !1139 [东北大学][高校贡献][Pytorch][ResNet18]-高性能预训练模型提交+ModelArts+SDK&MxBase !1139 [东北大学][高校贡献][Pytorch][ResNet18]-高性能预训练模型提交+ModelArts+SDK&MxBase

--- PyTorch/contrib/cv/classification/ResNet18_ID1593_for_PyTorch/pthtar2onnx.py
+++ PyTorch/contrib/cv/classification/ResNet18_ID1593_for_PyTorch/pthtar2onnx.py
@@ -15,18 +15,32 @@
 import torch
 import torchvision
 import torch.onnx
+import resnet
+from collections import OrderedDict
 
-def convert():
-    checkpoint = torch.load("./checkpoint.pth.tar", map_location='cpu')
-    model = torchvision.models.resnet18(pretrained=True)
-    model.load_state_dict(checkpoint['state_dict'], False)
+def proc_node_module(checkpoint, AttrName):
+    new_state_dict = OrderedDict()
+    for k, v in checkpoint[AttrName].items():
+        if(k[0:7] == "module."):
+            name = k[7:]
+        else:
+            name = k[0:]
+        new_state_dict[name] = v
+    return new_state_dict
+
+
+def convert(pth_file_path="./model_best.pth.tar", onnx_file_path="resnet18_npu_1.onnx"):
+    checkpoint = torch.load(pth_file_path, map_location='cpu')
+    checkpoint['state_dict'] = proc_node_module(checkpoint, 'state_dict')
+    model = resnet.build_resnet("resnet18", "classic")
+    model.load_state_dict(checkpoint['state_dict'])
     model.eval()
-    print(model)
+    #print(model)
 
     input_names = ["actual_input_1"]
     output_names = ["output1"]
-    dummy_input = torch.randn(16, 3, 224, 224)
-    torch.onnx.export(model, dummy_input, "resnet18_npu_16.onnx", input_names=input_names, output_names=output_names,
+    dummy_input = torch.randn(1, 3, 224, 224)
+    torch.onnx.export(model, dummy_input, onnx_file_path, input_names=input_names, output_names=output_names,
                       opset_version=11)
 
 
