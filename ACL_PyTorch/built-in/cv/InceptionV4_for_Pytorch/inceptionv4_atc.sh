8051d39bf7e9a53dee7fdc8c592b6e862224ee1a
zhaozeyong <651997388@qq.com>
2022-07-21 16:21:37 +0000

    inceptionV4 710修改

--- ACL_PyTorch/built-in/cv/InceptionV4_for_Pytorch/inceptionv4_atc.sh
+++ ACL_PyTorch/built-in/cv/InceptionV4_for_Pytorch/inceptionv4_atc.sh
@@ -7,7 +7,9 @@ export ASCEND_OPP_PATH=${install_path}/opp
 #export GLOBAL_LOG_LEVEL=3 #log level
 #export SLOG_PRINT_TO_STDOUT=0  #print log to tenminal
 #export DUMP_GE_GRAPH=0 #dump ge
+batch_size=$1
+chip_name=$2
 
-atc --model=./inceptionv4.onnx --framework=5 --output=inceptionv4_bs16 --input_format=NCHW --input_shape="actual_input_1:16,3,299,299" --log=info --soc_version=Ascend310
+atc --model=./inceptionv4.onnx --framework=5 --output=inceptionv4_bs${batch_size} --input_format=NCHW --input_shape="actual_input_1:${batch_size},3,299,299" --log=info --soc_version=Ascend${chip_name}
 
 #atc --model=./inceptionv4.onnx --framework=5 --output=inceptionv4_bs1 --input_format=NCHW --input_shape="actual_input_1:1,3,300,300" --log=info --soc_version=Ascend310 --insert_op_conf=aipp_inceptionv4_pth.config
