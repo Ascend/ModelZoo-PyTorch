3d0dfb71f945ba6c42cbd3cd581b134caef947a5
白浪 <bailang12@h-partners.com>
2022-07-22 06:23:03 +0000

    !1219 [众智][PyTorch]albert模型脚本整改 !1219 [众智][PyTorch]albert模型脚本整改

--- PyTorch/contrib/nlp/albert/test/train_performance_8p.sh
+++ PyTorch/contrib/nlp/albert/test/train_performance_8p.sh
@@ -1,9 +1,10 @@
 # encoding=utf-8
 
-source ./test/env_npu.sh
-
 # 数据集路径,保持为空,不需要修改
 data_path=""
+batch_size=440
+Network='albert'
+RANK_SIZE=8
 # 参数校验，data_path为必传参数，其他参数的增删由模型自身决定；此处新增参数需在上面有定义并赋值
 for para in $*
 do
@@ -31,6 +32,34 @@ if [ x"${cur_path_last_diename}" == x"test" ];then
 else
     test_path_dir=${cur_path}/test
 fi
+# 指定训练所使用的npu device卡id
+device_id=0
+
+# 校验是否指定了device_id,分动态分配device_id与手动指定device_id,此处不需要修改
+if [ $ASCEND_DEVICE_ID ];then
+    echo "device id is ${ASCEND_DEVICE_ID}"
+elif [ ${device_id} ];then
+    export ASCEND_DEVICE_ID=${device_id}
+    echo "device id is ${ASCEND_DEVICE_ID}"
+else
+    "[Error] device id must be config"
+    exit 1
+fi
+
+#非平台场景时source 环境变量
+check_etp_flag=`env | grep etp_running_flag`
+etp_flag=`echo ${check_etp_flag#*=}`
+if [ x"${etp_flag}" != x"true" ];then
+    source  ${test_path_dir}/env_npu.sh
+fi
+
+#################创建日志输出目录，不需要修改#################
+if [ -d ${test_path_dir}/output/${ASCEND_DEVICE_ID} ];then
+    rm -rf ${test_path_dir}/output/${ASCEND_DEVICE_ID}
+    mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+else
+    mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+fi
 
 rm -rf ./outputs
 mkdir ./outputs
@@ -40,9 +69,11 @@ export OUTPUR_DIR=./outputs
 export PYTHONPATH=./:$PYTHONPATH
 export DEVICE=npu
 TASK_NAME="SST-2"
+#训练开始时间，不需要修改
+start_time=$(date +%s)
 for i in $(seq 7 -1 0)
     do
-      python3.7 ./run_classifier.py \
+      nohup python3.7 ./run_classifier.py \
         --device=$DEVICE \
         --model_type=$BERT_MODEL \
         --model_name_or_path=$BERT_BASE_DIR/$BERT_MODEL \
@@ -63,10 +94,49 @@ for i in $(seq 7 -1 0)
         --seed=42 \
         --local_rank=$i \
         --fp16 \
-        --fp16_opt_level=O2 &
+        --fp16_opt_level=O2  > ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
     done
 
 wait
-rm -rf ./test/output
-mkdir ./test/output
-cp ./outputs/$TASK_NAME/*.log ./test/output/$TASK_NAME.log
+##################获取训练数据################
+#训练结束时间，不需要修改
+end_time=$(date +%s)
+e2e_time=$(( $end_time - $start_time ))
+
+#结果打印，不需要修改
+echo "------------------ Final result ------------------"
+#输出性能FPS，需要模型审视修改
+FPS=`grep -a 'FPS'  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "FPS = " '{print $2}'|awk 'NR==1{max=$1;next}{max=max>$1?max:$1}END{print max}'`
+#打印，不需要修改
+echo "Final Performance images/sec : $FPS"
+#输出训练精度,需要模型审视修改
+train_accuracy=`grep -a 'acc'  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "acc = " '{print $2}'|tail -1`
+#打印，不需要修改
+echo "Final Train Accuracy : ${train_accuracy}"
+#打印，不需要修改
+echo "E2E Training Duration sec : $e2e_time"
+
+#性能看护结果汇总
+#训练用例信息，不需要修改
+BatchSize=${batch_size}
+DeviceType=`uname -m`
+CaseName=${Network}_bs${BatchSize}_${RANK_SIZE}'p'_'acc'
+
+#获取性能数据，不需要修改
+#吞吐量
+ActualFPS=${FPS}
+
+#从train_$ASCEND_DEVICE_ID.log提取Loss到train_${CaseName}_loss.txt中，需要模型审视修改
+grep -a 'loss' ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log|awk -F "loss: " '{print $2}' |awk -F " " '{print $1}' >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt
+#最后一个迭代loss值，不需要修改
+ActualLoss=`awk 'END {print}' ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt`
+
+#关键信息打印到${CaseName}.log中，不需要修改
+echo "Network = ${Network}" > ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RankSize = ${RANK_SIZE}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "BatchSize = ${BatchSize}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "DeviceType = ${DeviceType}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "CaseName = ${CaseName}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualFPS = ${ActualFPS}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualLoss = ${ActualLoss}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "E2ETrainingTime = ${e2e_time}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
