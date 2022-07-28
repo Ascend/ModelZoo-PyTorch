6ec450bced35920deb66b35a36e4f9761c57d5af
zhangYu <zlnangyu@163.com>
2022-07-28 12:25:21 +0000

    !714 [四川大学][高校贡献][Pytorch][ErfNet训练]-初次提交 !714 [四川大学][高校贡献][Pytorch][ErfNet训练]-初次提交

--- PyTorch/contrib/cv/semantic_segmentation/ErfNet/test/train_eval_8p.sh
+++ PyTorch/contrib/cv/semantic_segmentation/ErfNet/test/train_eval_8p.sh
@@ -15,6 +15,10 @@ if [[ $data_path == "" ]];then
     exit 1
 fi
 
+#################启动训练脚本#################
+#训练开始时间，不需要修改
+start_time=$(date +%s)
+
 RANK_ID_START=0
 RANK_SIZE=8
 
@@ -25,16 +29,32 @@ KERNEL_NUM=$(($(nproc)/8))
 PID_START=$((KERNEL_NUM * RANK_ID))
 PID_END=$((PID_START + KERNEL_NUM - 1))
 
-nohup taskset -c $PID_START-$PID_END python3.7.5 -u eval/eval_iou.py \
+nohup taskset -c $PID_START-$PID_END python3.7 -u eval/eval_iou.py \
 --datadir ${data_path} \
 --loadDir "save/erfnet_training1/" \
 --loadWeights "model_best.pth" \
 --amp \
 --opt-level "O2" \
---loss-scale-value 128 \
+--loss-scale-value "dynamic" \
 --device npu \
 --num_gpus 8 \
 --num-workers 32 \
 --batch-size 24 \
 --rank_id $RANK_ID > erfnet_eval.log 2>&1 &
 done
+
+wait
+
+##################获取训练数据################
+# 训练结束时间，不需要修改
+end_time=$(date +%s)
+e2e_time=$(( $end_time - $start_time ))
+
+# 结果打印，不需要修改
+echo "------------------ Final result ------------------"
+
+# 输出训练精度,需要模型审视修改
+iou=`grep 'MEAN IoU' erfnet_eval.log |tail -n 1|awk '{print $3}'|awk 'END {print}'`
+# 打印，不需要修改
+echo "Final Train IoU: ${iou}"
+echo "E2E Training Duration sec : $e2e_time"
