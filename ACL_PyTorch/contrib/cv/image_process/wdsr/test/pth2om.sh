48a047dc92298a06e2ec1a07e66b5fa99a99fbf6
魏子浩 <weizihao0229@163.com>
2022-07-28 07:41:38 +0000

    !620 [天津大学][高校贡献][PyTorch离线推理][WDSR]-初次提交 * update ACL_PyTorch/contrib/cv/image_process/wdsr/README.md. * update ACL_PyTorch/contrib/cv/image_process/wdsr/README.md. * update ACL_PyTorch/contrib/cv/image_process/wdsr/README.md. * update ACL_PyTorch/contrib/cv/image_process/wdsr/README.md. * update ACL_PyTorch/contrib/cv/image_process/wdsr/README.md. * 删除文件 PyTorch/contrib/cv/others/WDSR * 删除文件 ACL_PyTorch/contrib/cv/image_process/wdsr/test/.keep * [天津大学][高校贡献][PyTorch离线推理][WDSR]-初次提交 * 新建 test * 删除文件 ACL_PyTorch/contrib/cv/image_process/wdsr/test/pth2om.sh * 删除文件 ACL_PyTorch/contrib/cv/image_process/wdsr/test/parse.py * 删除文件 ACL_PyTorch/contrib/cv/image_process/wdsr/test/eval_acc_perf.sh * [天津大学][高校贡献][PyTorch离线推理][WDSR]-初次提交 * 删除文件 ACL_PyTorch/contrib/cv/image_process/wdsr/requirements.txt * 删除文件 ACL_PyTorch/contrib/cv/image_process/wdsr/modelzoo_level.txt * 删除文件 ACL_PyTorch/contrib/cv/image_process/wdsr/env.sh * 删除文件 ACL_PyTorch/contrib/cv/image_process/wdsr/README.md * update PyTorch/contrib/cv/others/WDSR/modelzoo_level.txt. * update PyTorch/contrib/cv/others/WDSR/README.md. * update PyTorch/contrib/cv/others/WDSR/test/pth2om.sh. * update PyTorch/contrib/cv/others/WDSR/test/eval_acc_perf.sh. * update PyTorch/contrib/cv/others/WDSR/test/eval_acc_perf.sh. * update PyTorch/contrib/cv/others/WDSR/test/aipp_test.sh. * 删除文件 PyTorch/contrib/cv/others/WDSR/env.sh * update PyTorch/contrib/cv/others/WDSR/modelzoo_level.txt. * 删除文件 PyTorch/contrib/cv/others/WDSR/.keep * [天津大学][高校贡献][PyTorch离线推理][WDSR]-初次提交 * 新建 WDSR

--- ACL_PyTorch/contrib/cv/image_process/wdsr/test/pth2om.sh
+++ ACL_PyTorch/contrib/cv/image_process/wdsr/test/pth2om.sh
@@ -3,11 +3,12 @@
 rm -rf wdsr.onnx
 python3.7 Wdsr_pth2onnx.py --ckpt epoch_30.pth --model wdsr --output_name wdsr.onnx --scale 2
 
-source env.sh
-rm -rf Wdsr_bs1.om Wdsr_bs8.om
+. /usr/local/Ascend/ascend-toolkit/set_env.sh  
+rm -rf Wdsr_bs1.om Wdsr_bs4.om Wdsr_bs8.om
 atc --framework=5 --model=wdsr.onnx --output=wdsr_bs1 --input_format=NCHW --input_shape="image:1,3,1020,1020" --log=debug --soc_version=Ascend310
+atc --framework=5 --model=wdsr.onnx --output=wdsr_bs4 --input_format=NCHW --input_shape="image:4,3,1020,1020" --log=debug --soc_version=Ascend310
 atc --framework=5 --model=wdsr.onnx --output=wdsr_bs8 --input_format=NCHW --input_shape="image:8,3,1020,1020" --log=debug --soc_version=Ascend310
-if [ -f "wdsr_bs1.om" ] && [ -f "wdsr_bs8.om" ]; then
+if [ -f "wdsr_bs1.om" ] && [ -f "wdsr_bs8.om" ] && [ -f "wdsr_bs4.om" ]; then
     echo "success"
 else
     echo "fail!"
