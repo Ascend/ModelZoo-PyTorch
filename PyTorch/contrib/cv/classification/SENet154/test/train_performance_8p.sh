a38b9f31d2db8a7f1e158aef9171e2ea6fbd20aa
白浪 <bailang12@h-partners.com>
2022-07-28 12:00:49 +0000

    !1286 [众智][PyTorch]SENet154模型修复性能指标过滤bug !1286 [众智][PyTorch]SENet154模型修复性能指标过滤bug

--- PyTorch/contrib/cv/classification/SENet154/test/train_performance_8p.sh
+++ PyTorch/contrib/cv/classification/SENet154/test/train_performance_8p.sh
@@ -102,7 +102,7 @@ FPS=`grep -a 'FPS'  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_D
 echo "Final Performance images/sec : $FPS"
 
 # 输出训练精度,需要模型审视修改
-train_accuracy=`grep -a '* Acc@1' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk 'END {print}'|awk -F "Acc@1" '{print $NF}'|awk -F " " '{print $1}'`
+train_accuracy=`grep -a 'Acc@1' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk 'END {print}'|awk -F "Acc@1: " '{print $NF}'|awk -F " " '{print $1}'`
 # 打印，不需要修改
 echo "Final Train Accuracy : ${train_accuracy}"
 echo "E2E Training Duration sec : $e2e_time"
