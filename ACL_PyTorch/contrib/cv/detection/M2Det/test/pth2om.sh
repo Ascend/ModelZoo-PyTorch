addb7e1b9edd2e18bea3ce429531a8e3045c035e
朱哲良 <1326938221@qq.com>
2022-08-09 08:21:59 +0000

    update ACL_PyTorch/contrib/cv/detection/M2Det/test/pth2om.sh.

--- ACL_PyTorch/contrib/cv/detection/M2Det/test/pth2om.sh
+++ ACL_PyTorch/contrib/cv/detection/M2Det/test/pth2om.sh
@@ -5,9 +5,9 @@ python3.7 M2Det_pth2onnx.py -c=M2Det/configs/m2det512_vgg.py -pth=M2Det/weights/
 
 source env.sh
 rm -rf m2det512_bs1.om
-atc --framework=5 --model=m2det512.onnx --input_format=NCHW --input_shape="image:1,3,512,512" --output=m2det512_bs1 --log=debug --soc_version=Ascend310 --out-nodes="Softmax_1234:0;Reshape_1231:0"
+atc --framework=5 --model=m2det512.onnx --input_format=NCHW --input_shape="image:1,3,512,512" --output=m2det512_bs1 --log=debug --soc_version=Ascend${chip_name} --out-nodes="Softmax_1234:0;Reshape_1231:0"
 rm -rf m2det512_bs16.om
-atc --framework=5 --model=m2det512.onnx --input_format=NCHW --input_shape="image:16,3,512,512" --output=m2det512_bs16 --log=debug --soc_version=Ascend310 --out-nodes="Softmax_1234:0;Reshape_1231:0"
+atc --framework=5 --model=m2det512.onnx --input_format=NCHW --input_shape="image:16,3,512,512" --output=m2det512_bs16 --log=debug --soc_version=Ascend${chip_name} --out-nodes="Softmax_1234:0;Reshape_1231:0"
 if [ -f "m2det512_bs1.om" ] && [ -f "m2det512_bs16.om" ]; then
     echo "success"
 else
