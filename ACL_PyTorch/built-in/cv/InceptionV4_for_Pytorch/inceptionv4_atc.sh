1c6405de45509b2c4d3926f0ab381b655abe7cf6
赵泽勇 <651997388@qq.com>
2022-08-11 05:34:16 +0000

    删除文件 ACL_PyTorch/built-in/cv/InceptionV4_for_Pytorch/inceptionv4_atc.sh

--- ACL_PyTorch/built-in/cv/InceptionV4_for_Pytorch/inceptionv4_atc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /usr/local/Ascend/ascend-toolkit/set_env.sh
-export LD_LIBRARY_PATH=/usr/local/Ascend/driver/lib64/driver/:${LD_LIBRARY_PATH}
-batch_size=$1
-chip_name=$2
-
-atc --model=./inceptionv4.onnx --framework=5 --output=inceptionv4_bs${batch_size} --input_format=NCHW --input_shape="actual_input_1:${batch_size},3,299,299" --log=info --soc_version=Ascend${chip_name}
