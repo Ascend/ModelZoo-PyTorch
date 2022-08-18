1a499bc35d098a75967ff4e7b644b014d58d5a11
zheng-wengang1 <zhengwengang2@huawei.com>
2022-08-18 16:19:22 +0800

    feat: add acl_pytorch demo for swintransfromer model(timm)

---
+++ ACL_PyTorch/built-in/cv/SwinTransformer_for_Pytorch/merge_add.py
@@ -0,0 +1,86 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import argparse
+import numpy as np
+from magiconnx import OnnxGraph
+
+
+def get_value_info(graph, out_name):
+    for value in graph._model.graph.value_info:
+        if value.name == out_name:
+            return [dim.dim_value for dim in value.type.tensor_type.shape.dim]
+
+
+def merge_add(graph):
+    def check_next_node(node, op_type):
+        try:
+            next_node = graph.get_next_nodes(node.name)[0]
+            if next_node.op_type != op_type:
+                return False
+        except:
+            return False
+        return True
+
+    for matmul_node in graph.get_nodes(op_type="MatMul"):
+        # check pattern: matmul->add->reshape->add->reshape
+        if not check_next_node(matmul_node, "Add"):
+            continue
+        add_node1 = graph.get_next_nodes(matmul_node.name)[0]
+        if not check_next_node(add_node1, "Reshape"):
+            continue
+        reshape_node1 = graph.get_next_nodes(add_node1.name)[0]
+        if not check_next_node(reshape_node1, "Add"):
+            continue
+        add_node2 = graph.get_next_nodes(reshape_node1.name)[0]
+        if not check_next_node(add_node2, "Reshape"):
+            continue
+        reshape_node2 = graph.get_next_nodes(add_node2.name)[0]
+
+        input_shape = get_value_info(graph, graph[matmul_node.name].outputs[0])
+        # merge add value
+        add_value1 = graph[add_node1.inputs[1]].value
+        add_value2 = graph[add_node2.inputs[1]].value
+        broad_dim0 = input_shape[0]
+        broad_dim1 = add_value1.shape[1]
+        broad_dim2 = add_value2.shape[1]
+        add_value1 = np.tile(add_value1, (broad_dim0, 1, 1, 1))
+        add_value2 = np.tile(add_value2, (broad_dim0//broad_dim1, 1, broad_dim2, 1, 1))
+        out_shape = add_value1.shape
+        graph[add_node1.inputs[1]].value = add_value1 + add_value2.reshape(out_shape)
+
+        # del reshape->add->reshape
+        graph.del_node(reshape_node1.name)
+        graph.del_node(add_node2.name)
+        graph.del_node(reshape_node2.name)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='SwinTransformer preprocess.')
+    parser.add_argument('-i', '--input_path', type=str, required=True,
+                        help='input model path')
+    parser.add_argument('-o', '--out_path', type=str, required=True,
+                        help='output path for optimized model')
+    args = parser.parse_args()
+    os.makedirs(os.path.dirname(args.out_path), exist_ok=True)
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    onnx_graph = OnnxGraph(args.input_path)
+    merge_add(onnx_graph)
+    onnx_graph.save(args.out_path)
