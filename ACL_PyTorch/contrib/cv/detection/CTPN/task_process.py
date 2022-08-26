c982662c7c5d646b6c922fe4dc7a08e1cc6cee83
menglijinghua <508304482@qq.com>
2022-08-26 10:38:06 +0800

    python3.7改成了python3

--- ACL_PyTorch/contrib/cv/detection/CTPN/task_process.py
+++ ACL_PyTorch/contrib/cv/detection/CTPN/task_process.py
@@ -30,14 +30,14 @@ def task_process(args):
             os.system('rm -rf ctpn_change_{}x{}.onnx'.format(h, w))
         for i in range(config.center_len):
             h, w = config.center_list[i][0], config.center_list[i][1]
-            os.system('python3.7 change_model.py --input_path=ctpn_{}x{}.onnx \
+            os.system('python3 change_model.py --input_path=ctpn_{}x{}.onnx \
             --output_path=ctpn_change_{}x{}.onnx'.format(h, w, h, w))
     if args.mode == 'preprocess':
         for i in range(config.center_len):
             os.system('rm -rf data/images_bin_{}x{}'.format(config.center_list[i][0], config.center_list[i][1]))
         for i in range(config.center_len):
             os.system('mkdir data/images_bin_{}x{}'.format(config.center_list[i][0], config.center_list[i][1]))
-        os.system('python3.7 ctpn_preprocess.py --src_dir={} --save_path=./data/images_bin'.format(args.src_dir))
+        os.system('python3 ctpn_preprocess.py --src_dir={} --save_path=./data/images_bin'.format(args.src_dir))
     if args.mode == 'ais_infer':
         fps_all = 0
         for i in range(config.center_len):
