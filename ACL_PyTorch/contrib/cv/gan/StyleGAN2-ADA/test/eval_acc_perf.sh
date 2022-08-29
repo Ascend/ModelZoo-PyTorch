11e7d964195747745ac58d78e0d06af55b6b0d60
liuzhuheng <liuzhuheng@huawei.com>
2022-08-29 23:48:44 +0800

    【自研】【Pytorch离线推理】StyleGAN2-ADA模型切换ais-infer工具

--- ACL_PyTorch/contrib/cv/gan/StyleGAN2-ADA/test/eval_acc_perf.sh
+++ ACL_PyTorch/contrib/cv/gan/StyleGAN2-ADA/test/eval_acc_perf.sh
@@ -25,8 +25,10 @@ mkdir -p dump/bs1
 touch dump/log.txt
 #mkdir -p dump/bs16
 
-./msame --model "./G_ema_om_bs1.om" --input "./input/bs1" --output "./dump/bs1" >> dump/log.txt
+#./msame --model "./G_ema_om_bs1.om" --input "./input/bs1" --output "./dump/bs1" >> dump/log.txt
 #./msame --model "./G_ema_om_bs16.om" --input "./input/bs16" --output "./dump/bs16"
+
+ python3 ./ais_infer_x86_64/ais_infer.py --model ../G_ema_om_bs1.om --input ./input/bs1/ --output ./dump/bs1/ >> dump/log.txt
 
 if [ -d "./dump/bs1" ]; then
   echo "inference with om success"
@@ -40,6 +42,8 @@ bin_path_bs1="dump/bs1/`ls dump/bs1 | tail -1`"
 if [ -d ${image_path}]; then
   rm -r ${image_path}
 fi
+
+rm ./dump/bs1/*/summary.json
 
 python stylegan2-ada-pytorch_postprocess.py --bin_path=${bin_path_bs1} --image_path=${image_path}
 
