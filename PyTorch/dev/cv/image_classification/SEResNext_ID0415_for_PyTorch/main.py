f23fe7f037f3fc34ce00dff8d3d2e151d0cf5a59
wuxingpeng <wxpeng2013@163.com>
2022-08-29 12:44:48 +0000

    !1635 【PyTorch】【built-in】【DeepMar_for_PyTorch】等多个网络适配torch1.8接口； !1635 【PyTorch】【built-in】【DeepMar_for_PyTorch】等多个网络适配torch1.8接口；

--- PyTorch/dev/cv/image_classification/SEResNext_ID0415_for_PyTorch/main.py
+++ PyTorch/dev/cv/image_classification/SEResNext_ID0415_for_PyTorch/main.py
@@ -72,6 +72,8 @@ import random
 
 import numpy as np
 import torch
+if torch.__version__ >= "1.8":
+    import torch_npu
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.parallel
