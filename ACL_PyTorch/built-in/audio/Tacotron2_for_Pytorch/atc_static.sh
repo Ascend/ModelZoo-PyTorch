e0172c1b3bdbbf0998417559dbb9891e51967f7f
彭奥奥 <pengaoao@huawei.com>
2022-09-20 09:28:52 +0000

    !1863 Tacotron2 !1863 Tacotron2

--- ACL_PyTorch/built-in/audio/Tacotron2_for_Pytorch/atc_static.sh
+++ ACL_PyTorch/built-in/audio/Tacotron2_for_Pytorch/atc_static.sh
@@ -15,4 +15,4 @@ atc --framework=5 --output=decoder_static  --input_format=ND  --soc_version=${SO
     --input_shape="decoder_input:${batch_size},80;attention_hidden:${batch_size},1024;attention_cell:${batch_size},1024;decoder_hidden:${batch_size},1024;decoder_cell:${batch_size},1024;attention_weights:${batch_size},${seq_len};attention_weights_cum:${batch_size},${seq_len};attention_context:${batch_size},512;
     memory_out:${batch_size},${seq_len},512;processed_memory_out:${batch_size},${seq_len},128;mask_out:${batch_size},${seq_len};gate_output_input:${batch_size},1,1;mel_output_input:${batch_size},80,1;not_finished_input:${batch_size};mel_lengths_input:${batch_size}"
 
-atc --framework=5 --output=postnet_static --input_format=ND  --soc_version=${SOC_VERSION} --input_shape="mel_outputs:${batch_size},80,2000"
+atc --framework=5 --model=postnet.onnx --output=postnet_static --input_format=ND  --soc_version=${SOC_VERSION} --input_shape="mel_outputs:${batch_size},80,2000"
