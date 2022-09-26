83e1aaab4c0df221d147f792f987e680bb807206
sebdog <lh76997@gmail.com>
2022-09-26 01:51:51 +0000

    update ACL_PyTorch/contrib/nlp/ELMo/elmo_postprocess.py.
    Signed-off-by: sebdog <lh76997@gmail.com>

--- ACL_PyTorch/contrib/nlp/ELMo/elmo_postprocess.py
+++ ACL_PyTorch/contrib/nlp/ELMo/elmo_postprocess.py
@@ -24,24 +24,14 @@ import argparse
 import numpy as np
 
 
-def cosine_similarity(x, y):
-    x1 = x.flatten().astype(dtype='float64')
-    y1 = y.flatten().astype(dtype='float64')
-    dot = np.dot(x1, y1)
-    lx = np.linalg.norm(x1)
-    ly = np.linalg.norm(y1)
-    cos = dot / (lx * ly)
-    return cos
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--inputs', default='bin_path/')
     parser.add_argument('--om_output')
-    parser.add_argument('--option_file', default="elmo_2x4096_512_2048cnn_2xhighway_options.json")
-    parser.add_argument('--weight_file', default="elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5")
+    parser.add_argument('--option_file', default='elmo_2x4096_512_2048cnn_2xhighway_options.json')
+    parser.add_argument('--weight_file', default='elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5')
     opt = parser.parse_args()
-    
+
     elmo = Elmo(opt.option_file, opt.weight_file, 1)
     elmo.eval()
     
@@ -49,20 +39,21 @@ def main():
 
     similarity = 0
     nums = len(os.listdir(opt.inputs))
+    cos = torch.nn.CosineSimilarity(dim=0)
     for i in range(nums):
         input_file = np.fromfile(opt.inputs + '{0}.bin'.format(i), dtype='int32').reshape((1, 8, 50))
         input_file = torch.from_numpy(input_file)
         
         om_output_file = np.fromfile(om_output_path + '{0}_0.bin'.format(i), dtype='float32').reshape((1, 8, 1024))
-        
+        om_output_file = torch.Tensor(om_output_file.flatten().astype(dtype='float64'))
         output = elmo.forward(input_file)
         
-        output = output['elmo_representations'][0].detach().numpy()
+        output = output['elmo_representations'][0].detach().flatten()
 
-        cosine_sim = cosine_similarity(om_output_file, output)
+        cosine_sim = cos(om_output_file, output)
         similarity += cosine_sim
+        print(cosine_sim)
     print('average similarity: ', similarity / nums)
-
 
 if __name__ == '__main__':
     main()
