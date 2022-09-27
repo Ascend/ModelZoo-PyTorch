354fa1855d1173f8d30094fb9f84dced07399202
memg <xumengmeng16@huawei-partners.com>
2022-09-27 10:44:03 +0000

    !1977 【模型调优】【PyTorch】【dev】【Swin-Transformer_ID2379_for_PyTorch】修改8P分布式 !1977 【模型调优】【PyTorch】【dev】【Swin-Transformer_ID2379_for_PyTorch】修改8P分布式

--- PyTorch/dev/nlp/Swin-Transformer_ID2379_for_PyTorch/test/train_performance_8p.sh
+++ PyTorch/dev/nlp/Swin-Transformer_ID2379_for_PyTorch/test/train_performance_8p.sh
@@ -2,34 +2,32 @@
 source /usr/local/Ascend/latest/bin/setenv.bash
 #当前路径,不需要修改
 cur_path=`pwd`
-export ASCEND_SLOG_PRINT_TO_STDOUT=0
-export NPU_CALCULATE_DEVICE=$ASCEND_DEVICE_ID
 #集合通信参数,不需要修改
 
 export HCCL_WHITELIST_DISABLE=1
 export MASTER_ADDR=127.0.0.1
 export MASTER_PORT=23456
 export RANK=0
-export WORLD_SIZE=1
 
-
-export RANK_SIZE=1
+export RANK_SIZE=8
 export JOB_ID=10087
 RANK_ID_START=0
 
 #进入到conda环境
-
+#export PATH=/usr/local/python3.7.5/bin:/home/anaconda3/bin:$PATH
+#export PATH=/home/anaconda3/bin:$PATH
+#export LD_LIBRARY_PATH=/home/anaconda3/lib:$LD_LIBRARY_PATH
 #source activate py8
 
 for para in $*
 do
-    if [[ $para == --data_path* ]];then
-        data_path=`echo ${para#*=}`
+	if [[ $para == --data_path* ]];then
+		data_path=`echo ${para#*=}`
     elif [[ $para == --conda_name* ]];then
         conda_name=`echo ${para#*=}`
         source set_conda.sh
         source activate $conda_name
-    fi
+	fi
 done
 
 #pip3 uninstall timm -y
@@ -51,7 +49,7 @@ batch_size=64
 #训练step
 #train_steps=`expr 1281167 / ${batch_size}`
 #学习率
-learning_rate=0.495
+learning_rate=
 
 #TF2.X独有，不需要修改
 #export NPU_LOOP_SIZE=${train_steps}
@@ -64,7 +62,6 @@ data_dump_flag=False
 data_dump_step="10"
 profiling=False
 autotune=False
-bin_mode=False
 PREC=""
 
 # 帮助信息，不h需要修改
@@ -73,12 +70,12 @@ if [[ $1 == --help || $1 == -h ]];then
     echo " "
     echo "parameter explain:
     --precision_mode         precision mode(allow_fp32_to_fp16/force_fp16/must_keep_origin_dtype/allow_mix_precision)
-    --over_dump                 if or not over detection, default is False
-    --data_dump_flag         data dump flag, default is False
-    --data_dump_step         data dump step, default is 10
-    --profiling                 if or not profiling for performance debug, default is False
-    --data_path                 source data of training
-    -h/--help                 show help message
+    --over_dump		         if or not over detection, default is False
+    --data_dump_flag	     data dump flag, default is False
+    --data_dump_step		 data dump step, default is 10
+    --profiling		         if or not profiling for performance debug, default is False
+    --data_path		         source data of training
+    -h/--help		         show help message
     "
     exit 1
 fi
@@ -110,8 +107,6 @@ elif [[ $para == --over_dump* ]];then
         mkdir -p ${profiling_dump_path}
     elif [[ $para == --data_path* ]];then
         data_path=`echo ${para#*=}`
-    elif [[ $para == --bin_mode* ]];then
-        bin_mode="True"
     fi
 done
 
@@ -127,10 +122,6 @@ start_time=$(date +%s)
 #进入训练脚本目录，需要模型审视修改
 cd $cur_path/../
 
-#新增模糊编译接口
-if [ $bin_mode == "True" ];then
-    sed -i "67itorch.npu.set_compile_mode(jit_compile=False)" ${cur_path}/main.py
-fi
 #kill残余的此网络进程
 #ps -ef | grep moby | awk '{print $2}' | xargs kill -9
 
@@ -145,12 +136,11 @@ sed -i "s|pass|break|g" moby_main.py
 
 for((RANK_ID=$RANK_ID_START;RANK_ID<$((RANK_SIZE+RANK_ID_START));RANK_ID++));
 do
-    #设置环境变量，不需要修改
-    echo "Device ID: $ASCEND_DEVICE_ID"
+    echo "Device ID: $RANK_ID"
     export RANK_ID=$RANK_ID
-    
-    
-    
+    export ASCEND_DEVICE_ID=$RANK_ID
+    export NPU_CALCULATE_DEVICE=$ASCEND_DEVICE_ID
+
     #创建DeviceID输出目录，不需要修改
     if [ -d ${cur_path}/output/${ASCEND_DEVICE_ID} ];then
         rm -rf ${cur_path}/output/${ASCEND_DEVICE_ID}
@@ -158,15 +148,6 @@ do
     else
         mkdir -p ${cur_path}/output/$ASCEND_DEVICE_ID/ckpt
     fi
-    
-    #绑核，不需要绑核的模型删除，需要绑核的模型根据实际修改
-    #cpucount=`lscpu | grep "CPU(s):" | head -n 1 | awk '{print $2}'`
-    #cpustep=`expr $cpucount / 8`
-    #echo "taskset c steps:" $cpustep
-    #let a=RANK_ID*$cpustep
-    #let b=RANK_ID+1
-    #let c=b*$cpustep-1
-
     #执行训练脚本，以下传参不需要修改，其他需要模型审视修改
     nohup python3 moby_main.py $PREC --cfg configs/moby_swin_tiny.yaml --local_rank 0 --data-path $data_path --batch-size ${batch_size} > ${cur_path}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
 done 
@@ -177,11 +158,12 @@ sed -i "s|TRAIN.EPOCHS = 1|TRAIN.EPOCHS = 300|g" config.py
 sed -i "s|EPOCHS: 1|EPOCHS: 300|g"  configs/moby_swin_tiny.yaml
 sed -i "s|break|pass|g" moby_main.py
 
+ASCEND_DEVICE_ID=0
 #kill残余的此网络进程
 #ps -ef | grep moby | awk '{print $2}' | xargs kill -9
 
 #退出conda环境
-conda deactivate
+#conda deactivate
 
 #训练结束时间，不需要修改
 end_time=$(date +%s)
@@ -191,8 +173,7 @@ e2e_time=$(( $end_time - $start_time ))
 echo "------------------ Final result ------------------"
 #输出性能FPS，需要模型审视修改
 FPS=`grep FPS $cur_path/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "FPS:" '{print $2}'|tail -n +2|awk '{sum+=$1} END {print"",sum/NR}'|sed s/[[:space:]]//g`
-#FPS=`awk 'BEGIN{printf "%.2f\n",'${batch_size}'*'${perf}'}'`
-
+#FPS=`awk 'BEGIN{printf "%.2f\n",'${RANK_SIZE}'*'${perf}'}'`
 
 #打印，不需要修改
 echo "Final Performance images/sec : $FPS"
