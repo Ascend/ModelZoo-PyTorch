5a69ff1aff8ebb253745ed0dad7e05e9cdc8893c
chenping68 <chenping68@h-partners.com>
2022-09-27 08:18:00 +0000

    !1982 【PyTorch】【built-in】【ResNet3D】loss scale 修改为静态 !1982 【PyTorch】【built-in】【ResNet3D】loss scale 修改为静态

--- PyTorch/built-in/cv/classification/3D_ResNet_ID0421_for_PyTorch/test/train_performance_8p.sh
+++ PyTorch/built-in/cv/classification/3D_ResNet_ID0421_for_PyTorch/test/train_performance_8p.sh
@@ -153,7 +153,7 @@ nohup python3 main.py \
   --no_val \
   --n_epochs ${train_epochs} \
   --opt_level O2 \
-  --loss_scale_value 'dynamic' \
+  --loss_scale_value 1024 \
   --distributed \
   --ngpus_per_node 8 \
   --device_list '0,1,2,3,4,5,6,7' \
