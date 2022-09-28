af6f4dcab18108924a402e15b57de234966c2101
bam_bo_o <zyz_bamboo@126.com>
2022-09-28 07:02:30 +0000

    !1123 [西安交通大学][高校贡献][PyTorch离线推理][UNet]-初次提交 * UNet-zyz-827 * UNet-zyz-827 * UNet-zyz-806-2 * UNet-zyz-806 * UNet-zyz-805 * UNet-zyz-718 * UNet-zyz-715 * UNet-zyz-714 * UNet-zyz-713 * UNet-zyz-3

---
+++ ACL_PyTorch/contrib/cv/segmentation/UNet/json_parse.py
@@ -0,0 +1,33 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+import os
+
+import sys
+file_dir = sys.argv[1]
+
+with open(file_dir + "sumary.json",'r') as load_f:
+    load_dict = json.load(load_f)
+
+print(load_dict.keys())
+
+load_dict = load_dict['filesinfo']
+load_dict_0 = load_dict['0']
+print(load_dict_0.keys())
+
+for i in load_dict.keys():
+    data = load_dict[i]
+    infile = os.path.basename(data['infiles'][0])
+    outfile = os.path.basename(data['outfiles'][0])
+    os.rename(file_dir + outfile, file_dir + infile)
