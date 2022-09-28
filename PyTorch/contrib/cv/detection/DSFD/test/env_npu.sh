32848b3af7085b679197bf842ec207603d6dee87
lj_new <lijing202@huawei.com>
2022-09-28 10:46:37 +0000

    !2025 pytorch批量环境变量整改 * 删除无关文件 * 修改环境变量 * 修改环境变量

--- PyTorch/contrib/cv/detection/DSFD/test/env_npu.sh
+++ PyTorch/contrib/cv/detection/DSFD/test/env_npu.sh
@@ -15,9 +15,7 @@ fi
 
 
 #将Host日志输出到串口,0-关闭/1-开启
-export ASCEND_SLOG_PRINT_TO_STDOUT=0
-#关闭多线程下发
-export TASK_QUEUE_ENABLE=0
+export ASCEND_SLOG_PRINT_TO_STDOUT=1
 #设置默认日志级别,0-debug/1-info/2-warning/3-error
 export ASCEND_GLOBAL_LOG_LEVEL=3
 #设置Event日志开启标志,0-关闭/1-开启
@@ -27,12 +25,12 @@ export TASK_QUEUE_ENABLE=1
 #设置是否开启PTCopy,0-关闭/1-开启
 export PTCOPY_ENABLE=1
 #设置是否开启combined标志,0-关闭/1-开启
-export COMBINED_ENABLE=1
+export COMBINED_ENABLE=0
+#设置特殊场景是否需要重新编译,不需要修改
+export DYNAMIC_OP="ADD#MUL"
 #HCCL白名单开关,1-关闭/0-开启
 export HCCL_WHITELIST_DISABLE=1
 export HCCL_IF_IP=$(hostname -I |awk '{print $1}')
-#export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
-
 
 path_lib=$(python3.7 -c """
 import sys
@@ -51,8 +49,6 @@ for index in range(len(sys.path)):
 print(result)"""
 )
 
+echo ${path_lib}
+
 export LD_LIBRARY_PATH=/usr/local/python3.7.5/lib/:${path_lib}:$LD_LIBRARY_PATH
-
-export DYNAMIC_OP="ADD#MUL"
-
-
