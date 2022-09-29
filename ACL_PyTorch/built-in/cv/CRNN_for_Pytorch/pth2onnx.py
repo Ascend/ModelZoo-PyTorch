b7d710fc71019bf9fac89bd3013bff721d86c670
Nagaloop <chenjian244@huawei.com>
2022-09-29 06:29:13 +0000

    !2044 CRNN 适配新的pth * update ACL_PyTorch/built-in/cv/CRNN_for_Pytorch/pth2onnx.py. * update ACL_PyTorch/built-in/cv/CRNN_for_Pytorch/pth2onnx.py. * update ACL_PyTorch/built-in/cv/CRNN_for_Pytorch/pth2onnx.py.

--- ACL_PyTorch/built-in/cv/CRNN_for_Pytorch/pth2onnx.py
+++ ACL_PyTorch/built-in/cv/CRNN_for_Pytorch/pth2onnx.py
@@ -1,22 +1,40 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
 import crnn
 import onnx
 import torch.onnx
+import sys
 
-def convert():
-    checkpoint = torch.load("./checkpoint_16_CRNN_acc_0.7963.pth", map_location='cpu')
-    model = crnn.CRNN(32,1,37,256)
-    model.load_state_dict(checkpoint['state_dict'])
+def convert(path, out):
+    checkpoint = torch.load(path, map_location='cpu')
+    model = crnn.CRNN(32, 1, 37, 256)
+    model.load_state_dict(
+        {k.replace('module.', ""): v for k, v in checkpoint['state_dict'].items()})
     model.eval()
     print(model)
 
     input_names = ["actual_input_1"]
     output_names = ["output1"]
     dummy_input = torch.randn(1, 1, 32, 100)
-    dynamic_axes = {'actual_input_1':{0:'-1'},'output1':{1:'-1'}}
+    dynamic_axes = {'actual_input_1': {0: '-1'}, 'output1': {1: '-1'}}
     print('\nStarting ONNX export with onnx %s...' % onnx.__version__)
-    torch.onnx.export(model, dummy_input, "crnn_npu_dy.onnx", input_names=input_names,dynamic_axes = dynamic_axes, output_names=output_names, opset_version=11)
+    torch.onnx.export(model, dummy_input, out, input_names=input_names, dynamic_axes = dynamic_axes, output_names=output_names, opset_version=11)
 
 
 if __name__ == "__main__":
-    convert()
+    path = sys.argv[1]
+    out = sys.argv[2]
+    convert(path, out)
