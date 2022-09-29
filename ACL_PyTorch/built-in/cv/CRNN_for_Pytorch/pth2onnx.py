6c00e1b1165a0e101f4e5b334f11b3388a44cdda
Nagaloop <chenjian244@huawei.com>
2022-09-29 12:06:36 +0000

    !2061 CRNN 适配新的pth * update ACL_PyTorch/built-in/cv/CRNN_for_Pytorch/README.md. * update ACL_PyTorch/built-in/cv/CRNN_for_Pytorch/pth2onnx.py.

--- ACL_PyTorch/built-in/cv/CRNN_for_Pytorch/pth2onnx.py
+++ ACL_PyTorch/built-in/cv/CRNN_for_Pytorch/pth2onnx.py
@@ -17,12 +17,34 @@ import crnn
 import onnx
 import torch.onnx
 import sys
+from collections import OrderedDict
+
+def proc_node_module(checkpoint, AttrName):
+    new_state_dict = OrderedDict()
+    for k, v in checkpoint[AttrName].items():
+        if (k[0:7] == "module."):
+            name = k[7:]
+        else:
+            name = k[0:]
+        new_state_dict[name] = v
+    return new_state_dict
+
+
+def lstm_op_adapter(state_dict):
+    ret = {}
+    for key, value in state_dict.items():
+        if not (key.startswith('rnn') and ('fw' in key or 'bw' in key)):
+            ret[key] = value
+            continue
+        param = state_dict[key].data.split(256)
+        ret[key] = torch.cat((param[0], param[2], param[1], param[3]), 0)
+    return ret
 
 def convert(path, out):
     checkpoint = torch.load(path, map_location='cpu')
+    checkpoint['state_dict'] = proc_node_module(checkpoint, 'state_dict')
     model = crnn.CRNN(32, 1, 37, 256)
-    model.load_state_dict(
-        {k.replace('module.', ""): v for k, v in checkpoint['state_dict'].items()})
+    model.load_state_dict(lstm_op_adapter(checkpoint['state_dict']))
     model.eval()
     print(model)
 
@@ -31,7 +53,13 @@ def convert(path, out):
     dummy_input = torch.randn(1, 1, 32, 100)
     dynamic_axes = {'actual_input_1': {0: '-1'}, 'output1': {1: '-1'}}
     print('\nStarting ONNX export with onnx %s...' % onnx.__version__)
-    torch.onnx.export(model, dummy_input, out, input_names=input_names, dynamic_axes = dynamic_axes, output_names=output_names, opset_version=11)
+    torch.onnx.export(model, 
+                    dummy_input, 
+                    out, 
+                    input_names=input_names, 
+                    dynamic_axes = dynamic_axes, 
+                    output_names=output_names, 
+                    opset_version=11)
 
 
 if __name__ == "__main__":
