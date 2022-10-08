477811bb7eeefe27cc4fcc297db991020c64a2d6
魏鹏飞 <weipengfei19@huawei.com>
2022-10-08 09:19:00 +0000

    !2107 [众智][PyTorch]ResNet34_ID1594_for_PyTorch模型8卡性能脚本优化workers数 * ResNet34模型优化workers数

--- PyTorch/contrib/cv/classification/ResNet34_ID1594_for_PyTorch/test/train_performance_8p.sh
+++ PyTorch/contrib/cv/classification/ResNet34_ID1594_for_PyTorch/test/train_performance_8p.sh
@@ -64,6 +64,9 @@ check_etp_flag=`env | grep etp_running_flag`
 etp_flag=`echo ${check_etp_flag#*=}`
 if [ x"${etp_flag}" != x"true" ];then
     source  ${test_path_dir}/env_npu.sh
+    workers=$(nproc)
+else
+    workers=1024
 fi
 
 python3 ./main.py \
@@ -71,7 +74,7 @@ python3 ./main.py \
     -a resnet34 \
     --addr=$(hostname -I |awk '{print $1}') \
     --seed=49 \
-    --workers=1024 \
+    --workers=${workers} \
     --learning-rate=${learning_rate} \
     --mom=0.9 \
     --weight-decay=1.0e-04  \
