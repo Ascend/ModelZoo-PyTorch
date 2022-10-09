a439b49be562eb3ead0cd557d3c811304f84e808
Hu1GM <mgh_1202@163.com>
2022-10-09 16:59:47 +0800

    version_name

--- ACL_PyTorch/contrib/cv/gan/StarGAN/pth2om.sh
+++ ACL_PyTorch/contrib/cv/gan/StarGAN/pth2om.sh
@@ -16,19 +16,19 @@ export ASCEND_OPP_PATH=${install_path}/opp
 export ASCEND_AICPU_PATH=/usr/local/Ascend/ascend-toolkit/latest
 
 atc --framework=5 --model=StarGAN.onnx --output=StarGAN_bs1 --input_format=NCHW \
-    --input_shape="real_img:1,3,128,128;attr:1,5" --log=debug --soc_version=Ascend710 
+    --input_shape="real_img:1,3,128,128;attr:1,5" --log=debug --soc_version=Ascend310P3 
 
 atc --framework=5 --model=StarGAN.onnx --output=StarGAN_bs4 --input_format=NCHW \
-    --input_shape="real_img:4,3,128,128;attr:4,5" --log=debug --soc_version=Ascend710 
+    --input_shape="real_img:4,3,128,128;attr:4,5" --log=debug --soc_version=Ascend310P3 
 
 atc --framework=5 --model=StarGAN.onnx --output=StarGAN_bs8 --input_format=NCHW \
-    --input_shape="real_img:8,3,128,128;attr:8,5" --log=debug --soc_version=Ascend710 
+    --input_shape="real_img:8,3,128,128;attr:8,5" --log=debug --soc_version=Ascend310P3
 
 atc --framework=5 --model=StarGAN.onnx --output=StarGAN_bs16 --input_format=NCHW \
-    --input_shape="real_img:16,3,128,128;attr:16,5" --log=debug --soc_version=Ascend710 
+    --input_shape="real_img:16,3,128,128;attr:16,5" --log=debug --soc_version=Ascend310P3
 
 atc --framework=5 --model=StarGAN.onnx --output=StarGAN_bs32 --input_format=NCHW \
-    --input_shape="real_img:32,3,128,128;attr:32,5" --log=debug --soc_version=Ascend710 
+    --input_shape="real_img:32,3,128,128;attr:32,5" --log=debug --soc_version=Ascend310P3
 
 atc --framework=5 --model=StarGAN.onnx --output=StarGAN_bs64 --input_format=NCHW \
-    --input_shape="real_img:64,3,128,128;attr:64,5" --log=debug --soc_version=Ascend710 
+    --input_shape="real_img:64,3,128,128;attr:64,5" --log=debug --soc_version=Ascend310P3
