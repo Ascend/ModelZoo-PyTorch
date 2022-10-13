d2ec503296d355f06fbedd336141fc40a2d35e58
wuxiankun <wuxiankun@huawei.com>
2022-10-13 03:20:13 +0000

    !2148 【pytorch】【built-in】修复之前prbug，R2U-net 8p打屏问题 * fix before pr gitee bug, remove blank

--- PyTorch/built-in/cv/semantic_segmentation/R2U_Net_for_PyTorch/test/train_performance_8p.sh
+++ PyTorch/built-in/cv/semantic_segmentation/R2U_Net_for_PyTorch/test/train_performance_8p.sh
@@ -108,7 +108,7 @@ e2e_time=$(( $end_time - $start_time ))
 echo "------------------ Final result ------------------"
 #输出性能FPS，需要模型审视修改
 FPS=`grep -a 'FPS'  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|grep FPS|awk -F "FPS: " '{print $NF}'|awk 'NR==1{max=$1;next}{max=max>$1?max:$1}END{print max}'`
-FinalFPS = `awk -v x="$FPS" -v y="$RANK_SIZE" 'BEGIN{printf "%.3f\n", x*y}'`
+FinalFPS=`awk -v x="$FPS" -v y="$RANK_SIZE" 'BEGIN{printf "%.3f\n", x*y}'`
 #打印，不需要修改
 echo "Final Performance images/sec : $FinalFPS"
 
