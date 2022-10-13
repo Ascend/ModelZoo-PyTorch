c23ca039783aa62377e74ec282520211fd0246d5
迎接光辉岁月 <raojiayi@huawei.com>
2022-10-13 06:25:05 +0000

    !2150 update ACL_PyTorch/contrib/cv/classfication/Shufflenetv1/test/pth2om.sh. * update ACL_PyTorch/contrib/cv/classfication/Shufflenetv1/test/pth2om.sh.

--- ACL_PyTorch/contrib/cv/classfication/Shufflenetv1/test/pth2om.sh
+++ ACL_PyTorch/contrib/cv/classfication/Shufflenetv1/test/pth2om.sh
@@ -1,8 +1,8 @@
 source /usr/local/Ascend/ascend-toolkit/set_env.sh
 rm -rf shufflenetv1_bs1.onnx
 rm -rf shufflenetv1_bs16.onnx
-python3.7 shufflenetv1_pth2onnx_bs1.py 1.0x.pth.tar shufflenetv1_bs1.onnx
-python3.7 shufflenetv1_pth2onnx_bs16.py 1.0x.pth.tar shufflenetv1_bs16.onnx
+python3.7 shufflenetv1_pth2onnx_bs1.py 1.0x.pth.tar shufflenetv1_bs1.onnx 1
+python3.7 shufflenetv1_pth2onnx_bs16.py 1.0x.pth.tar shufflenetv1_bs16.onnx 16
 rm -rf shufflenetv1_bs1.om shufflenetv1_bs16.om
 atc --framework=5 --model=./shufflenetv1_bs1.onnx --input_format=NCHW --input_shape="image:1,3,224,224" --output=shufflenetv1_bs1 --log=debug --soc_version=$1
 atc --framework=5 --model=./shufflenetv1_bs16.onnx --input_format=NCHW --input_shape="image:16,3,224,224" --output=shufflenetv1_bs16 --log=debug --soc_version=$1
