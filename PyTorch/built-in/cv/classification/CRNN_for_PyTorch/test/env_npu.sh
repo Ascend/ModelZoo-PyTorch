69c578b76a567b3ec73802b2e17aec1cfb3d5306
魏鹏飞 <weipengfei19@huawei.com>
2022-10-17 04:34:09 +0000

    !2184 [自研][PyTorch]CRNN_for_PyTorch模型优化环境变量 !2184 [自研][PyTorch]CRNN_for_PyTorch模型优化环境变量

--- PyTorch/built-in/cv/classification/CRNN_for_PyTorch/test/env_npu.sh
+++ PyTorch/built-in/cv/classification/CRNN_for_PyTorch/test/env_npu.sh
@@ -22,7 +22,8 @@ msnpureport -g error -d 4
 msnpureport -g error -d 5
 msnpureport -g error -d 6
 msnpureport -g error -d 7
-
+#关闭Device侧Event日志
+msnpureport -e disable
 
 #将Host日志输出到串口,0-关闭/1-开启
 export ASCEND_SLOG_PRINT_TO_STDOUT=0
@@ -40,10 +41,7 @@ export COMBINED_ENABLE=1
 export DYNAMIC_OP="ADD#MUL"
 #HCCL白名单开关,1-关闭/0-开启
 export HCCL_WHITELIST_DISABLE=1
-#设置Device侧日志等级为error
-${install_path}/driver/tools/msnpureport -g error
-#关闭Device侧Event日志
-${install_path}/driver/tools/msnpureport -e disable
+
 
 
 path_lib=$(python3.7 -c """
