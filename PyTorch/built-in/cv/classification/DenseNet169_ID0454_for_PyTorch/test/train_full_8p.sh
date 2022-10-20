d1cfa08dea30be7145bda3dd41e08a04b6af3316
dongwenbo6 <dongwenbo6@huawei.com>
2022-10-20 11:51:48 +0000

    !2228 [自研][PyTorch][DenseNet169_for_PyTorch]-修改训练脚本，FPS取值设为平均值 * FPS取值修改为平均值 * FPS

--- PyTorch/built-in/cv/classification/DenseNet169_ID0454_for_PyTorch/test/train_full_8p.sh
+++ PyTorch/built-in/cv/classification/DenseNet169_ID0454_for_PyTorch/test/train_full_8p.sh
@@ -107,7 +107,7 @@ e2e_time=$(( $end_time - $start_time ))
 #结果打印，不需要修改
 echo "------------------ Final result ------------------"
 #输出性能FPS，需要模型审视修改
-FPS=`grep -a 'Epoch:'  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|grep eta:|awk -F "img/s: " '{print $NF}'|awk 'NR==1{max=$1;next}{max=max>$1?max:$1}END{print max}'`
+FPS=`grep -a 'img/s:'  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log | tail -n 101| head -n 100 | awk '{print$9}'| awk '{sum+=$1} END {print sum/NR}'`
 
 #输出训练精度,需要模型审视修改
 train_accuracy=`grep -a '* Acc@1' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk 'END {print}'|awk -F "Acc@1" '{print $NF}'|awk -F " " '{print $1}'`
