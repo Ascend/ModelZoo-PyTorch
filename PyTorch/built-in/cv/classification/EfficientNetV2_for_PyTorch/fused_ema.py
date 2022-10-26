5fd0eb9df72668f614015bd1ca35bfe89fa1f069
lj_new <lijing202@huawei.com>
2022-10-26 08:48:43 +0000

    !2169 [自研][PyTorch] EfficientNetV2模型上库 * 增加readme * fix eval metric * performance script * metric * performance script * init * init efficientnetv2 * init efficientnetv2 * Merge https://gitee.com/li_jing_hw/ModelZoo-PyTorch into master * 修复部分arm设备上的性能问题

---
+++ PyTorch/built-in/cv/classification/EfficientNetV2_for_PyTorch/fused_ema.py
@@ -0,0 +1,93 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from copy import deepcopy
+import torch
+
+class ModelEmaV2Npu(torch.nn.Module):
+    """ Model Exponential Moving Average V2
+    Keep a moving average of everything in the model state_dict (parameters and buffers).
+    V2 of this module is simpler, it does not match params/buffers based on name but simply
+    iterates in order. It works with torchscript (JIT of full model).
+    This is intended to allow functionality like
+    https://www.tensorflow.org/api_docs/python/tf/train/ExponentialMovingAverage
+    A smoothed version of the weights is necessary for some training schemes to perform well.
+    E.g. Google's hyper-params for training MNASNet, MobileNet-V3, EfficientNet, etc that use
+    RMSprop with a short 2.4-3 epoch decay period and slow LR decay rate of .96-.99 requires EMA
+    smoothing of weights to match results. Pay attention to the decay constant you are using
+    relative to your update count per epoch.
+    To keep EMA from using GPU resources, set device='cpu'. This will save a bit of memory but
+    disable validation of the EMA weights. Validation will have to be done manually in a separate
+    process, or after the training stops converging.
+    This class is sensitive where it is initialized in the sequence of model init,
+    GPU assignment and distributed training wrappers.
+    """
+    def __init__(self, model, decay=0.9999, device=None):
+        super(ModelEmaV2Npu, self).__init__()
+        # make a copy of the model for accumulating moving average of weights
+        self.module = deepcopy(model)
+        self.module.eval()
+        self.decay = decay
+        self.device = device  # perform ema on different device from model if set
+        if self.device is not None:
+            self.module.to(device=device)
+        self.is_fused = False
+
+    def _update(self, model, update_fn):
+        with torch.no_grad():
+            for ema_v, model_v in zip(self.module.state_dict().values(), model.state_dict().values()):
+                if self.device is not None:
+                    model_v = model_v.to(device=self.device)
+                ema_v.copy_(update_fn(ema_v, model_v))
+
+    def update(self, model, model_params_fused):
+        with torch.no_grad():
+            if not self.is_fused:
+                from apex.contrib.combine_tensors import combine_npu
+                decay = []
+                no_decay = []
+                for name, param in self.module.named_parameters():
+                    if not param.requires_grad:
+                        continue
+                    if len(param.shape) == 1 or name.endswith(".bias"):
+                        no_decay.append(param)
+                    else:
+                        decay.append(param)
+                ema_all_params = no_decay + decay
+                self.ema_params_fused = combine_npu(ema_all_params)
+
+                ema_all_buffers = []
+                for name, b in self.module.named_buffers():
+                    if b.dtype.is_floating_point:
+                        ema_all_buffers.append(b)
+                    else:
+                        continue
+                self.ema_buffers_fused = combine_npu(ema_all_buffers)
+
+                model_all_buffers = []
+                for name, b in model.named_buffers():
+                    if b.dtype.is_floating_point:
+                        model_all_buffers.append(b)
+                    else:
+                        continue
+                self.model_buffers_fused = combine_npu(model_all_buffers)
+                self.is_fused = True
+            
+            self.ema_params_fused *= self.decay
+            self.ema_params_fused.add_(model_params_fused, alpha=1 - self.decay)
+
+            self.ema_buffers_fused *= self.decay
+            self.ema_buffers_fused.add_(self.model_buffers_fused, alpha=1 - self.decay)           
+
+    def set(self, model):
+        self._update(model, update_fn=lambda e, m: m)
---
+++ PyTorch/built-in/cv/classification/EfficientNetV2_for_PyTorch/test/train_performance_8p.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+#规避环境变量冲突
+if [ -f /usr/local/Ascend/bin/setenv.bash ];then
+    unset PYTHONPATH
+    source /usr/local/Ascend/bin/setenv.bash  
+fi
+#集合通信参数,不需要修改
+export RANK_SIZE=8
+RANK_ID_START=0
+
+# 数据集路径,保持为空,不需要修改
+data_path=""
+device_id=0
+# 训练周期
+epochs=1
+#网络名称,同目录名称,需要模型审视修改
+Network="EfficientNetV2_for_Pytorch"
+
+#训练batch_size,,需要模型审视修改
+batch_size=256
+
+#参数校验，不需要修改
+for para in $*
+do
+    if [[ $para == --data_path* ]];then
+        data_path=`echo ${para#*=}`
+    elif [[ $para == --epochs* ]];then
+        epochs=`echo ${para#*=}`
+    fi
+done
+
+#校验是否传入data_path,不需要修改
+if [[ $data_path == "" ]];then
+    echo "[Error] para \"data_path\" must be confing"
+    exit 1
+fi
+
+cur_path=`pwd`
+cur_path_last_dirname=${cur_path##*/}
+if [ x"${cur_path_last_dirname}" == x"test" ];then
+    test_path_dir=${cur_path}
+    cd ..
+    cur_path=`pwd`
+else
+    test_path_dir=${cur_path}/test
+fi
+
+if [ $ASCEND_DEVICE_ID ];then
+    echo "device id is ${ASCEND_DEVICE_ID}"
+elif [ ${device_id} ];then
+    export ASCEND_DEVICE_ID=${device_id}
+    echo "device id is ${ASCEND_DEVICE_ID}"
+else
+    "[Error] device id must be config"
+    exit 1
+fi
+
+#训练开始时间，不需要修改
+start_time=$(date +%s)
+
+#进入训练脚本目录，需要模型审视修改
+
+
+# 非平台场景时source 环境变量
+check_etp_flag=`env | grep etp_running_flag`
+etp_flag=`echo ${check_etp_flag#*=}`
+if [ x"${etp_flag}" != x"true" ];then
+    echo "source"
+    source ${test_path_dir}/env_npu.sh
+else
+   #修改参数
+    sed -i "s|pass|break|g"  $cur_path/references/classification/utils.py
+fi
+
+
+#################创建日志输出目录，不需要修改#################
+if [ -d ${test_path_dir}/output/${ASCEND_DEVICE_ID} ];then
+    rm -rf ${test_path_dir}/output/${ASCEND_DEVICE_ID}
+    mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+else
+    mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+fi
+
+nohup bash distributed_train.sh 8 \
+    $data_path \
+    --model efficientnetv2_rw_t \
+    -b $batch_size \
+    --sched step \
+    --epochs ${epochs} \
+    --decay-epochs 2.4 \
+    --decay-rate .97 \
+    --opt rmsproptf \
+    --opt-eps .001 \
+    -j 8 \
+    --warmup-lr 4e-6 \
+    --weight-decay 1e-5 \
+    --drop 0.3 \
+    --drop-connect 0.2 \
+    --aa rand-m9-mstd0.5 \
+    --lr .112 \
+    --log-interval 1 \
+    --img-size 288 \
+    --apex-amp \
+    --model-ema \
+    --model-ema-decay 0.9999 \
+    --perf-steps 100 \
+    --mixup 0.2 > ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
+wait
+
+#8p情况下仅0卡(主节点)有完整日志,因此后续日志提取仅涉及0卡
+ASCEND_DEVICE_ID=0
+
+#训练结束时间，不需要修改
+end_time=$(date +%s)
+e2e_time=$(( $end_time - $start_time ))
+
+#参数回改
+sed -i "s|break|pass|g"  $cur_path/references/classification/utils.py
+wait
+
+#结果打印，不需要修改
+echo "------------------ Final result ------------------"
+#输出性能FPS，需要模型审视修改
+FPS=`grep -a 'Train:'  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk 'END {print}'|awk -F "/s" '{print $2}'|awk '{print $2}'`
+
+#打印，不需要修改
+echo "E2E Training Duration sec : $e2e_time"
+
+#性能看护结果汇总
+#训练用例信息，不需要修改
+BatchSize=${batch_size}
+DeviceType=`uname -m`
+CaseName=${Network}_bs${BatchSize}_${RANK_SIZE}'p'_'perf'
+
+##获取性能数据，不需要修改
+#吞吐量
+ActualFPS=$FPS
+#单迭代训练时长
+TrainingTime=`awk 'BEGIN{printf "%.2f\n", '${batch_size}'*1000/'${FPS}'}'`
+
+#打印，不需要修改
+echo "Final Performance images/sec : $ActualFPS"
+
+#从train_$ASCEND_DEVICE_ID.log提取Loss到train_${CaseName}_loss.txt中，需要模型审视修改
+grep Train: ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log|awk -F "Loss: " '{print $NF}' | awk -F " " '{print $1}' > ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt
+
+#最后一个迭代loss值，不需要修改
+ActualLoss=`awk 'END {print}' ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt`
+
+#关键信息打印到${CaseName}.log中，不需要修改
+echo "Network = ${Network}" > ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RankSize = ${RANK_SIZE}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "BatchSize = ${BatchSize}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "DeviceType = ${DeviceType}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "CaseName = ${CaseName}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualFPS = ${ActualFPS}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "TrainingTime = ${TrainingTime}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualLoss = ${ActualLoss}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "E2ETrainingTime = ${e2e_time}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
