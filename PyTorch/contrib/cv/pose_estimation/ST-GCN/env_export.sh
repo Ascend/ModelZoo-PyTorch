399493173573c7fbc57cb40df2b7e93db027a93e
唐红艳 <1349905607@qq.com>
2022-10-28 06:10:32 +0000

    !2335 高性能预训练模型 seresnet50 和 ST-GCN 回归问题修改 !2335 高性能预训练模型 seresnet50 和 ST-GCN 回归问题修改

--- PyTorch/contrib/cv/pose_estimation/ST-GCN/env_export.sh
+++ PyTorch/contrib/cv/pose_estimation/ST-GCN/env_export.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright 2022 Huawei Technologies Co., Ltd
+
+# Copyright(C) 2022 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ============================================================================
 
-air_path=$1
-om_path=$2
-
-echo "Input AIR file path: ${onnx_path}"
-echo "Output OM file path: ${om_path}"
-
-atc --framework=5 --model="${onnx_path}" \
-    --output="${om_path}" \
-    --enable_small_channel=1 \
-    --soc_version=Ascend310 \
-    --op_select_implmode="high_precision"
-
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
