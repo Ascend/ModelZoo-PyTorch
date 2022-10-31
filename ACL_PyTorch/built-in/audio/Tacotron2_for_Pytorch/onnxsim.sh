532850a88dcb72b17c9d7c090abac95c1c13689f
彭奥奥 <pengaoao@huawei.com>
2022-10-31 11:48:19 +0000

    !2256 Tacotron2 onxsim.sh fix !2256 Tacotron2 onxsim.sh fix

--- ACL_PyTorch/built-in/audio/Tacotron2_for_Pytorch/onnxsim.sh
+++ ACL_PyTorch/built-in/audio/Tacotron2_for_Pytorch/onnxsim.sh
@@ -3,4 +3,4 @@ output_model=$2
 batch_size=$3
 seq_len=$4
 python3 -m onnxsim --input-shape "decoder_input:${batch_size},80" "attention_hidden:${batch_size},1024" "attention_cell:${batch_size},1024" "decoder_hidden:${batch_size},1024" "decoder_cell:${batch_size},1024" "attention_weights:${batch_size},${seq_len}" "attention_weights_cum:${batch_size},${seq_len}" "attention_context:${batch_size},512" \
-    "memory:${batch_size},${seq_len},512" "processed_memory:${batch_size},${seq_len},128" "mask:${batch_size},${seq_len}" "gate_output_input:${batch_size},1,1" "mel_output_input:${batch_size},80,1" "not_finished_input:${batch_size}" "mel_lengths_input:${batch_size}" --dynamic-input-shape $input_model $output_model
+    "memory_out:${batch_size},${seq_len},512" "processed_memory_out:${batch_size},${seq_len},128" "mask_out:${batch_size},${seq_len}" "gate_output_input:${batch_size},1,1" "mel_output_input:${batch_size},80,1" "not_finished_input:${batch_size}" "mel_lengths_input:${batch_size}" --dynamic-input-shape $input_model $output_model
