a88dc25c0a9b8864c1106f2ade0cabc93b1525cf
谢婷 <847284173@qq.com>
2022-10-31 01:08:29 +0000

    !1979 【中国海洋大学】【高校贡献】【MindX高性能预训练模型】【densenet201】-初次提交 !1979 【中国海洋大学】【高校贡献】【MindX高性能预训练模型】【densenet201】-初次提交

---
+++ PyTorch/built-in/cv/classification/DenseNet201_ID0453_for_PyTorch/modelarts/train_start.py
@@ -0,0 +1,421 @@
+# Copyright (c) Soumith Chintala 2016,
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+import datetime
+import os
+import time
+import sys
+import torch
+import torch.utils.data
+from torch import nn
+import torchvision
+from torchvision import transforms
+import torch.npu
+import random
+import numpy as np
+import utils
+
+# ---------modelarts modification-----------------
+import torchvision.models as models
+import moxing as mox
+import torch.onnx
+from collections import OrderedDict
+real_path = '/cache/data_url'
+directory = '/cache/training'
+# ---------modelarts modification end-------------
+
+try:
+    import apex
+    from apex import amp
+except ImportError:
+    amp = None
+
+
+def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, args):
+    model.train()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value}'))
+    metric_logger.add_meter('img/s', utils.SmoothedValue(window_size=10, fmt='{value}'))
+
+    header = 'Epoch: [{}]'.format(epoch)
+    for image, target in metric_logger.log_every(data_loader, args, header):
+        start_time = time.time()
+        image, target = image.to(device), target.to(torch.int).to(device)
+        output = model(image)
+        loss = criterion(output, target)
+
+        optimizer.zero_grad()
+        if args.apex:
+            with amp.scale_loss(loss, optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
+        optimizer.step()
+
+        acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
+        batch_size = image.shape[0]
+        metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
+        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+        metric_logger.meters['img/s'].update(batch_size / (time.time() - start_time))
+
+
+def evaluate(model, criterion, data_loader, device):
+    model.eval()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Test:'
+    with torch.no_grad():
+        for image, target in metric_logger.log_every(data_loader, args, header):
+            image = image.to(device, non_blocking=True)
+            target = target.to(torch.int).to(device, non_blocking=True)
+            output = model(image)
+            loss = criterion(output, target)
+
+            acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
+            # FIXME need to take into account that the datasets
+            # could have been padded in distributed setup
+            batch_size = image.shape[0]
+            metric_logger.update(loss=loss.item())
+            metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+            metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes(device)
+
+    print(' * Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f}'
+          .format(top1=metric_logger.acc1, top5=metric_logger.acc5))
+    return metric_logger.acc1.global_avg
+
+
+def _get_cache_path(filepath):
+    import hashlib
+    h = hashlib.sha1(filepath.encode()).hexdigest()
+    cache_path = os.path.join("~", ".torch", "vision", "datasets", "imagefolder", h[:10] + ".pt")
+    cache_path = os.path.expanduser(cache_path)
+    return cache_path
+
+
+def main(args):
+    #设置环境变量
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29688'
+
+    #设置seed
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    os.environ['PYTHONHASHSEED'] = str(args.seed)
+
+    if args.apex:
+        if sys.version_info < (3, 0):
+            raise RuntimeError("Apex currently only supports Python 3. Aborting.")
+        if amp is None:
+            raise RuntimeError("Failed to import apex. Please install apex from https://www.github.com/nvidia/apex "
+                               "to enable mixed-precision training.")
+
+    args.output_dir = directory
+    if args.output_dir:
+        utils.mkdir(args.output_dir)
+
+    if args.distributed:
+        utils.init_distributed_mode(args)
+
+    args.is_master_node = not args.distributed or args.device_id == 0
+    if args.is_master_node:
+        print(args)
+
+    device = torch.device(f'npu:{args.device_id}')
+    torch.npu.set_device(device)
+
+    # Data loading code
+    print("Loading data")
+
+    # ---------------modelarts modification-----------------
+    if not os.path.exists(real_path):
+        os.makedirs(real_path)
+    mox.file.copy_parallel(args.data_url, real_path)
+    print("training data finish copy to %s." % real_path)
+    # --------------modelarts modification end--------------
+
+    traindir = os.path.join(real_path, 'train')
+    valdir = os.path.join(real_path, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    print("Loading training data")
+    st = time.time()
+    cache_path = _get_cache_path(traindir)
+    if args.cache_dataset and os.path.exists(cache_path):
+        # Attention, as the transforms are also cached!
+        print("Loading dataset_train from {}".format(cache_path))
+        dataset, _ = torch.load(cache_path)
+    else:
+        dataset = torchvision.datasets.ImageFolder(
+            traindir,
+            transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                normalize,
+            ]))
+        if args.is_master_node and args.cache_dataset:
+            print("Saving dataset_train to {}".format(cache_path))
+            utils.mkdir(os.path.dirname(cache_path))
+            utils.save_on_master((dataset, traindir), cache_path)
+    print("Took", time.time() - st)
+
+    print("Loading validation data")
+    cache_path = _get_cache_path(valdir)
+    if args.cache_dataset and os.path.exists(cache_path):
+        # Attention, as the transforms are also cached!
+        print("Loading dataset_test from {}".format(cache_path))
+        dataset_test, _ = torch.load(cache_path)
+    else:
+        dataset_test = torchvision.datasets.ImageFolder(
+            valdir,
+            transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                normalize,
+            ]))
+        if args.is_master_node and args.cache_dataset:
+            print("Saving dataset_test to {}".format(cache_path))
+            utils.mkdir(os.path.dirname(cache_path))
+            utils.save_on_master((dataset_test, valdir), cache_path)
+
+    print("Creating data loaders")
+    if args.distributed:
+        train_sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+        test_sampler = torch.utils.data.distributed.DistributedSampler(dataset_test)
+    else:
+        train_sampler = torch.utils.data.RandomSampler(dataset)
+        test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=args.batch_size,
+        sampler=train_sampler, num_workers=args.workers, pin_memory=True)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=args.batch_size,
+        sampler=test_sampler, num_workers=args.workers, pin_memory=True)
+
+    if args.is_master_node:
+        print("Creating model")
+        print(torchvision.models.__dict__)
+    model = models.densenet201(pretrained=False)
+    pre = torch.load(args.model)
+    model.load_state_dict(pre, strict=False)
+
+    if args.pretrained:
+        checkpoint = torch.load(args.pretrained_weight_path, map_location='cpu')
+        if 'module.' in list(checkpoint['model'].keys())[0]:
+            checkpoint['model'] = {k.replace('module.', ''): v for k, v in checkpoint['model'].items()}
+        checkpoint['model'].pop('classifier.weight')
+        checkpoint['model'].pop('classifier.bias')
+        model = model.load_state_dict(checkpoint['model'], strict=False)
+    model = model.to(device)
+    if args.distributed and args.sync_bn:
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer = apex.optimizers.NpuFusedSGD(
+        model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+    if args.apex:
+        model, optimizer = amp.initialize(model, optimizer,
+                                          opt_level=args.apex_opt_level,
+                                          loss_scale=args.loss_scale_value,
+                                          combine_grad=True)
+
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_step_size, gamma=args.lr_gamma)
+
+    model_without_ddp = model
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.device_id])
+        model_without_ddp = model.module
+
+    if args.resume:
+        checkpoint = torch.load(args.resume, map_location='cpu')
+        model_without_ddp.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        lr_scheduler.load_state_dict(checkpoint['lr_scheduler'])
+        args.start_epoch = checkpoint['epoch'] + 1
+
+    if args.test_only:
+        evaluate(model, criterion, data_loader_test, device=device)
+        return
+
+    if args.is_master_node:
+        print("Start training")
+    start_time = time.time()
+    for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed:
+            train_sampler.set_epoch(epoch)
+        train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, args)
+        lr_scheduler.step()
+        evaluate(model, criterion, data_loader_test, device=device)
+        if args.is_master_node and args.output_dir:
+            checkpoint = {
+                'model': model_without_ddp.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'lr_scheduler': lr_scheduler.state_dict(),
+                'epoch': epoch,
+                'args': args}
+            utils.save_on_master(
+                checkpoint,
+                os.path.join(args.output_dir, 'model_{}.pth'.format(epoch)))
+            # ---------modelarts modification end-------------
+            filename = os.path.join(args.output_dir, 'checkpoint201.pth')
+            utils.save_on_master(
+                checkpoint,
+                filename)
+            onnx_name = '201_npu.onnx'
+            onnx_file = os.path.join(directory, onnx_name)
+            convert(filename, onnx_file)
+            print("pth model to onnx success")
+            mox.file.copy_parallel(directory, args.train_url)
+            print("final success")
+            # ---------modelarts modification end-------------
+            
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    if args.is_master_node:
+        print('Training time {}'.format(total_time_str))
+
+
+# ---------------modelarts modification-----------------
+def proc_nodes_module(checkpoint, AttrName):
+    new_state_dict = OrderedDict()
+    for k, v in checkpoint[AttrName].items():
+        if (k[0:7] == "module."):
+            name = k[7:]
+        else:
+            name = k[0:]
+        new_state_dict[name] = v
+    return new_state_dict
+
+
+def convert(pth_file, onnx_file):
+    model = models.densenet201(pretrained=False)
+    checkpoint = torch.load(pth_file, map_location='cpu')
+    checkpoint['model'] = proc_nodes_module(checkpoint, 'model')
+    model.load_state_dict(checkpoint['model'])
+    model.eval()
+    input_names = ["actual_input_1"]
+    output_names = ["output1"]
+    dummy_input = torch.randn(1, 3, 224, 224)
+    torch.onnx.export(model, dummy_input, onnx_file, input_names=input_names, output_names=output_names, opset_version=11)
+# ---------------modelarts modification-----------------
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='PyTorch Classification Training')
+
+    parser.add_argument('--data-path', default='', help='dataset')
+    parser.add_argument('--model', default='/home/work/user-job-dir/code/densenet201-c1103571.pth', help='model')
+    parser.add_argument('--device_id', default=0, type=int, help='device id')
+    parser.add_argument('-b', '--batch-size', default=128, type=int)
+    parser.add_argument('--epochs', default=1, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('-j', '--workers', default=16, type=int, metavar='N',
+                        help='number of data loading workers (default: 16)')
+    parser.add_argument('--lr', default=0.1, type=float, help='initial learning rate')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    parser.add_argument('--lr-step-size', default=30, type=int, help='decrease lr every step-size epochs')
+    parser.add_argument('--lr-gamma', default=0.1, type=float, help='decrease lr by a factor of lr-gamma')
+    parser.add_argument('--print-freq', default=1, type=int, help='print frequency')
+    parser.add_argument('--output-dir', default='.', help='path where to save')
+    parser.add_argument('--resume', default='', help='resume from checkpoint')
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument(
+        "--cache-dataset",
+        dest="cache_dataset",
+        help="Cache the datasets for quicker initialization. It also serializes the transforms",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--sync-bn",
+        dest="sync_bn",
+        help="Use sync batch norm",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--test-only",
+        dest="test_only",
+        help="Only test the model",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--pretrained",
+        dest="pretrained",
+        help="Use pre-trained models from the modelzoo",
+        action="store_true",
+    )
+    parser.add_argument("--num_classes", default=1000, type=int, help='num of classes')
+    parser.add_argument('--pretrained_weight_path', default='', help='pretrained weight path')
+
+    # Mixed precision training parameters
+    parser.add_argument('--apex', default=True, action='store_true',
+                        help='Use apex for mixed precision training')
+    parser.add_argument('--apex-opt-level', default='O2', type=str,
+                        help='For apex mixed precision training'
+                             'O0 for FP32 training, O1 for mixed precision training.'
+                             'For further detail, see https://github.com/NVIDIA/apex/tree/master/examples/imagenet'
+                        )
+    parser.add_argument('--loss_scale_value',
+                        default=1024,
+                        type=int,
+                        help='set loss scale value.')
+
+    # distributed training parameters
+    parser.add_argument('--world-size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--dist-url', default='env://', help='url used to set up distributed training')
+    parser.add_argument('--distributed',
+                        action='store_true',
+                        help='Use multi-processing distributed training to launch '
+                        'N processes per node, which has N GPUs.')
+    parser.add_argument('--dist_rank',
+                        default=0,
+                        type=int,
+                        help='node rank for distributed training')
+    parser.add_argument('--seed',
+                        default=1234,
+                        type=int,
+                        help='Manually set random seed')
+
+    # -----------modelarts modification-----------------
+    parser.add_argument('--data_url', metavar='DIR', default='',
+                        help='path to dataset')
+    parser.add_argument('--train_url', metavar='DIR', default='',
+                        help='path to pth/onnx')
+    parser.add_argument('--onnx', default=True, help="convert pth model to onnx")
+    # -----------modelarts modification end-------------
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
