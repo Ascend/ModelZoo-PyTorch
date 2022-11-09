f69945a95367495f38e187c18a1b6f83bf4e29f9
陈翔 <chenxiang116@huawei.com>
2022-11-09 06:46:26 +0000

    !2519 [PyTorch][built-in][DynamicUNet_for_Pytorch]修改功能报错 !2519 [PyTorch][built-in][DynamicUNet_for_Pytorch]修改功能报错

--- PyTorch/built-in/cv/semantic_segmentation/DynamicUNet_for_Pytorch/runner.py
+++ PyTorch/built-in/cv/semantic_segmentation/DynamicUNet_for_Pytorch/runner.py
@@ -50,7 +50,7 @@ np.random.seed(seed)
 torch.manual_seed(seed)
 
 class NoProfiling(object):
-    def __enter_(self):
+    def __enter__(self):
         pass
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
