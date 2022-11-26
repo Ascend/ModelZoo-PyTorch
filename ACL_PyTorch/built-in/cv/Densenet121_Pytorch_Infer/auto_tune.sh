a7f426871d1fb2923a763a98a0bcbe2c8a2cc67d
柯丹宁 <kedanning@huawei.com>
2022-11-26 08:56:53 +0000

    !2635 [ACL_Pytorch][built-in][cv][Densene121]代码及资料整改 * fix for review * fix densenet121

--- ACL_PyTorch/built-in/cv/Densenet121_Pytorch_Infer/auto_tune.sh
+++
@@ -1,9 +0,0 @@
-source /usr/local/Ascend/ascend-toolkit/set_env.sh
-export SLOG_PRINT_TO_STDOUT=1
-# export REPEAT_TUNE=True
-
-atc --model=$1 --framework=5 --output=$2 --input_format=NCHW --input_shape="actual_input_1:16,3,224,224" --enable_small_channel=1 --log=info --soc_version=Ascend310
-
-# --input_fp16_nodes="actual_input_1"
-# --enable_small_channel=1
-# --auto_tune_mode="RL,GA"
---
+++ ACL_PyTorch/built-in/cv/Densenet121_Pytorch_Infer/densenet121_pth2onnx.py
@@ -0,0 +1,55 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
+import sys
+
+import torch
+import torchvision
+
+def pth2onnx(input_file, output_file):
+    model = torchvision.models.densenet121()
+    pattern = re.compile(
+        r'^(.*denselayer\d+\.(?:norm|relu|conv))\.((?:[12])\.(?:weight|bias|running_mean|running_var))$'
+        )
+    state_dict = torch.load(input_file, map_location='cpu')      
+    for key in list(state_dict.keys()):      
+        res = pattern.match(key)
+        if res:
+            new_key = res.group(1) + res.group(2)
+            state_dict[new_key] = state_dict[key]
+            del state_dict[key]
+    model.load_state_dict(state_dict)
+    model.eval()
+
+    dummy_input = torch.randn(1, 3, 224, 224)
+    input_names = ['input_0']
+    output_names = ['output_0']
+    dynamic_axes = {'input_0':{0:"-1"}, 'output_0':{0:"-1"}}
+
+    torch.onnx.export(
+        model,
+        dummy_input,
+        output_file,
+        input_names=input_names,
+        output_names=output_names,
+        dynamic_axes=dynamic_axes,
+        verbose=False,
+        opset_version=11
+    )
+
+if __name__=="__main__":
+    pth_file = sys.argv[1]
+    onnx_file = sys.argv[2]
+    pth2onnx(pth_file, onnx_file)
