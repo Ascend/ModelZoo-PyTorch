dcd64a03067656fedc7d85b02c2954e60de668fc
柯丹宁 <kedanning@huawei.com>
2022-11-26 08:56:02 +0000

    !2634 [ACL_Pytorch][built-in][cv][ShufflenetV2]代码及资料整改 * fix for review * fix shufflenetv2

--- ACL_PyTorch/built-in/cv/Shufflenetv2_for_Pytorch/auto_tune.sh
+++
@@ -1,4 +0,0 @@
-source /usr/local/Ascend/ascend-toolkit/set_env.sh
-# export REPEAT_TUNE=True
-
-atc --model=$1 --framework=5 --output=./shufflenetV2 --input_format=NCHW --input_shape="actual_input_1:1,3,224,224" --log=info --soc_version=Ascend310 --auto_tune_mode="RL,GA"
