db23e3a30704c6df5ebcc9c5ab25ea674c55d192
陈睿敏 <chenruimin1@huawei.com>
2022-11-30 12:10:21 +0000

    !2821 [自研][PyTorch] YOLOV4_eb5f166_for_PyTorch上库 !2821 [自研][PyTorch] YOLOV4_eb5f166_for_PyTorch上库

---
+++ PyTorch/built-in/cv/detection/YOLOV4_eb5f166_for_PyTorch/test/train_full_8p.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+#网络名称,同目录名称,需要模型审视修改
+Network="yolov4"
+
+cur_path=`pwd`
+model_name=yolov4-pacsp
+batch_size=256
+
+# 参数校验，data_path为必传参数，其他参数的增删由模型自身决定；此处新增参数需在上面有定义并赋值
+for para in $*
+do
+    if [[ $para == --data_path* ]];then
+        data_path=`echo ${para#*=}`
+    fi
+done
+
+# 校验是否传入data_path,不需要修改
+if [[ $data_path == "" ]];then
+    echo "[Error] para \"data_path\" must be confing"
+    exit 1
+fi
+
+ln -snf $data_path ./
+
+# 校验是否指定了device_id,分动态分配device_id与手动指定device_id,此处不需要修改
+ASCEND_DEVICE_ID=0
+echo "device id is ${ASCEND_DEVICE_ID}"
+
+source ${cur_path}/test/env_npu.sh
+
+#训练开始时间，不需要修改
+start_time=$(date +%s)
+echo "start_time: ${start_time}"
+
+if [ -d ${cur_path}/test/output/${ASCEND_DEVICE_ID} ];
+then
+	rm -rf ${cur_path}/test/output/${ASCEND_DEVICE_ID}
+	mkdir -p ${cur_path}/test/output/${ASCEND_DEVICE_ID}
+else
+	mkdir -p ${cur_path}/test/output/${ASCEND_DEVICE_ID}
+fi
+
+export MASTER_ADDR=127.0.0.1
+export MASTER_PORT=29500
+export WORLD_SIZE=8
+KERNEL_NUM=$(($(nproc)/8))
+for i in $(seq 0 7)
+do 
+export RANK=$i
+let p_start=$((KERNEL_NUM * i))
+let p_end=$((p_start + KERNEL_NUM - 1))
+nohup taskset -c $p_start-$p_end python3 train.py \
+    --workers 8 \
+    --batch-size $batch_size \
+    --data data/coco.yaml \
+    --img 640 640 \
+    --cfg cfg/yolov4-pacsp.cfg \
+    --weights '' \
+    --name $model_name \
+    --local_rank $i \
+    --hyp data/hyp.scratch.yaml > ${cur_path}/test/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
+done
+wait
+
+# #训练结束时间，不需要修改
+end_time=$(date +%s)
+echo "end_time: ${end_time}"
+e2e_time=$(( $end_time - $start_time ))
+
+#最后一个迭代FPS值
+FPS=`grep -a 'FPS' ${cur_path}/test/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log | awk -F " " '{print $4}' | sort -n | tail -5 | awk '{a+=$1} END {if (NR != 0) printf("%.3f",a/NR)}'`
+
+#取acc值
+acc=`grep -a 'IoU=0.50:0.95' ${cur_path}/test/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log | grep 'Average Precision'|awk 'NR==1'| awk -F " " '{print $13}'`
+
+#打印，不需要修改
+echo "ActualFPS : $FPS"
+echo "ActualACC : $acc"
+echo "E2E Training Duration sec : $e2e_time"
+
+#稳定性精度看护结果汇总
+#训练用例信息，不需要修改
+BatchSize=${batch_size}
+DeviceType=`uname -m`
+CaseName=${Network}_bs${BatchSize}_${WORLD_SIZE}'p'_'acc'
+
+##获取性能数据，不需要修改
+#单迭代训练时长
+TrainingTime=`awk 'BEGIN{printf "%.2f\n", '${batch_size}'*1000/'${FPS}'}'`
+
+#关键信息打印到${CaseName}.log中，不需要修改
+echo "Network = ${Network}" > $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RankSize = ${RANK_SIZE}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "BatchSize = ${BatchSize}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "DeviceType = ${DeviceType}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "CaseName = ${CaseName}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualFPS = ${FPS}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "TrainingTime = ${TrainingTime}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "E2ETrainingTime = ${e2e_time}" >> $cur_path/test/output/$ASCEND_DEVICE_ID/${CaseName}.log
---
+++ PyTorch/built-in/cv/detection/YOLOV4_eb5f166_for_PyTorch/utils/parse_config.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+# BSD 3-Clause License
+#
+# Copyright (c) 2017
+# All rights reserved.
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ==========================================================================
+
+import os
+
+import numpy as np
+
+
+def parse_model_cfg(path):
+    # Parse the yolo *.cfg file and return module definitions path may be 'cfg/yolov3.cfg', 'yolov3.cfg', or 'yolov3'
+    if not path.endswith('.cfg'):  # add .cfg suffix if omitted
+        path += '.cfg'
+    if not os.path.exists(path) and os.path.exists('cfg' + os.sep + path):  # add cfg/ prefix if omitted
+        path = 'cfg' + os.sep + path
+
+    with open(path, 'r') as f:
+        lines = f.read().split('\n')
+    lines = [x for x in lines if x and not x.startswith('#')]
+    lines = [x.rstrip().lstrip() for x in lines]  # get rid of fringe whitespaces
+    mdefs = []  # module definitions
+    for line in lines:
+        if line.startswith('['):  # This marks the start of a new block
+            mdefs.append({})
+            mdefs[-1]['type'] = line[1:-1].rstrip()
+            if mdefs[-1]['type'] == 'convolutional':
+                mdefs[-1]['batch_normalize'] = 0  # pre-populate with zeros (may be overwritten later)
+        
+        else:
+            key, val = line.split("=")
+            key = key.rstrip()
+
+            if key == 'anchors':  # return nparray
+                mdefs[-1][key] = np.array([float(x) for x in val.split(',')]).reshape((-1, 2))  # np anchors
+            elif (key in ['from', 'layers', 'mask']) or (key == 'size' and ',' in val):  # return array
+                mdefs[-1][key] = [int(x) for x in val.split(',')]
+            else:
+                val = val.strip()
+                if val.isnumeric():  # return int or float
+                    mdefs[-1][key] = int(val) if (int(val) - float(val)) == 0 else float(val)
+                else:
+                    mdefs[-1][key] = val  # return string
+
+    # Check all fields are supported
+    supported = ['type', 'batch_normalize', 'filters', 'size', 'stride', 'pad', 'activation', 'layers', 'groups',
+                 'from', 'mask', 'anchors', 'classes', 'num', 'jitter', 'ignore_thresh', 'truth_thresh', 'random',
+                 'stride_x', 'stride_y', 'weights_type', 'weights_normalization', 'scale_x_y', 'beta_nms', 'nms_kind',
+                 'iou_loss', 'iou_normalizer', 'cls_normalizer', 'iou_thresh', 'atoms', 'na', 'nc']
+
+    f = []  # fields
+    for x in mdefs[1:]:
+        [f.append(k) for k in x if k not in f]
+    u = [x for x in f if x not in supported]  # unsupported fields
+    assert not any(u), "Unsupported fields %s in %s. See https://github.com/ultralytics/yolov3/issues/631" % (u, path)
+
+    return mdefs
+
+
+def parse_data_cfg(path):
+    # Parses the data configuration file
+    if not os.path.exists(path) and os.path.exists('data' + os.sep + path):  # add data/ prefix if omitted
+        path = 'data' + os.sep + path
+
+    with open(path, 'r') as f:
+        lines = f.readlines()
+
+    options = dict()
+    for line in lines:
+        line = line.strip()
+        if line == '' or line.startswith('#'):
+            continue
+        key, val = line.split('=')
+        options[key.strip()] = val.strip()
+
+    return options
