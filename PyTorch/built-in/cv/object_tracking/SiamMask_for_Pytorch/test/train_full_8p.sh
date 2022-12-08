d43373b6b0e96aee7be9f1e85a2b42fecc843291
丁子叉 <dingzilin@huawei.com>
2022-12-08 13:37:52 +0000

    !2915 [自研][pytorch]siammask模型修改8p全量脚本 !2915 [自研][pytorch]siammask模型修改8p全量脚本

--- PyTorch/built-in/cv/object_tracking/SiamMask_for_Pytorch/test/train_full_8p.sh
+++ PyTorch/built-in/cv/object_tracking/SiamMask_for_Pytorch/test/train_full_8p.sh
@@ -143,7 +143,7 @@ FPS=`awk 'BEGIN{printf "%.2f\n", '${batch_size}'/'${time}'*8}'`
 echo "Final Performance images/sec : $FPS"
 
 #输出训练精度,需要模型审视修改
-train_accuracy=`grep "INFO:global:Epoch:" ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}_8p.log | tail -n 201 | awk -F "siammask_loss" '{print $2}' | awk -F " " '{print $2}' | awk '{sum+=$1} END {print sum/200}'`
+train_accuracy=`grep "INFO:global:Epoch:" ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}_8p.log | tail -n 201 | awk -F "siammask_loss" '{print $2}' | awk -F " " '{print $2}' | awk '$1 != "nan" {print $1}' | awk '{sum+=$1} END {print sum/200}'`
 train_accuracy=${train_accuracy}
 #打印，不需要修改
 echo "Final Train Accuracy : ${train_accuracy}"
