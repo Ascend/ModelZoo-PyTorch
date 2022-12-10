3d2f7a93f76f3e2e8bd1b253c254aee149d891d2
李欣悦 <11379118+lixinyue0911@user.noreply.gitee.com>
2022-12-10 08:58:00 +0000

    !2685 [西安电子科技大学][高校贡献][Pytorch离线推理][PointNet]--初次提交 * update ACL_PyTorch/contrib/cv/classfication/PointNet/README.md. * update ACL_PyTorch/contrib/cv/classfication/PointNet/requirements.txt. * update ACL_PyTorch/contrib/cv/classfication/PointNet/README.md. * update ACL_PyTorch/contrib/cv/classfication/PointNet/requirements.txt. * update ACL_PyTorch/contrib/cv/classfication/PointNet/README.md. * update ACL_PyTorch/contrib/cv/classfication/PointNet/pointnet_postprocess.py. * update ACL_PyTorch/contrib/cv/classfication/PointNet/README.md. * update ACL_PyTorch/contrib/cv/classfication/PointNet/README.md. * update ACL_PyTorch/contrib/cv/classfication/PointNet/pointnet_preprocess.py. * update ACL_PyTorch/contrib/cv/classfication/PointNet/pointnet_postprocess.py. * lxy6 * lxy6 * lxy5 * lxy5 * lxy4

--- ACL_PyTorch/contrib/cv/classfication/PointNet/test/download.sh
+++
@@ -1,8 +0,0 @@
-SCRIPT=`realpath $0`
-SCRIPTPATH=`dirname $SCRIPT`
-
-cd $SCRIPTPATH/..
-wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
-unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
-rm shapenetcore_partanno_segmentation_benchmark_v0.zip
-cd -
--- ACL_PyTorch/contrib/cv/classfication/PointNet/test/perform_310.sh
+++
@@ -1,5 +0,0 @@
-./benchmark.x86_64 -round=100 -om_path=./pointnet_bs1_fixed.om -device_id=1 -batch_size=1
-./benchmark.x86_64 -round=100 -om_path=./pointnet_bs4_fixed.om -device_id=1 -batch_size=4
-./benchmark.x86_64 -round=100 -om_path=./pointnet_bs8_fixed.om -device_id=1 -batch_size=8
-./benchmark.x86_64 -round=100 -om_path=./pointnet_bs16_fixed.om -device_id=1 -batch_size=16
-./benchmark.x86_64 -round=100 -om_path=./pointnet_bs32_fixed.om -device_id=1 -batch_size=32
