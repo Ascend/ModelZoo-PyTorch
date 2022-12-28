d6d082188e6ce7d6912f001d1158d914ed16922a
郑文钢 <zhengwengang2@huawei.com>
2022-12-28 01:49:07 +0000

    !3114 【众智】【PyTorch离线推理】【cv】ViT_base模型代码与资料整改 * ViT_base: fix code/readme format for CI * ViT_base: fix version for requirements * ViT_base: add input paras to support similar models * ViT_base: optimize preprocess * ViT_base: reformat evaluation code * ViT_base: fix format && update performance * Rename scripts for ViT_base * Rename model name: vit_base_patch32_224->ViT_base

--- ACL_PyTorch/contrib/cv/classfication/ViT_base/Vit_base_preprocess.py
+++ ACL_PyTorch/contrib/cv/classfication/ViT_base/Vit_base_preprocess.py
@@ -22,6 +22,7 @@ import timm
 from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
 import argparse
+from tqdm import tqdm
 
 
 parser = argparse.ArgumentParser(description='Path', add_help=False)
@@ -29,25 +30,38 @@ parser.add_argument('--data-path', metavar='DIR',
                     help='path to dataset')
 parser.add_argument('--store-path', metavar='DIR',
                     help='path to store')
+parser.add_argument('--model-name', default='vit_base_patch32_224',
+                    type=str, help='mode name for vit')
+parser.add_argument('--dtype', default='float32',
+                    type=str, help='dtype for input data')
 
 
 def preprocess(src_path, save_path):
-    os.mkdir(save_path)
-    model = timm.create_model('vit_base_patch32_224')
+    model = timm.create_model(args.model_name)
     model.eval()
-    config = resolve_data_config({},model=model)
+    config = resolve_data_config({}, model=model)
     transform = create_transform(**config)
-    i = 0
     in_files = os.listdir(src_path)
-    for file in in_files:
-        i = i + 1
-        print(file, "===", i)
-        input_image = Image.open(src_path+'/'+file).convert('RGB')
+    def process(image_path):
+        file_name = os.path.basename(image_path)
+        input_image = Image.open(image_path).convert('RGB')
         input_tensor = transform(input_image)
-        img = np.array(input_tensor).astype(np.float32)
-        img.tofile(os.path.join(save_path, file.split('.')[0] + ".bin"))
+        img = np.array(input_tensor).astype(args.dtype)
+        img.tofile(os.path.join(save_path, file_name.split('.')[0] + ".bin"))
+
+    image_path_list = []
+    for file_name in tqdm(in_files):
+        file_path = os.path.join(src_path, file_name)
+        if os.path.isdir(file_path):
+            for image_name in os.listdir(file_path):
+                image_path = os.path.join(file_path, image_name)
+                image_path_list.append(image_path)
+        else:
+            image_path_list.append(file_path)
+    for image_path in tqdm(image_path_list):
+        process(image_path)
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    preprocess(args.data_path,args.store_path)
+    preprocess(args.data_path, args.store_path)
--- ACL_PyTorch/contrib/cv/classfication/ViT_base/Vit_base_pth2onnx.py
+++ ACL_PyTorch/contrib/cv/classfication/ViT_base/Vit_base_pth2onnx.py
@@ -13,27 +13,34 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-
+import os
 import torch
 import timm
 import argparse
 
 
 parser = argparse.ArgumentParser(description='Path', add_help=False)
-parser.add_argument('--model-path', default="B_32-i21k-300ep-lr_0.001-aug_medium1-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_224.npz", metavar='DIR',
+parser.add_argument('--model_path', default="B_32-i21k-300ep-lr_0.001-aug_medium1-wd_0.03-do_0.0-sd_0.0--imagenet2012-steps_20k-lr_0.03-res_224.npz", metavar='DIR',
                     help='path to model')
-parser.add_argument('--batch-size',default="1", type=int,
+parser.add_argument('--save_dir',default="models/onnx", type=str,
+                    help='save dir for onnx model')
+parser.add_argument('--batch_size',default=1, type=int,
                     help='batch size')
+parser.add_argument('--input_size',default=224, type=int,
+                    help='size for input image')
+parser.add_argument('--model_name',default="vit_base_patch32_224", type=str,
+                    help='model name for ViT')
 
 
 def main():
     args = parser.parse_args()
-    model = timm.create_model('vit_base_patch32_224')
+    model = timm.create_model(args.model_name)
     model.load_pretrained(args.model_path)
     model.eval()
-    tensor = torch.zeros(args.batch_size,3,224,224)
-    torch.onnx.export(model, tensor, "vit_bs"+str(args.batch_size)+".onnx", opset_version=11,
+    tensor = torch.zeros(args.batch_size, 3, args.input_size, args.input_size)
+    save_path = os.path.join(args.save_dir,
+                             "vit_base_bs"+str(args.batch_size)+".onnx")
+    torch.onnx.export(model, tensor, save_path, opset_version=11,
                       do_constant_folding=True, input_names=["input"], output_names=["output"])
 
 
