7c48a297f08eb7e9260ffb426faba688d465f89b
魏鹏飞 <weipengfei19@huawei.com>
2022-12-29 07:30:24 +0000

    !3236 [众智][PyTorch]SOLOv2模型更正权重文件默认路径 * 更正权重文件默认路径

--- PyTorch/contrib/cv/detection/SOLOv2/test/train_full_1p.sh
+++ PyTorch/contrib/cv/detection/SOLOv2/test/train_full_1p.sh
@@ -72,7 +72,7 @@ export RANK=0
 python3.7 tools/train.py configs/solov2/solov2_r50_fpn_8gpu_1x.py --opt-level $apex --autoscale-lr --seed 0 --total_epochs 12 \
       --batch_size=${batch_size} --data_root=$data_path --gpu-ids ${device_id} > ${cur_path}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
 wait
-python3.7 tools/test_ins.py configs/solov2/solov2_r50_fpn_8gpu_1x.py  work_dirs/solov2_release_r50_fpn_8gpu_1x/latest.pth --show \
+python3.7 tools/test_ins.py configs/solov2/solov2_r50_fpn_8gpu_1x.py  test/work_dirs/solov2_release_r50_fpn_8gpu_1x/latest.pth --show \
       --gpu-ids ${device_id} --out  results_solo.pkl --eval segm --data_root=$data_path >> ${cur_path}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
 wait
 
