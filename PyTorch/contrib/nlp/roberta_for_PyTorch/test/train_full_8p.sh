525800d1283b7c1f1931f906bcf579547b786ccd
dongwenbo6 <dongwenbo6@huawei.com>
2022-12-29 07:28:12 +0000

    !3240 [众智][PyTorch][roberta_for_pytorch]-roberta模型整改 * roberta_for_pytorch整改

--- PyTorch/contrib/nlp/roberta_for_PyTorch/test/train_full_8p.sh
+++ PyTorch/contrib/nlp/roberta_for_PyTorch/test/train_full_8p.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
-TOTAL_NUM_UPDATES=20935	  
-WARMUP_UPDATES=1256      
-LR=2e-05                
+#当前路径,不需要修改
+cur_path=`pwd`
+
+#集合通信参数,不需要修改
+export RANK_SIZE=8
+
+#数据集路径,保持为空,不需要修改
+data_path=""
+
+#基础参数，需要模型审视修改
+#网络名称，同目录名称
+Network="roberta_for_PyTorch"
+#训练epoch
+train_epochs=10
+#训练batch_size
+batch_size=64
+#学习率
+learning_rate=2e-05
+
+#参数校验，不需要修改
+for para in $*
+do
+    if [[ $para == --data_path* ]];then
+        data_path=`echo ${para#*=}`
+    fi
+done
+
+# 校验是否传入data_path,不需要修改
+if [[ $data_path == "" ]];then
+    echo "[Error] para \"data_path\" must be confing"
+    exit 1
+fi
+
+TOTAL_NUM_UPDATES=20935
+WARMUP_UPDATES=1256
 NUM_CLASSES=2
-MAX_SENTENCES=64       
+MAX_SENTENCES=$batch_size
 ROBERTA_PATH=./pre_train_model/roberta.base/model.pt
 DISTRIBUTED_BACKEND=hccl
 DISTRIBUTED_WORLD_SIZE=8
@@ -14,6 +46,52 @@ if [ ! -d $OUTPUT_DIR ]
 then
     mkdir $OUTPUT_DIR
 fi
+
+###############指定训练脚本执行路径###############
+# cd到与test文件夹同层级目录下执行脚本，提高兼容性；test_path_dir为包含test文件夹的路径
+cur_path=`pwd`
+cur_path_last_diename=${cur_path##*/}
+if [ x"${cur_path_last_diename}" == x"test" ];then
+    test_path_dir=${cur_path}
+    cd ..
+    cur_path=`pwd`
+else
+    test_path_dir=${cur_path}/test
+fi
+
+###############指定训练脚本执行路径###############
+# cd到与test文件夹同层级目录下执行脚本，提高兼容性；test_path_dir为包含test文件夹的路径
+cur_path=`pwd`
+cur_path_last_diename=${cur_path##*/}
+if [ x"${cur_path_last_diename}" == x"test" ];then
+    test_path_dir=${cur_path}
+    cd ..
+    cur_path=`pwd`
+else
+    test_path_dir=${cur_path}/test
+fi
+
+
+#################创建日志输出目录，不需要修改#################
+ASCEND_DEVICE_ID=0
+if [ -d ${test_path_dir}/output/${ASCEND_DEVICE_ID} ];then
+    rm -rf ${test_path_dir}/output/${ASCEND_DEVICE_ID}
+    mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+else
+    mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+fi
+
+#################启动训练脚本#################
+
+# 非平台场景时source 环境变量
+check_etp_flag=`env | grep etp_running_flag`
+etp_flag=`echo ${check_etp_flag#*=}`
+if [ x"${etp_flag}" != x"true" ];then
+    source ${test_path_dir}/env_npu.sh
+fi
+
+#训练开始时间，不需要修改
+start_time=$(date +%s)
 
 RANK_ID_START=0
 RANK_SIZE=8
@@ -26,7 +104,7 @@ PID_START=$((KERNEL_NUM * RANK_ID))
 PID_END=$((PID_START + KERNEL_NUM - 1))
 
 nohup \
-taskset -c $PID_START-$PID_END python3.7.5 -u ./train.py ./data/SST-2/ "$@"  \
+taskset -c $PID_START-$PID_END python3.7 -u ./train.py $data_path  \
     --restore-file $ROBERTA_PATH \
     --max-positions 512 \
     --batch-size $MAX_SENTENCES \
@@ -42,9 +120,9 @@ taskset -c $PID_START-$PID_END python3.7.5 -u ./train.py ./data/SST-2/ "$@"  \
     --dropout 0.1 --attention-dropout 0.1 \
     --weight-decay 0.1 --optimizer adam --adam-betas "(0.9, 0.98)" --adam-eps 1e-06 \
     --clip-norm 0.0 \
-    --lr-scheduler polynomial_decay --lr $LR --total-num-update $TOTAL_NUM_UPDATES --warmup-updates $WARMUP_UPDATES \
+    --lr-scheduler polynomial_decay --lr $learning_rate --total-num-update $TOTAL_NUM_UPDATES --warmup-updates $WARMUP_UPDATES \
     --use-apex --use-npu-adam --opt-level O2 --loss-scale 32 \
-    --max-epoch 10 \
+    --max-epoch $train_epochs \
     --log-interval 10 \
     --find-unused-parameters \
     --best-checkpoint-metric accuracy --maximize-best-checkpoint-metric \
@@ -52,13 +130,54 @@ taskset -c $PID_START-$PID_END python3.7.5 -u ./train.py ./data/SST-2/ "$@"  \
     --distributed-world-size $RANK_SIZE \
     --distributed-no-spawn \
     --npu \
-    --save-dir $OUTPUT_DIR/checkpoints \
+    --no-save \
     --num-workers $(($(nproc)/8)) \
     --load-checkpoint-on-all-dp-ranks \
     --device-id $RANK_ID \
-	--distributed-rank $RANK_ID & 
+	  --distributed-rank $RANK_ID > ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
 done
 
 wait
 
-mv nohup.out $OUTPUT_DIR/8p_npu_full.log
+#训练结束时间，不需要修改
+end_time=$(date +%s)
+e2e_time=$(( $end_time - $start_time ))
+
+##################获取训练数据################
+#结果打印，不需要修改
+echo "------------------ Final result ------------------"
+#输出性能FPS，需要模型审视修改
+FPS=`grep -a 'FPS='  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F 'FPS=' '{print $2}'|awk 'NR>10'|awk '{sum+=$1} END {print sum/NR}'`
+#打印，不需要修改
+echo "Final Performance images/sec : $FPS"
+
+#输出训练精度,需要模型审视修改
+train_accuracy=`grep -a 'valid | epoch'  ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|tail -1|awk -F 'accuracy' '{print $2}'|awk -F '|' '{print $1}'`
+#打印，不需要修改
+echo "Final Train Accuracy : ${train_accuracy}"
+echo "E2E Training Duration sec : $e2e_time"
+
+#性能看护结果汇总
+#训练用例信息，不需要修改
+BatchSize=${batch_size}
+DeviceType=`uname -m`
+CaseName=${Network}_bs${BatchSize}_${RANK_SIZE}'p'_'perf'
+
+#获取性能数据，不需要修改
+#吞吐量
+ActualFPS=${FPS}
+
+#从train_$ASCEND_DEVICE_ID.log提取Loss到train_${CaseName}_loss.txt中，需要模型审视修改
+grep -a 'loss=' ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_$ASCEND_DEVICE_ID.log|awk -F 'loss=' '{print $2}' |awk -F ',' '{print $1}' > ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt
+#最后一个迭代loss值，不需要修改
+ActualLoss=`awk 'END {print}' ${test_path_dir}/output/$ASCEND_DEVICE_ID/train_${CaseName}_loss.txt`
+
+#关键信息打印到${CaseName}.log中，不需要修改
+echo "Network = ${Network}" > ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "RankSize = ${RANK_SIZE}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "BatchSize = ${BatchSize}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "DeviceType = ${DeviceType}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "CaseName = ${CaseName}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualFPS = ${ActualFPS}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "ActualLoss = ${ActualLoss}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
+echo "E2ETrainingTime = ${e2e_time}" >> ${test_path_dir}/output/$ASCEND_DEVICE_ID/${CaseName}.log
