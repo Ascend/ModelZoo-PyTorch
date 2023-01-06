877659bb5fe063e1a9cc08e76cc951a71d1f632b
litianhua3 <litianhua3@huawei.com>
2023-01-06 09:29:41 +0000

    !3378 模型整改 !3378 模型整改

--- ACL_PyTorch/contrib/nlp/hubert/hubert_data.sh
+++ ACL_PyTorch/contrib/nlp/hubert/hubert_data.sh
@@ -2,11 +2,5 @@
 set -e
 
 # test-clean
-python ./fairseq/examples/wav2vec/wav2vec_manifest.py ./data/LibriSpeech/test-clean/ --dest ./data/test-clean --ext flac --valid-percent 0
-python ./fairseq/examples/wav2vec/libri_labels.py ./data/test-clean/train.tsv --output-dir ./data/test-clean --output-name train
-
-
-if [ ! -d "./data/pt" ]; then
-  mkdir -p ./data/pt
-  wget -nc -O  ./data/pt/hubert_large_ll60k_finetune_ls960.pt https://dl.fbaipublicfiles.com/hubert/hubert_large_ll60k_finetune_ls960.pt
-fi
+python3.7.5 ./fairseq/examples/wav2vec/wav2vec_manifest.py ./data/LibriSpeech/test-clean/ --dest ./data/test-clean --ext flac --valid-percent 0
+python3.7.5 ./fairseq/examples/wav2vec/libri_labels.py ./data/test-clean/train.tsv --output-dir ./data/test-clean --output-name train
