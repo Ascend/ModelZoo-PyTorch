c997e39b2e8eb94f10c0c7f9555a78311de03df4
zheng-wengang1 <zhengwengang2@huawei.com>
2023-01-12 21:09:35 +0800

    BertSum: del old useless scripts

--- ACL_PyTorch/contrib/nlp/BertSum/test/pth2onnx.sh
+++
@@ -1,3 +0,0 @@
-cd ../
-python BertSum-pth2onnx.py -mode test -bert_data_path ../bert_data/cnndm -model_path MODEL_PATH -visible_gpus -1 -gpu_ranks 0 -batch_size 1 -log_file LOG_FILE -result_path RESULT_PATH -test_all -block_trigram true -onnx_path bertsum_13000_9_bs1.onnx -path model_step_13000.pt
-
--- ACL_PyTorch/contrib/nlp/BertSum/test/simplify.sh
+++
@@ -1,2 +0,0 @@
-cd ../
-python -m onnxsim ./bertsum_13000_9_bs1.onnx ./bertsum_13000_9_sim_bs1.onnx
