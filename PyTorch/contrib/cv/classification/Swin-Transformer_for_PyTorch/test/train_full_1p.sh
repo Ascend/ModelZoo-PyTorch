eff2008862f66da8b1664f552d74e0d752c54c81
魏鹏飞 <weipengfei19@huawei.com>
2023-01-13 03:37:39 +0000

    !3478 [众智][PyTorch]Swin-Transformer_for_PyTorch模型更正train_full_1p.sh中变量获取不对 * Swin-Transformer_for_PyTorch更正train_full_1p.sh脚本中变量获取不对

--- PyTorch/contrib/cv/classification/Swin-Transformer_for_PyTorch/test/train_full_1p.sh
+++ PyTorch/contrib/cv/classification/Swin-Transformer_for_PyTorch/test/train_full_1p.sh
@@ -49,7 +49,7 @@ fi
 
 #################创建日志输出目录，不需要修改#################
 
-ASCEND_DEVICE_ID=$device_id}
+ASCEND_DEVICE_ID=${device_id}
 if [ -d ${test_path_dir}/output/${ASCEND_DEVICE_ID} ];then
     rm -rf ${test_path_dir}/output/${ASCEND_DEVICE_ID}
     mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
