a6b3c1f6757ea7df9c078a29ed08806f856e3d0f
小天真 <1643806656@qq.com>
2023-01-16 08:28:09 +0000

    !3489 [PyTorch][built-in][cv][detection][Faster_Mask_RCNN_for_PyTorch]网络关闭强制走静态 * CascadedMaskRCNN，BiseNetV1_for_PyTorch还原强制走静态 * Faster_Mask_RCNN_for_PyTorch，CascadedMaskRCNN，BiseNetV1_for_PyTorch * Faster_Mask_RCNN_for_PyTorch,CascadedMaskRCNN,BiseNetV1_for_PyTorch关闭强制走静态

--- PyTorch/built-in/cv/detection/Faster_Mask_RCNN_for_PyTorch/tools/train_net.py
+++ PyTorch/built-in/cv/detection/Faster_Mask_RCNN_for_PyTorch/tools/train_net.py
@@ -186,7 +186,6 @@ def main(args):
 
 
 if __name__ == "__main__":
-    torch.npu.set_compile_mode(jit_compile=True)
     args = default_argument_parser().parse_args()
     print("Command Line Args:", args)
     launch(
