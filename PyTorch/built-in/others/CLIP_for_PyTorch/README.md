50424faa9ca0abf6c812c9186f2b13a2ceb26316
孙银磊 <sunyinlei@huawei.com>
2023-01-16 07:59:18 +0000

    !3493 [自研][Pytorch]上传CLIP集群训练脚本 * CLIP模型增加集群模式脚本

--- PyTorch/built-in/others/CLIP_for_PyTorch/README.md
+++ PyTorch/built-in/others/CLIP_for_PyTorch/README.md
@@ -97,7 +97,7 @@ wget http://images.cocodataset.org/annotations/image_info_test2017.zip
       ├──val2017.zip  
       ├──test2017.zip  
       ├──annotations_trainval2017.zip  
-      └── image_info_test2017.zip  
+      └─ image_info_test2017.zip  
 ```
 
 > **说明：** 
@@ -138,19 +138,19 @@ python3.7 save_clip_roberta.py
 
 1. 进入解压后的源码包根目录。
 
-   ```
+   ```bash
    cd /${模型文件夹名称} 
    ```
 
 2. 运行训练脚本。
 
-   该模型支持单机单卡训练和单机8卡训练。
+   该模型支持单机单卡训练、单机8卡训练和多机训练。
 
    + 单机单卡训练
 
      启动单卡训练：
 
-     ```
+     ```bash
      bash test/train_clip_full_1p.sh --data_path=/opt/npu/dataset/coco --model_path=./clip-roberta --train_epochs=3    # 1p精度
      
      bash test/train_clip_performance_1p.sh --data_path=/opt/npu/dataset/coco --model_path=./clip-roberta --train_epochs=1    # 1p性能
@@ -160,45 +160,57 @@ python3.7 save_clip_roberta.py
    
      启动8卡训练：
    
-     ```
+     ```bash
      bash test/train_clip_full_8p.sh --data_path=/opt/npu/dataset/coco --model_path=./clip-roberta --train_epochs=3    # 8卡精度
      
      bash test/train_clip_performance_8p.sh --data_path=/opt/npu/dataset/coco --model_path=./clip-roberta --train_epochs=1    # 8卡性能
      ```
      
+   + 多机训练
+     
+     请参考[PyTorch模型多机多卡训练适配指南](https://gitee.com/ascend/pytorch/blob/master/docs/zh/PyTorch%E6%A8%A1%E5%9E%8B%E5%A4%9A%E6%9C%BA%E5%A4%9A%E5%8D%A1%E8%AE%AD%E7%BB%83%E9%80%82%E9%85%8D%E6%8C%87%E5%8D%97.md)中的“多机多卡训练流程”-“准备环境”章节进行环境设置，然后在每台服务器上使用如下命令启动训练：
+     
+     ```bash
+     bash test/train_clip_cluster.sh --data_path=/opt/npu/dataset/coco --model_path=./clip-roberta --train_epochs=3 --nnodes=${机器总数量} --node_rank=${当前机器rank(0,1,2..)} --master_addr=${主服务器地址} --master_port=${主服务器端口号}
+     ```
+     
    + 训练脚本参数说明：
      
-     ```
+     ```bash
      --data_path:    coco数据集路径,和准备数据集章节中的coco文件夹路径保持一致
      --model_path:   预训练模型文件夹路径，和获取预训练模型章节生成的clip-roberta文件夹路径保持一致
      --train_epochs: 训练的epoch数
+     --batch_size:   train和eval的batch size大小
+     --model_name:   模型名称，默认为CLIP
      ```
      
    + 脚本中调用的python命令参数说明如下：
      
-          --output_dir                                   // 训练结果和checkpoint保存路径
-          --num_train_epochs                             // 训练的epoch次数
-          --model_name_or_path                           // 预训练模型文件夹路径
-          --data_dir                                     // 数据集路径
-          --dataset_name                                 // 数据集名称
-          --dataset_config_name                          // 数据集配置名称
-          --image_column                                 // 图片所在的列
-          --caption_column                               // 图片标题所在的列
-          --remove_unused_columns                        // 是否删除未使用的列
-          --do_train                                     // 执行训练
-          --do_eval                                      // 执行评估
-          --fp16                                         // 使用混合精度
-          --dataloader_drop_last                         // 丢弃最后一个不完整的batch
-          --fp16_opt_level                               // 混合精度级别
-          --loss_scale                                   // 混合精度的loss_scale
-          --use_combine_grad                             // 使用combine_grad选项
-          --per_device_train_batch_size                  // 训练时使用的batch_size
-          --per_device_eval_batch_size                   // 评估时使用的batch_size
-          --learning_rate                                // 学习率
-          --warmup_steps                                 // warmup steps，用于调整学习率
-          --weight_decay                                 // 权重衰减值
-          --overwrite_output_dir                         // 覆盖输出目录
-          --local_rank                                   // 使用的卡id
+      ```bash
+      --output_dir                                   // 训练结果和checkpoint保存路径
+      --num_train_epochs                             // 训练的epoch次数
+      --model_name_or_path                           // 预训练模型文件夹路径
+      --data_dir                                     // 数据集路径
+      --dataset_name                                 // 数据集名称
+      --dataset_config_name                          // 数据集配置名称
+      --image_column                                 // 图片所在的列
+      --caption_column                               // 图片标题所在的列
+      --remove_unused_columns                        // 是否删除未使用的列
+      --do_train                                     // 执行训练
+      --do_eval                                      // 执行评估
+      --fp16                                         // 使用混合精度
+      --dataloader_drop_last                         // 丢弃最后一个不完整的batch
+      --fp16_opt_level                               // 混合精度级别
+      --loss_scale                                   // 混合精度的loss_scale
+      --use_combine_grad                             // 使用combine_grad选项
+      --per_device_train_batch_size                  // 训练时使用的batch_size
+      --per_device_eval_batch_size                   // 评估时使用的batch_size
+      --learning_rate                                // 学习率
+      --warmup_steps                                 // warmup steps，用于调整学习率
+      --weight_decay                                 // 权重衰减值
+      --overwrite_output_dir                         // 覆盖输出目录
+     --local_rank                                   // 使用的卡id
+     ```
      
      训练完成后，权重文件保存在output_dir路径下，并输出模型训练精度和性能信息。
      
@@ -208,20 +220,23 @@ python3.7 save_clip_roberta.py
 
 **表 2**  训练结果展示表
 
-| NAME     | eval loss |   FPS   | AMP_Type | Epochs |
-| -------- | :-------: | :-----: | :------: | ------ |
-| 1p-NPU   |  2.1984   | 25.486  |    O2    | 3      |
-| 1p-竞品A |     -     |    -    |    O2    | 3      |
-| 8p-NPU   |  1.5591   | 193.268 |    O2    | 3      |
-| 8p-竞品A |  1.5565   | 177.471 |    O2    | 3      |
+| NAME     | eval loss |   FPS   | AMP_Type | Epochs | Batch Size |
+| -------- | :-------: | :-----: | :------: | ------ | ---------- |
+| 1p-NPU   |  2.1984   | 25.486  |    O2    | 3      | 256        |
+| 1p-竞品A |     -     |    -    |    O2    | 3      | 256        |
+| 8p-NPU   |  1.5591   | 193.268 |    O2    | 3      | 64         |
+| 8p-竞品A |  1.5565   | 177.471 |    O2    | 3      | 64         |
+| 8p-NPU   | 2.2393   | 247.558 |    O2    | 3      | 256        |
+| 8p-竞品A |     -     |    -    |    O2    | 3      | 256        |
 
 # 版本说明
 
 ## 变更
-
-2022.12.20：首次发布。
+2023.01.16：添加集群训练脚本说明
 
 2023.01.12：Readme整改。
+
+2022.12.20：首次发布。
 
 ## 已知问题
 
---
+++ PyTorch/built-in/others/CLIP_for_PyTorch/test/train_clip_cluster.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+
+Network="clip"
+model_name=clip
+train_epochs=3
+batch_size=64
+model_path=""
+data_path=""
+nnodes=""
+node_rank=""
+master_addr=""
+master_port=""
+
+# cd到与test文件夹同层级目录下执行脚本，提高兼容性；test_path_dir为包含test文件夹的路径
+cur_path=$(pwd)
+cur_path_last_dirname=${cur_path##*/}
+if [ x"${cur_path_last_dirname}" == x"test" ]; then
+  test_path_dir=${cur_path}
+  cd ..
+  cur_path=$(pwd)
+else
+  test_path_dir=${cur_path}/test
+fi
+#集合通信参数,不需要修改
+export RANK_SIZE=8
+
+# 数据集路径,保持为空,不需要修改
+data_path=""
+#网络名称
+device_number=8
+for para in $*; do
+  if [[ $para == --model_name* ]]; then
+    model_name=$(echo ${para#*=})
+  elif [[ $para == --batch_size* ]]; then
+    batch_size=$(echo ${para#*=})
+  elif [[ $para == --model_path* ]]; then
+    model_path=$(echo ${para#*=})
+  elif [[ $para == --data_path* ]]; then
+    data_path=$(echo ${para#*=})
+  elif [[ $para == --train_epochs* ]]; then
+    train_epochs=$(echo ${para#*=})
+  elif [[ $para == --node_rank* ]]; then
+    node_rank=$(echo ${para#*=})
+  elif [[ $para == --master_addr* ]]; then
+    master_addr=$(echo ${para#*=})
+  elif [[ $para == --master_port* ]]; then
+    master_port=$(echo ${para#*=})
+  elif [[ $para == --nnodes* ]]; then
+    nnodes=$(echo ${para#*=})
+  fi
+done
+
+#校验是否传入model_path
+if [[ $model_path == "" ]]; then
+  echo "[Error] para \"model_path\" must be confing"
+  exit 1
+fi
+#校验是否传入data_path
+if [[ $data_path == "" ]]; then
+  echo "[Error] para \"data_path\" must be confing"
+  exit 1
+fi
+#校验是否传入nnodes
+if [[ $nnodes == "" ]]; then
+  echo "[Error] para \"nnodes\" must be confing"
+  exit 1
+fi
+#校验是否传入node_rank
+if [[ $node_rank == "" ]]; then
+  echo "[Error] para \"node_rank\" must be confing"
+  exit 1
+fi
+#校验是否传入master_addr
+if [[ $master_addr == "" ]]; then
+  echo "[Error] para \"master_addr\" must be confing"
+  exit 1
+fi
+#校验是否传入master_port
+if [[ $master_port == "" ]]; then
+  echo "[Error] para \"master_port\" must be confing"
+  exit 1
+fi
+
+#非平台场景时source 环境变量
+check_etp_flag=$(env | grep etp_running_flag)
+etp_flag=$(echo ${check_etp_flag#*=})
+if [ x"${etp_flag}" != x"true" ]; then
+  source ${test_path_dir}/env_npu.sh
+fi
+
+ASCEND_DEVICE_ID=0
+#################创建日志输出目录，不需要修改#################
+if [ -d ${test_path_dir}/output/${ASCEND_DEVICE_ID} ]; then
+  rm -rf ${test_path_dir}/output/${ASCEND_DEVICE_ID}
+  mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+else
+  mkdir -p ${test_path_dir}/output/$ASCEND_DEVICE_ID
+fi
+
+export HCCL_WHITELIST_DISABLE=1
+export HCCL_IF_IP=$(hostname -I | awk '{print $1}')
+export MASTER_ADDR=${master_addr}
+export MASTER_PORT=${master_port}
+export WORLD_SIZE=$((nnodes * device_number))
+
+#训练开始时间，不需要修改
+start_time=$(date +%s)
+if [ $(uname -m) = "aarch64" ]; then
+  for i in $(seq 0 7); do
+    rank=$((i + node_rank * device_number))
+    export RANK=${rank}
+    export LOCAL_RANK=$i
+    KERNEL_NUM=$(($(nproc) / 8))
+    PID_START=$((KERNEL_NUM * i))
+    PID_END=$((PID_START + KERNEL_NUM - 1))
+    taskset -c $PID_START-$PID_END $CMD \
+      python3.7 ./run_clip.py --output_dir ./clip-roberta-finetuned-npu-8p \
+      --num_train_epochs ${train_epochs} \
+      --model_name_or_path "$model_path" \
+      --data_dir $data_path \
+      --dataset_name ydshieh/coco_dataset_script \
+      --dataset_config_name=2017 \
+      --image_column image_path --caption_column caption \
+      --remove_unused_columns=False \
+      --do_train --do_eval --fp16 --dataloader_drop_last \
+      --fp16_opt_level O2 --loss_scale 12800000 --use_combine_grad \
+      --per_device_train_batch_size=$batch_size --per_device_eval_batch_size=$batch_size \
+      --learning_rate="5e-5" --warmup_steps="0" --weight_decay 0.1 \
+      --overwrite_output_dir \
+      --local_rank $i >${test_path_dir}/output/$ASCEND_DEVICE_ID/train_${ASCEND_DEVICE_ID}.log 2>&1 &
+  done
+else
+  python3.7 -m torch.distributed.launch --nproc_per_node 8 \
+    --nnodes=$nnodes \
+    --node_rank $node_rank \
+    --master_addr $master_addr \
+    --master_port $master_port \
+    ./run_clip.py --output_dir ./clip-roberta-finetuned-npu-8p \
+    --num_train_epochs ${train_epochs} \
+    --model_name_or_path "$model_path" \
+    --data_dir $data_path \
+    --dataset_name ydshieh/coco_dataset_script \
+    --dataset_config_name=2017 \
+    --image_column image_path --caption_column caption \
+    --remove_unused_columns=False \
+    --do_train --do_eval --fp16 --dataloader_drop_last \
+    --fp16_opt_level O2 --loss_scale 12800000 --use_combine_grad \
+    --per_device_train_batch_size=$batch_size --per_device_eval_batch_size=$batch_size \
+    --learning_rate="5e-5" --warmup_steps="0" --weight_decay 0.1 \
+    --overwrite_output_dir >${test_path_dir}/output/$ASCEND_DEVICE_ID/train_${ASCEND_DEVICE_ID}.log 2>&1 &
+fi
+
+wait
+
+total_device=$((nnodes * device_number))
+if [[ x"${master_addr}" == x"${HCCL_IF_IP}" ]]; then
+  #训练结束时间，不需要修改
+  end_time=$(date +%s)
+  e2e_time=$(($end_time - $start_time))
+
+  #结果打印，不需要修改
+  echo "------------------ Final result ------------------"
+  #输出性能FPS，需要模型审视修改
+  FPS=$(grep "train_samples_per_second =" ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log | awk 'END {print $3}')
+
+  #打印，不需要修改
+  echo "Final Performance images/sec : $FPS"
+
+  #输出训练精度,需要模型审视修改
+  train_loss=$(grep "eval_loss" ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log | awk 'END {print $3}')
+
+  #打印，不需要修改
+  echo "Final Train Loss : ${train_loss}"
+  echo "E2E Training Duration sec : $e2e_time"
+
+  #性能看护结果汇总
+  #训练用例信息，不需要修改
+  BatchSize=${batch_size}
+  DeviceType=$(uname -m)
+  CaseName=${Network}_bs${BatchSize}_${total_device}'p'_'perf'
+
+  ##获取性能数据，不需要修改
+  #吞吐量
+  ActualFPS=${FPS}
+  #单迭代训练时长
+  TrainingTime=$(awk 'BEGIN{printf "%.2f\n", '${batch_size}'*1000/'${FPS}'}')
+
+  #从train_$ASCEND_DEVICE_ID.log提取Loss到train_${CaseName}_loss.txt中，需要根据模型审视
+  grep "{'loss" ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log | awk -F "{'loss" '{print $2}' | awk '{print $2}' | awk -F "," '{print $1}' >${test_path_dir}/output/${ASCEND_DEVICE_ID}//train_${CaseName}_loss.txt
+  #最后一个迭代loss值，不需要修改
+  ActualLoss=$(awk 'END {print}' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${CaseName}_loss.txt)
+
+  #关键信息打印到${CaseName}.log中，不需要修改
+  echo "Network = ${Network}" >${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+  echo "RankSize = ${WORLD_SIZE}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+  echo "BatchSize = ${BatchSize}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+  echo "DeviceType = ${DeviceType}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+  echo "CaseName = ${CaseName}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+  echo "ActualFPS = ${ActualFPS}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+  echo "TrainingTime = ${TrainingTime}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+  echo "ActualLoss = ${ActualLoss}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+  echo "E2ETrainingTime = ${e2e_time}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+  echo "TrainAccuracy = ${train_loss}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+fi
