acb022933eb7cc97efd93c591929e4f33829b6dd
王国庆 <3224925783@qq.com>
2023-01-28 09:40:23 +0000

    update PyTorch/built-in/nlp/Bert-Squad_ID0470_for_PyTorch/test/train_ID4126_large_performance_1p.sh.
    Signed-off-by: 王国庆 <3224925783@qq.com>

--- PyTorch/built-in/nlp/Bert-Squad_ID0470_for_PyTorch/test/train_ID4126_large_performance_1p.sh
+++ PyTorch/built-in/nlp/Bert-Squad_ID0470_for_PyTorch/test/train_ID4126_large_performance_1p.sh
@@ -92,6 +92,7 @@ check_etp_flag=`env | grep etp_running_flag`
 etp_flag=`echo ${check_etp_flag#*=}`
 if [ x"${etp_flag}" != x"true" ];then
     source  ${test_path_dir}/env_npu.sh
+    ASCEND_DEVICE_ID=0
 fi
 #训练开始时间，不需要修改
 start_time=$(date +%s)
