ce65720db78f16e37b236842eb7663ac49a21e98
wuxiankun <wuxiankun@huawei.com>
2023-01-31 09:29:52 +0800

    support pass log name

--- PyTorch/contrib/cv/classification/Vit_small_patch16_224/distributed_train_npu.sh
+++ PyTorch/contrib/cv/classification/Vit_small_patch16_224/distributed_train_npu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RANK_ID_START=0
 RANK_SIZE=$1
-
+OUTPUT_LOG=${OUTPUT_LOG:-"output.log"}
 shift
 
 for((RANK_ID=$RANK_ID_START;RANK_ID<$((RANK_SIZE+RANK_ID_START));RANK_ID++));
@@ -10,5 +10,5 @@ KERNEL_NUM=$(($(nproc)/8))
 PID_START=$((KERNEL_NUM * RANK_ID))
 PID_END=$((PID_START + KERNEL_NUM - 1))
 nohup taskset -c $PID_START-$PID_END python \
-    train.py --local_rank $RANK_ID "$@" &
+    train.py --local_rank $RANK_ID "$@" > ${OUTPUT_LOG} 2>&1 &
 done
