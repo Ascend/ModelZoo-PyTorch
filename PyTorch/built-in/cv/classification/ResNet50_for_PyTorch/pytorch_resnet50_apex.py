d2b81819fb3c307bb56021ffe8e421399e42b6d5
xudaohong <xudaohong@huawei.com>
2023-02-03 06:51:07 +0000

    !3644 [fix] fix tensor device bug * [fix] fix tensor device bug

--- PyTorch/built-in/cv/classification/ResNet50_for_PyTorch/pytorch_resnet50_apex.py
+++ PyTorch/built-in/cv/classification/ResNet50_for_PyTorch/pytorch_resnet50_apex.py
@@ -653,7 +653,7 @@ class LabelSmoothing(nn.Module):
 
     def forward(self, x, target):
         logprobs = torch.nn.functional.log_softmax(x, dim=-1)
-        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1)).to(CALCULATE_DEVICE)
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1).to(CALCULATE_DEVICE))
         nll_loss = nll_loss.squeeze(1)
         smooth_loss = -logprobs.mean(dim=-1)
         loss = self.confidence * nll_loss + self.smoothing * smooth_loss
