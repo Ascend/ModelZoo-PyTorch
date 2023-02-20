9b1eec46b41f696b12ac31628dd704fb1b7088a4
wangzw1022 <wangzhiwei73@huawei.com>
2023-02-20 20:41:18 +0800

    CLIP模型性能调优
    CLIP模型性能调优

--- PyTorch/built-in/others/CLIP_for_PyTorch/transformers/src/transformers/utils/npu_module.py
+++ PyTorch/built-in/others/CLIP_for_PyTorch/transformers/src/transformers/utils/npu_module.py
@@ -15,7 +15,7 @@
 import torch
 import torch.nn as nn
 import torch_npu
-
+from torch._six import inf
 
 class MatmulApply(torch.autograd.Function):
     @staticmethod
@@ -42,3 +42,31 @@ def Matmul_transpose(tensor1, tensor2):
 class NpuLinear(nn.Linear):
     def forward(self, input):
         return torch.npu_linear(input, self.weight, self.bias)
+
+def clip_grad_norm_fused(combined_grads, combined_grad_masks, max_norm, norm_type):
+    max_norm = float(max_norm)
+    norm_type = float(norm_type)
+    tmp_lst = []
+    if norm_type == inf:
+        for combined_grad, combined_grad_mask in zip(combined_grads, combined_grad_masks):
+            if combined_grad is not None:
+                tmp_lst.append(combined_grad.float().abs().mul_(combined_grad_mask).max())
+        total_norm = max(tmp_lst)
+    else:
+        for combined_grad, combined_grad_mask in zip(combined_grads, combined_grad_masks):
+            if combined_grad is not None:
+                combined_grad = combined_grad.float()
+                tmp_lst.append(combined_grad.mul(combined_grad).mul_(combined_grad_mask).sum())
+        total_norm = torch.stack(tmp_lst).sum().pow(1/norm_type)
+    clip_coef = max_norm / (total_norm + 1e-6)
+    if clip_coef < 1:
+        for combined_grad in combined_grads:
+            if combined_grad is not None:
+                combined_grad.mul_(clip_coef)
+    return total_norm
+
+def clip_optimizer_grad_norm_fused(self, max_norm, norm_type=2):
+    combined_grads = self.get_optimizer_combined_grads()
+    combined_grad_masks = self.get_optimizer_combined_grad_masks()
+    total_norm = clip_grad_norm_fused(combined_grads, combined_grad_masks, max_norm, norm_type)
+    return total_norm
