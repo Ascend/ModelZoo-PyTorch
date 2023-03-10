d1cc5d74e8a333bbc903b802919e27006eba4ced
xiahaisheng <2652788731@qq.com>
2023-03-10 05:56:15 +0000

    update ACL_PyTorch/contrib/cv/classfication/RARE_MobileNetV3/RARE_MobileNetV3_tps_postprocess.py.
    Signed-off-by: xiahaisheng <2652788731@qq.com>

--- ACL_PyTorch/contrib/cv/classfication/RARE_MobileNetV3/RARE_MobileNetV3_tps_postprocess.py
+++ ACL_PyTorch/contrib/cv/classfication/RARE_MobileNetV3/RARE_MobileNetV3_tps_postprocess.py
@@ -15,27 +15,25 @@
 import os
 import sys
 
-__dir__ = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(__dir__)
-sys.path.append(os.path.abspath(os.path.join(__dir__, 'PaddleOCR')))
-
 import paddle
 import numpy as np
-import tools.program as program
-
 from tqdm import tqdm
 from ppocr.data import build_dataloader
 from ppocr.metrics import build_metric
 from ppocr.postprocess import build_post_process
+import tools.program as program
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(__dir__)
+sys.path.append(os.path.abspath(os.path.join(__dir__, 'PaddleOCR')))
 
 
-def main(config, device, logger, vdl_writer):
-    valid_dataloader = build_dataloader(config, 'Eval', device, logger)
+def main(configs, devices, loggers, vdl_writers):
+    valid_dataloader = build_dataloader(configs, 'Eval', devices, loggers)
 
-    eval_class = build_metric(config['Metric'])
+    eval_class = build_metric(configs['Metric'])
 
-    global_config = config['Global']
-    post_process_class = build_post_process(config['PostProcess'], global_config)
+    global_config = configs['Global']
+    post_process_class = build_post_process(configs['PostProcess'], global_config)
     
     pbar = tqdm(
             total=len(valid_dataloader),
@@ -45,7 +43,7 @@ def main(config, device, logger, vdl_writer):
             
     for idx, batch in enumerate(valid_dataloader):
             result_name = "img_{}_0.bin".format(idx)           
-            result_path = os.path.join(__dir__, config['results'], result_name)
+            result_path = os.path.join(__dir__, configs['results'], result_name)
             
             preds = paddle.to_tensor(np.fromfile(result_path, dtype=np.float32).reshape(1, 25, 38))
                         
