44149ab084eeda19a23e8b12c186470b93facf75
bailang <bailang12@h-partners.com>
2023-03-10 10:30:11 +0800

    [自研][PyTorch]YoloV3_ID1790_for_PyTorch模型修复无法获取性能值问题
    Signed-off-by: bailang <bailang12@h-partners.com>

--- PyTorch/built-in/cv/detection/YoloV3_ID1790_for_PyTorch/test/train_full_1p.sh
+++ PyTorch/built-in/cv/detection/YoloV3_ID1790_for_PyTorch/test/train_full_1p.sh
@@ -112,7 +112,7 @@ e2e_time=$(( $end_time - $start_time ))
 #结果打印，不需要修改
 echo "------------------ Final result ------------------"
 #输出性能FPS，需要模型审视修改
-time=`grep -a 'time'  $test_path_dir/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "time: " '{print $2}'|awk -F "," '{print $1}'|awk 'END {print}'|sed 's/.$//'`
+time=`grep -a 'Epoch'  $test_path_dir/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|awk -F "time: " '{print $2}'|awk -F "," '{print $1}'|awk 'END {print}'|sed 's/.$//'`
 FPS=`awk 'BEGIN{printf "%.2f\n", '${batch_size}'/'${time}'}'`
 #打印，不需要修改
 echo "Final Performance images/sec : $FPS"
