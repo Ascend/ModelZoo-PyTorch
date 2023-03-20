8beb57d2063aae401162ed9d2a4f610c5109d29a
白浪 <bailang12@h-partners.com>
2023-03-20 08:57:27 +0000

    update PyTorch/built-in/cv/classification/ResNet50_ID4149_for_PyTorch/test/train_cluster.sh.
    Signed-off-by: 白浪 <bailang12@h-partners.com>

--- PyTorch/built-in/cv/classification/ResNet50_ID4149_for_PyTorch/test/train_cluster.sh
+++ PyTorch/built-in/cv/classification/ResNet50_ID4149_for_PyTorch/test/train_cluster.sh
@@ -82,6 +82,7 @@ do
         PID_END=$((PID_START + KERNEL_NUM - 1))
         nohup taskset -c $PID_START-$PID_END python3.7 main.py \
             --data ${data_path} \
+            --addr ${master_addr} \            
             --amp \
             --world-size ${world_size} \
             --seed 60 \
@@ -96,6 +97,7 @@ do
     else
         nohup python3.7 main.py \
             --data ${data_path} \
+            --addr ${master_addr} \            
             --amp \
             --world-size ${world_size} \
             --seed 60 \
