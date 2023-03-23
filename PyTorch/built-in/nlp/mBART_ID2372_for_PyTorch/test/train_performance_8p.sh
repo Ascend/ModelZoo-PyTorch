5feb9d6481effa76c28f36844305fc4b9d2dbc36
邱彬 <qiubinemail@qq.com>
2023-03-23 16:46:15 +0800

    [自研][pytorch][mBART]16P训练报错

--- PyTorch/built-in/nlp/mBART_ID2372_for_PyTorch/test/train_performance_8p.sh
+++ PyTorch/built-in/nlp/mBART_ID2372_for_PyTorch/test/train_performance_8p.sh
@@ -3,7 +3,7 @@
 #当前路径,不需要修改
 cur_path=`pwd`
 export SCALAR_TO_HOST_MEM=1
-
+export MASTER_ADDR="127.0.0.1"
 export MKL_SERVICE_FORCE_INTEL=1
 export BMMV2_ENABLE=1
 langs=ar_AR,cs_CZ,de_DE,en_XX,es_XX,et_EE,fi_FI,fr_XX,gu_IN,hi_IN,it_IT,ja_XX,kk_KZ,ko_KR,lt_LT,lv_LV,my_MM,ne_NP,nl_XX,ro_RO,ru_RU,si_LK,tr_TR,vi_VN,zh_CN
