5fe64e6e09cab2681bf6cd70109b05eef1ce0ed0
mamba_chen <chenhao388@huawei.com>
2023-04-11 02:36:54 +0000

    !4478 [自研][PyTorch] [LayoutXLM] 关闭convert_sync_batchnorm * [自研][PyTorch] [LayoutXLM] 关闭convert_sync_batchnorm

--- PyTorch/built-in/others/LayoutXLM_for_PyTorch/layoutlmft/models/layoutlmv2/configuration_layoutlmv2.py
+++ PyTorch/built-in/others/LayoutXLM_for_PyTorch/layoutlmft/models/layoutlmv2/configuration_layoutlmv2.py
@@ -1,7 +1,37 @@
 # coding=utf-8
+# BSD 3-Clause License
+#
+# Copyright (c) 2017
+# All rights reserved.
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ==========================================================================
 from transformers.models.layoutlm.configuration_layoutlm import LayoutLMConfig
 from transformers.utils import logging
-
 
 logger = logging.get_logger(__name__)
 
@@ -15,35 +45,35 @@ class LayoutLMv2Config(LayoutLMConfig):
     model_type = "layoutlmv2"
 
     def __init__(
-        self,
-        vocab_size=30522,
-        hidden_size=768,
-        num_hidden_layers=12,
-        num_attention_heads=12,
-        intermediate_size=3072,
-        hidden_act="gelu",
-        hidden_dropout_prob=0.1,
-        attention_probs_dropout_prob=0.1,
-        max_position_embeddings=512,
-        type_vocab_size=2,
-        initializer_range=0.02,
-        layer_norm_eps=1e-12,
-        pad_token_id=0,
-        gradient_checkpointing=False,
-        max_2d_position_embeddings=1024,
-        max_rel_pos=128,
-        rel_pos_bins=32,
-        fast_qkv=True,
-        max_rel_2d_pos=256,
-        rel_2d_pos_bins=64,
-        convert_sync_batchnorm=True,
-        image_feature_pool_shape=[7, 7, 256],
-        coordinate_size=128,
-        shape_size=128,
-        has_relative_attention_bias=True,
-        has_spatial_attention_bias=True,
-        has_visual_segment_embedding=False,
-        **kwargs
+            self,
+            vocab_size=30522,
+            hidden_size=768,
+            num_hidden_layers=12,
+            num_attention_heads=12,
+            intermediate_size=3072,
+            hidden_act="gelu",
+            hidden_dropout_prob=0.1,
+            attention_probs_dropout_prob=0.1,
+            max_position_embeddings=512,
+            type_vocab_size=2,
+            initializer_range=0.02,
+            layer_norm_eps=1e-12,
+            pad_token_id=0,
+            gradient_checkpointing=False,
+            max_2d_position_embeddings=1024,
+            max_rel_pos=128,
+            rel_pos_bins=32,
+            fast_qkv=True,
+            max_rel_2d_pos=256,
+            rel_2d_pos_bins=64,
+            convert_sync_batchnorm=False,  # 由于1980B上存在精度问题，所以暂时关闭
+            image_feature_pool_shape=[7, 7, 256],
+            coordinate_size=128,
+            shape_size=128,
+            has_relative_attention_bias=True,
+            has_spatial_attention_bias=True,
+            has_visual_segment_embedding=False,
+            **kwargs
     ):
         super().__init__(
             vocab_size=vocab_size,
--- PyTorch/built-in/others/LayoutXLM_for_PyTorch/layoutlmft/models/layoutlmv2/modeling_layoutlmv2.py
+++ PyTorch/built-in/others/LayoutXLM_for_PyTorch/layoutlmft/models/layoutlmv2/modeling_layoutlmv2.py
@@ -1,4 +1,35 @@
 # coding=utf-8
+# BSD 3-Clause License
+#
+# Copyright (c) 2017
+# All rights reserved.
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ==========================================================================
 import math
 
 import torch
@@ -27,14 +58,12 @@ from ...utils import ReOutput
 from .configuration_layoutlmv2 import LayoutLMv2Config
 from .detectron2_config import add_layoutlmv2_config
 
-
 logger = logging.get_logger(__name__)
 
 LAYOUTLMV2_PRETRAINED_MODEL_ARCHIVE_LIST = [
     "layoutlmv2-base-uncased",
     "layoutlmv2-large-uncased",
 ]
-
 
 LayoutLMv2LayerNorm = torch.nn.LayerNorm
 
@@ -809,7 +838,6 @@ class LayoutLMv2Model(LayoutLMv2PreTrainedModel):
         sequence_output = encoder_outputs[0]
 
         return sequence_output
-
 
 
 class LayoutLMv2ModelForTokenClassification(LayoutLMv2Model):
