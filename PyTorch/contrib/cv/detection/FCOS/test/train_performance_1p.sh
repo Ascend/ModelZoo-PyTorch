d3c84b2a5056ae168e44953e70e43a1a17551fc1
白浪 <bailang12@h-partners.com>
2023-04-12 01:23:17 +0000

    !4483 [众智][PyTorch]FCOS模型修复无法获取性能值问题 * [众智][PyTorch]FCOS模型修复无法获取性能值问题

--- PyTorch/contrib/cv/detection/FCOS/test/train_performance_1p.sh
+++ PyTorch/contrib/cv/detection/FCOS/test/train_performance_1p.sh
@@ -94,7 +94,7 @@ e2e_time=$(( $end_time - $start_time ))
 #结果打印，不需要修改
 echo "------------------ Final result ------------------"
 #输出性能FPS，需要模型审视修改
-step_time=`grep -a 'time:' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log|grep -a 'INFO:'|awk -F ' ' '{print $8}'|tail -n 10|awk '{sum += $1} END {print sum/NR}'`
+step_time=`grep -a 'time:' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log| awk -F 'time: ' '{print $2}'|awk -F ',' '{print $1}'|tail -n 10|awk '{sum += $1} END {print sum/NR}'`
 FPS=`awk 'BEGIN{printf "%.2f\n", '${batch_size}'/'${step_time}'}'`
 #打印，不需要修改
 echo "Final Performance images/sec : $FPS"
