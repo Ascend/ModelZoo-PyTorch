4f3631d2c9f8ebaa6666c3a6c7fab49ef394e3a3
王姜奔 <wangjiangben@huawei.com>
2023-05-04 16:00:04 +0800

    Code refactoring for clean code

--- PyTorch/dev/perf/Swin_Cifar10/train_cifar10.py
+++ PyTorch/dev/perf/Swin_Cifar10/train_cifar10.py
@@ -38,238 +38,10 @@ import torch.backends.cudnn as cudnn
 import torchvision.transforms as transforms
 
 import bugfix
-from models import *
 from utils import progress_bar
 from randomaug import RandAugment
 from models.vit import ViT
 from models.convmixer import ConvMixer
-
-# parsers
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=1e-4, type=float, help='learning rate') # resnets.. 1e-3, Vit..1e-4
-parser.add_argument('--opt', default="adam")
-parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
-parser.add_argument('--noaug', action='store_true', help='disable use randomaug')
-parser.add_argument('--noamp', action='store_true', help='disable mixed precision \
-                    training. for older pytorch versions')
-parser.add_argument('--wandb', action='store_true', help='disable wandb')
-parser.add_argument('--mixup', action='store_true', help='add mixup augumentations')
-parser.add_argument('--net', default='vit')
-parser.add_argument('--bs', default='512')
-parser.add_argument('--size', default="32")
-parser.add_argument('--n_epochs', type=int, default='200')
-parser.add_argument('--patch', default='4', type=int, help="patch for ViT")
-parser.add_argument('--dimhead', default="512", type=int)
-parser.add_argument('--convkernel', default='8', type=int, help="parameter for convmixer")
-
-# add extra parameter for ddp
-parser.add_argument('--world_size', default=1, type=int, help="world_size in ddp")
-parser.add_argument('--local_rank', default=0, type=int, help="local_rank in ddp")
-
-# add extra parameter for ddp
-parser.add_argument('--eval_interval', default=50, type=int, help="how many epoch eval at once")
-
-args = parser.parse_args()
-
-# take in args
-# Fixed the error judgment about bool type in the source code
-usewandb = args.wandb
-if usewandb:
-    import wandb
-    watermark = "{}_lr{}".format(args.net, args.lr)
-    wandb.init(project="cifar10-challange",
-            name=watermark)
-    wandb.config.update(args)
-
-bs = int(args.bs)
-imsize = int(args.size)
-
-# Fixed the error judgment about bool type in the source code
-use_amp = not args.noamp
-aug = not args.noaug
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
-# Data
-print('==> Preparing data..')
-if args.net=="vit_timm":
-    size = 384
-else:
-    size = imsize
-
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.Resize(size),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.Resize(size),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-# Add RandAugment with N, M(hyperparameter)
-if aug:
-    # Remove out-of-specification syntax
-    N = 2
-    M = 14
-    transform_train.transforms.insert(0, RandAugment(N, M))
-
-# ddp init
-torch.distributed.init_process_group(backend='nccl',  init_method='tcp://127.0.0.1:23333',
-                        world_size=args.world_size, rank=args.local_rank)
-
-# Prepare dataset
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-train_sampler = torch.utils.data.distributed.DistributedSampler(trainset)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=False, num_workers=8,
-                                          sampler=train_sampler, persistent_workers=True)
-
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
-
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-# Model factory..
-print('==> Building model..')
-if args.net=='res18':
-    net = ResNet18()
-elif args.net=='vgg':
-    net = VGG('VGG19')
-elif args.net=='res34':
-    net = ResNet34()
-elif args.net=='res50':
-    net = ResNet50()
-elif args.net=='res101':
-    net = ResNet101()
-elif args.net=="convmixer":
-    # from paper, accuracy >96%. you can tune the depth and dim to scale accuracy and speed.
-    net = ConvMixer(256, 16, kernel_size=args.convkernel, patch_size=1, n_classes=10)
-elif args.net=="mlpmixer":
-    from models.mlpmixer import MLPMixer
-    net = MLPMixer(
-    image_size = 32,
-    channels = 3,
-    patch_size = args.patch,
-    dim = 512,
-    depth = 6,
-    num_classes = 10
-)
-elif args.net=="vit_small":
-    from models.vit_small import ViT
-    net = ViT(
-    image_size = size,
-    patch_size = args.patch,
-    num_classes = 10,
-    dim = int(args.dimhead),
-    depth = 6,
-    heads = 8,
-    mlp_dim = 512,
-    dropout = 0.1,
-    emb_dropout = 0.1
-)
-elif args.net=="vit_tiny":
-    from models.vit_small import ViT
-    net = ViT(
-    image_size = size,
-    patch_size = args.patch,
-    num_classes = 10,
-    dim = int(args.dimhead),
-    depth = 4,
-    heads = 6,
-    mlp_dim = 256,
-    dropout = 0.1,
-    emb_dropout = 0.1
-)
-elif args.net=="simplevit":
-    from models.simplevit import SimpleViT
-    net = SimpleViT(
-    image_size = size,
-    patch_size = args.patch,
-    num_classes = 10,
-    dim = int(args.dimhead),
-    depth = 6,
-    heads = 8,
-    mlp_dim = 512
-)
-elif args.net=="vit":
-    # ViT for cifar10
-    net = ViT(
-    image_size = size,
-    patch_size = args.patch,
-    num_classes = 10,
-    dim = int(args.dimhead),
-    depth = 6,
-    heads = 8,
-    mlp_dim = 512,
-    dropout = 0.1,
-    emb_dropout = 0.1
-)
-elif args.net=="vit_timm":
-    import timm
-    net = timm.create_model("vit_base_patch16_384", pretrained=True)
-    net.head = nn.Linear(net.head.in_features, 10)
-elif args.net=="cait":
-    from models.cait import CaiT
-    net = CaiT(
-    image_size = size,
-    patch_size = args.patch,
-    num_classes = 10,
-    dim = int(args.dimhead),
-    depth = 6,   # depth of transformer for patch to patch attention only
-    cls_depth=2, # depth of cross attention of CLS tokens to patch
-    heads = 8,
-    mlp_dim = 512,
-    dropout = 0.1,
-    emb_dropout = 0.1,
-    layer_dropout = 0.05
-)
-elif args.net=="cait_small":
-    from models.cait import CaiT
-    net = CaiT(
-    image_size = size,
-    patch_size = args.patch,
-    num_classes = 10,
-    dim = int(args.dimhead),
-    depth = 6,   # depth of transformer for patch to patch attention only
-    cls_depth=2, # depth of cross attention of CLS tokens to patch
-    heads = 6,
-    mlp_dim = 256,
-    dropout = 0.1,
-    emb_dropout = 0.1,
-    layer_dropout = 0.05
-)
-elif args.net=="swin":
-    from models.swin import swin_t
-    net = swin_t(window_size=args.patch,
-                num_classes=10,
-                downscaling_factors=(2,2,2,1))
-
-# For Multi-GPU
-if 'cuda' in device:
-    print(device)
-    print("using distributed data parallel")
-    torch.cuda.set_device(args.local_rank)
-
-    # set jit_compile=True avoiding unexpected ERROR
-    torch.npu.set_compile_mode(jit_compile=True)
-
-    net = torch.nn.parallel.DistributedDataParallel(net.cuda(), device_ids=[args.local_rank])
-    cudnn.benchmark = True
-
-if args.resume:
-    # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/{}-ckpt.t7'.format(args.net))
-    net.load_state_dict(checkpoint['net'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
 
 class LabelSmoothing(nn.Module):
     """
@@ -295,21 +67,7 @@ class LabelSmoothing(nn.Module):
         loss = self.confidence * nll_loss + self.smoothing * smooth_loss
         return loss.mean()
 
-# Loss is CE
-criterion = nn.CrossEntropyLoss()
-criterion_ls = LabelSmoothing()
-
-if args.opt == "adam":
-    optimizer = optim.Adam(net.parameters(), lr=args.lr)
-elif args.opt == "sgd":
-    optimizer = optim.SGD(net.parameters(), lr=args.lr)  
-    
-# use cosine scheduling
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.n_epochs)
-
-##### Training
-scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
-def train(epoch, scaler):
+def train(epoch, scaler, criterion_ls, trainloader):
     print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
@@ -338,7 +96,7 @@ def train(epoch, scaler):
     return train_loss/(batch_idx+1)
 
 ##### Validation
-def test(epoch, best_acc):
+def test(epoch, best_acc, criterion, testloader):
     net.eval()
     test_loss = 0
     correct = 0
@@ -380,31 +138,260 @@ def test(epoch, best_acc):
         print(content + "\n", flush=True)
     return test_loss, acc, best_acc
 
-list_loss = []
-list_acc = []
+def get_args():
+    # parsers
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+    parser.add_argument('--lr', default=1e-4, type=float, help='learning rate') # resnets.. 1e-3, Vit..1e-4
+    parser.add_argument('--opt', default="adam")
+    parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+    parser.add_argument('--noaug', action='store_true', help='disable use randomaug')
+    parser.add_argument('--noamp', action='store_true', help='disable mixed precision \
+                        training. for older pytorch versions')
+    parser.add_argument('--wandb', action='store_true', help='disable wandb')
+    parser.add_argument('--mixup', action='store_true', help='add mixup augumentations')
+    parser.add_argument('--net', default='vit')
+    parser.add_argument('--bs', default='512')
+    parser.add_argument('--size', default="32")
+    parser.add_argument('--n_epochs', type=int, default='200')
+    parser.add_argument('--patch', default='4', type=int, help="patch for ViT")
+    parser.add_argument('--dimhead', default="512", type=int)
+    parser.add_argument('--convkernel', default='8', type=int, help="parameter for convmixer")
 
-if usewandb:
-    wandb.watch(net)
+    # add extra parameter for ddp
+    parser.add_argument('--world_size', default=1, type=int, help="world_size in ddp")
+    parser.add_argument('--local_rank', default=0, type=int, help="local_rank in ddp")
 
-for epoch in range(start_epoch, args.n_epochs):
-    start = time.time()
-    trainloss = train(epoch, scaler)
+    # add extra parameter for ddp
+    parser.add_argument('--eval_interval', default=50, type=int, help="how many epoch eval at once")
 
-    if (epoch + 1) % args.eval_interval == 0 or epoch > int(args.n_epochs * 0.9):
-        val_loss, acc, best_acc = test(epoch, best_acc)
+    args = parser.parse_args()
+    return args
+
+def get_net(args):
+    # Model factory..
+    print('==> Building model..')
+    if args.net=='res18':
+        net = ResNet18()
+    elif args.net=='vgg':
+        net = VGG('VGG19')
+    elif args.net=='res34':
+        net = ResNet34()
+    elif args.net=='res50':
+        net = ResNet50()
+    elif args.net=='res101':
+        net = ResNet101()
+    elif args.net=="convmixer":
+        # from paper, accuracy >96%. you can tune the depth and dim to scale accuracy and speed.
+        net = ConvMixer(256, 16, kernel_size=args.convkernel, patch_size=1, n_classes=10)
+    elif args.net=="mlpmixer":
+        from models.mlpmixer import MLPMixer
+        net = MLPMixer(
+        image_size = 32,
+        channels = 3,
+        patch_size = args.patch,
+        dim = 512,
+        depth = 6,
+        num_classes = 10
+    )
+    elif args.net=="vit_small":
+        from models.vit_small import ViT
+        net = ViT(
+        image_size = size,
+        patch_size = args.patch,
+        num_classes = 10,
+        dim = int(args.dimhead),
+        depth = 6,
+        heads = 8,
+        mlp_dim = 512,
+        dropout = 0.1,
+        emb_dropout = 0.1
+    )
+    elif args.net=="vit_tiny":
+        from models.vit_small import ViT
+        net = ViT(
+        image_size = size,
+        patch_size = args.patch,
+        num_classes = 10,
+        dim = int(args.dimhead),
+        depth = 4,
+        heads = 6,
+        mlp_dim = 256,
+        dropout = 0.1,
+        emb_dropout = 0.1
+    )
+    elif args.net=="simplevit":
+        from models.simplevit import SimpleViT
+        net = SimpleViT(
+        image_size = size,
+        patch_size = args.patch,
+        num_classes = 10,
+        dim = int(args.dimhead),
+        depth = 6,
+        heads = 8,
+        mlp_dim = 512
+    )
+    elif args.net=="vit":
+        # ViT for cifar10
+        net = ViT(
+        image_size = size,
+        patch_size = args.patch,
+        num_classes = 10,
+        dim = int(args.dimhead),
+        depth = 6,
+        heads = 8,
+        mlp_dim = 512,
+        dropout = 0.1,
+        emb_dropout = 0.1
+    )
+    elif args.net=="vit_timm":
+        import timm
+        net = timm.create_model("vit_base_patch16_384", pretrained=True)
+        net.head = nn.Linear(net.head.in_features, 10)
+    elif args.net=="cait":
+        from models.cait import CaiT
+        net = CaiT(
+        image_size = size,
+        patch_size = args.patch,
+        num_classes = 10,
+        dim = int(args.dimhead),
+        depth = 6,   # depth of transformer for patch to patch attention only
+        cls_depth=2, # depth of cross attention of CLS tokens to patch
+        heads = 8,
+        mlp_dim = 512,
+        dropout = 0.1,
+        emb_dropout = 0.1,
+        layer_dropout = 0.05
+    )
+    elif args.net=="cait_small":
+        from models.cait import CaiT
+        net = CaiT(
+        image_size = size,
+        patch_size = args.patch,
+        num_classes = 10,
+        dim = int(args.dimhead),
+        depth = 6,   # depth of transformer for patch to patch attention only
+        cls_depth=2, # depth of cross attention of CLS tokens to patch
+        heads = 6,
+        mlp_dim = 256,
+        dropout = 0.1,
+        emb_dropout = 0.1,
+        layer_dropout = 0.05
+    )
+    elif args.net=="swin":
+        from models.swin import swin_t
+        net = swin_t(window_size=args.patch,
+                    num_classes=10,
+                    downscaling_factors=(2,2,2,1))
+    return net
+
+def main():
+    args = get_args()
+
+    bs = int(args.bs)
+    imsize = int(args.size)
+
+    # Fixed the error judgment about bool type in the source code
+    use_amp = not args.noamp
+    aug = not args.noaug
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    best_acc = 0  # best test accuracy
+    start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+    # Data
+    print('==> Preparing data..')
+    if args.net=="vit_timm":
+        size = 384
     else:
-        val_loss, acc, best_acc = -1, -1, -1
+        size = imsize
 
-    scheduler.step(epoch-1) # step cosine scheduling
-    
-    list_loss.append(val_loss)
-    list_acc.append(acc)
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.Resize(size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
-    # Write out csv..
-    os.makedirs("log", exist_ok=True)
-    with open(f'log/log_{args.net}_patch{args.patch}.csv', 'w') as f:
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerow(list_loss) 
-        writer.writerow(list_acc) 
-    print(list_loss)
+    transform_test = transforms.Compose([
+        transforms.Resize(size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
 
+    # Add RandAugment with N, M(hyperparameter)
+    if aug:
+        # Remove out-of-specification syntax
+        N = 2
+        M = 14
+        transform_train.transforms.insert(0, RandAugment(N, M))
+
+    # ddp init
+    torch.distributed.init_process_group(backend='nccl',  init_method='tcp://127.0.0.1:23333',
+                            world_size=args.world_size, rank=args.local_rank)
+
+    # Prepare dataset
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    train_sampler = torch.utils.data.distributed.DistributedSampler(trainset)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=False, num_workers=8,
+                                              sampler=train_sampler, persistent_workers=True)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    net = get_net(args)
+
+    # For Multi-GPU
+    if 'cuda' in device:
+        print(device)
+        print("using distributed data parallel")
+        torch.cuda.set_device(args.local_rank)
+
+        # set jit_compile=True avoiding unexpected ERROR
+        torch.npu.set_compile_mode(jit_compile=True)
+
+        net = torch.nn.parallel.DistributedDataParallel(net.cuda(), device_ids=[args.local_rank])
+        cudnn.benchmark = True
+
+    if args.resume:
+        # Load checkpoint.
+        print('==> Resuming from checkpoint..')
+        assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+        checkpoint = torch.load('./checkpoint/{}-ckpt.t7'.format(args.net))
+        net.load_state_dict(checkpoint['net'])
+        best_acc = checkpoint['acc']
+        start_epoch = checkpoint['epoch']
+
+    # Loss is CE
+    criterion = nn.CrossEntropyLoss()
+    criterion_ls = LabelSmoothing()
+
+    if args.opt == "adam":
+        optimizer = optim.Adam(net.parameters(), lr=args.lr)
+    elif args.opt == "sgd":
+        optimizer = optim.SGD(net.parameters(), lr=args.lr)
+
+    # use cosine scheduling
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.n_epochs)
+
+    ##### Training
+    scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
+
+    for epoch in range(start_epoch, args.n_epochs):
+        start = time.time()
+        trainloss = train(epoch, scaler, criterion_ls, trainloader)
+
+        if (epoch + 1) % args.eval_interval == 0 or epoch > int(args.n_epochs * 0.9):
+            val_loss, acc, best_acc = test(epoch, best_acc, criterion, testloader)
+        else:
+            val_loss, acc, best_acc = -1, -1, -1
+
+        scheduler.step(epoch-1) # step cosine scheduling
+
+        print(f"Epoch[{epoch}] epoch_time: {time.time() - start} trainloss: {trainloss} \
+              val_loss: {val_loss} acc: {acc} best_acc: {best_acc}", flush=True)
+
+if __name__ == '__main__':
+    main()
