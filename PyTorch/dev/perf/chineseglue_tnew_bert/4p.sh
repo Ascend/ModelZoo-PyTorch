b814fe6d50f1a1f54584a3e5f8056d6d20b0e1a8
王姜奔 <wangjiangben@huawei.com>
2023-05-04 21:38:43 +0800

    fix format error

--- PyTorch/dev/perf/chineseglue_tnew_bert/4p.sh
+++ PyTorch/dev/perf/chineseglue_tnew_bert/4p.sh
@@ -22,22 +22,22 @@ do
 	run.py \
 	--local_rank $RANK_ID \
 	--fp16 \
-  --model_type=bert \
-  --model_name_or_path=$BERT_BASE_DIR \
-  --task_name=$TASK_NAME \
-  --do_train \
-  --do_eval \
-  --do_lower_case \
-  --data_dir=$GLUE_DIR/${TASK_NAME}/ \
-  --max_seq_length=128 \
-  --per_gpu_train_batch_size=256 \
-  --per_gpu_eval_batch_size=32 \
-  --learning_rate=6e-5 \
-  --num_train_epochs=4.0 \
-  --logging_steps=8372 \
-  --save_steps=8372 \
-  --output_dir=$OUTPUR_DIR/${TASK_NAME}_output/ \
-  --overwrite_output_dir \
+	--model_type=bert \
+	--model_name_or_path=$BERT_BASE_DIR \
+	--task_name=$TASK_NAME \
+	--do_train \
+	--do_eval \
+	--do_lower_case \
+	--data_dir=$GLUE_DIR/${TASK_NAME}/ \
+	--max_seq_length=128 \
+	--per_gpu_train_batch_size=256 \
+	--per_gpu_eval_batch_size=32 \
+	--learning_rate=6e-5 \
+	--num_train_epochs=4.0 \
+	--logging_steps=8372 \
+	--save_steps=8372 \
+	--output_dir=$OUTPUR_DIR/${TASK_NAME}_output/ \
+	--overwrite_output_dir \
 	&
 done
 
