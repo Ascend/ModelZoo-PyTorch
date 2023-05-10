531a1600a9170b38d2c7be7cb8cafb697517f824
孙银磊 <sunyinlei@huawei.com>
2023-05-10 06:47:16 +0000

    !4663 [PyTorch][dev][perf]增加DeepLabV3运行脚本和Readme * 【PyTorch】【dev】【DeepLabV3】DeepLabV3适配脚本上传

---
+++ PyTorch/dev/perf/DeepLabV3_for_PyTorch/README.md
@@ -0,0 +1,171 @@
+# DeepLabV3 for PyTorch
+-   [概述](#概述)
+-   [准备训练环境](#准备训练环境)
+-   [开始训练](#开始训练)
+-   [训练结果展示](#训练结果展示)
+-   [版本说明](#版本说明)
+
+# 概述
+
+DeepLabv3是在DeepLabv1、DeepLabv2的基础上发展而来，DeepLab系列主要围绕空洞卷积、全连接条件随机场（Fully-connected Conditional Random Field (CRF)）以及ASPP展开讨论。DeepLabv3重新考虑了空洞卷积的使用，增加了多尺度分割物体的模块，同时改进了ASPP模块。
+
+- 参考实现：
+
+  ```
+  url=https://github.com/open-mmlab/mmsegmentation.git
+  commit_id=e64548fda0221ad708f5da29dc907e51a644c345
+  ```
+
+- 适配昇腾 AI 处理器的实现：
+
+  ```
+  url=https://gitee.com/ascend/ModelZoo-PyTorch.git
+  code_path=PyTorch/dev/perf/DeepLabV3_for_PyTorch
+  ```
+
+
+# 准备训练环境
+
+## 准备环境
+
+- 当前模型支持的 PyTorch 版本和已知三方库依赖如下表所示。
+
+  **表 1**  版本支持表
+  
+  | Torch_Version |      三方库依赖版本      |
+  | :-----------: | :----------------------: |
+  |  PyTorch 1.5  | torchvision==0.2.2.post3 |
+  |  PyTorch 1.8  |    torchvision==0.9.1    |
+  
+- 环境准备指导。
+
+  请参考《[Pytorch框架训练环境准备](https://www.hiascend.com/document/detail/zh/ModelZoo/pytorchframework/ptes)》。
+  
+- 安装依赖。
+
+  1、使用 NPU 设备源码编译安装 mmcv-full。
+  
+  - 创建MMCV目录，拉取MMCV 源码
+  
+  ```
+  git clone https://github.com/open-mmlab/mmcv.git
+  ```
+  
+  - 编译
+  
+  ```
+  MMCV_WITH_OPS=1 MAX_JOBS=8 FORCE_NPU=1 python3 setup.py build_ext
+  ```
+  
+  - 安装
+  
+  ```
+  MMCV_WITH_OPS=1 FORCE_NPU=1 python3 setup.py develop
+  ```
+
+  2、安装 MMEngine。
+  
+  ```
+  pip3 install mmengine==0.7.3
+  ```
+
+  3、安装 MMSegmentation
+  
+  下载MMSegmentation源码包:
+  ```
+  git clone -b main https://github.com/open-mmlab/mmsegmentation.git
+  ```
+  进入MMSegmentation目录，执行以下语句：
+  ```
+  git checkout e64548fda0221ad708f5da29dc907e51a644c345
+  pip3 install -e .
+  ```
+
+
+
+## 准备数据集
+
+1. 获取数据集。
+
+   请用户自行获取原始数据集**cityscapes**，并在`DeepLabV3_for_PyTorch`目录新建`data`文件夹，将下载好的数据集上传至该目录`DeepLabV3_for_PyTorch/data`。
+
+   数据集目录结构参考如下所示。
+
+   ```
+   $ DeepLabV3_for_PyTorch
+       ├── data
+       │   └── cityscapes
+       │       ├── leftImg8bit
+       │       │   ├── train
+       │       │   ├── val
+       │       │   └── test
+       │       └── gtFine
+       │           ├── train
+       │           ├── val
+       │           └── test
+       └── ... 
+   ```
+   > **说明：** 
+   > 该数据集的训练过程脚本只作为一种参考示例。
+
+
+
+
+# 开始训练
+
+## 训练模型
+
+1. 进入DeepLabV3_for_PyTorch目录。
+
+   ```
+   cd ./DeepLabV3_for_PyTorch
+   ```
+
+2. 运行训练脚本。
+
+   该模型支持单机单卡训练和单机8卡训练。
+
+   - 单机单卡训练
+
+     启动单卡训练。
+
+     ```
+     bash ./test/train_full_1p.sh               # 单卡精度
+          
+     bash ./test/train_performance_1p.sh        # 单卡性能
+     ```
+     
+   - 单机8卡训练
+
+     启动8卡训练。
+   
+     ```
+     bash ./test/train_full_8p.sh               # 8卡精度
+     
+     bash ./test/train_performance_8p.sh        # 8卡性能
+     ```
+   
+   训练完成后，权重文件保存在当前路径下，并输出模型训练精度和性能信息。
+
+
+# 训练结果展示
+
+**表 2**  训练结果展示表
+
+| NAME    | mIoU  | FPS  | iters | AMP_Type | Torch_Version |
+|---------|-------|------|-------|:---------|:--------------|
+| 1p-竞品A  | -     | 13.6 | 40000 | O2       | 1.8           |
+| 8p-竞品A  | 76.71 | 60.7 | 40000 | O2       | 1.8           |
+| 1p-NPU  | -     | 8.6  | 40000 | O2       | 1.8           |
+| 8p-NPU  | 77.61 | 65.7 | 40000 | O2       | 1.8           |
+
+
+# 版本说明
+
+## 变更
+
+2023.05.05：首次发布。
+
+## FAQ
+
+无。
---
+++ PyTorch/dev/perf/DeepLabV3_for_PyTorch/test/train_full_8p.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+source ./test/env_npu.sh
+
+#当前路径,不需要修改
+cur_path=$(pwd)
+cur_path_last_dirname=${cur_path##*/}
+if [ x"${cur_path_last_dirname}" == x"test" ]; then
+  test_path_dir=${cur_path}
+  cd ..
+  cur_path=$(pwd)
+else
+  test_path_dir=${cur_path}/test
+fi
+
+#网络名称,同目录名称,需要模型审视修改
+Network="DeepLabV3_for_PyTorch"
+
+# 指定训练所使用的npu device卡id
+device_id=0
+# 校验是否指定了device_id,分动态分配device_id与手动指定device_id,此处不需要修改
+if [ ${device_id} ]; then
+  export ASCEND_DEVICE_ID=${device_id}
+  echo "device id is ${ASCEND_DEVICE_ID}"
+else
+  "[Error] device id must be config"
+  exit 1
+fi
+
+#创建DeviceID输出目录，不需要修改
+if [ -d $test_path_dir/output/${ASCEND_DEVICE_ID} ]; then
+  rm -rf $test_path_dir/output/$ASCEND_DEVICE_ID
+  mkdir -p $test_path_dir/output/$ASCEND_DEVICE_ID
+else
+  mkdir -p $test_path_dir/output/$ASCEND_DEVICE_ID
+fi
+
+#################启动训练脚本#################
+start_time=$(date +%s)
+nohup bash tools/dist_train.sh \
+  configs/deeplabv3/deeplabv3_r101-d16-mg124_8xb8-40k_cityscapes-512x1024.py \
+  8 >${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
+wait
+
+##################获取训练数据################
+#训练结束时间，不需要修改
+end_time=$(date +%s)
+e2e_time=$(($end_time - $start_time))
+
+#结果打印，不需要修改
+echo "------------------ Final result ------------------"
+#输出性能FPS，需要模型审视修改
+FPS=$(grep "FPS:" ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log | grep "train" | awk -F "FPS:" '{print $2}' | awk 'END {print $1}')
+
+#打印，不需要修改
+echo "Final Performance images/sec : $FPS"
+
+#输出训练精度,需要模型审视修改
+train_accuracy=$(grep "mIoU:" ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log | awk -F "mIoU:" '{print $2}' | awk 'END {print $1}')
+
+#打印，不需要修改
+echo "Final Train Accuracy : ${train_accuracy}"
+echo "E2E Training Duration sec : $e2e_time"
+
+#性能看护结果汇总
+#训练用例信息，不需要修改
+BatchSize=64 #仅用于打印，不在训练中生效
+WORLD_SIZE=1
+DeviceType=$(uname -m)
+CaseName=${Network}_bs${BatchSize}_'p'_'acc'
+
+##获取性能数据，不需要修改
+#吞吐量
+ActualFPS=${FPS}
+#单迭代训练时长
+TrainingTime=$(awk 'BEGIN{printf "%.2f\n", '${BatchSize}'*1000/'${FPS}'}')
+
+#从train_$ASCEND_DEVICE_ID.log提取Loss到train_${CaseName}_loss.txt中，需要根据模型审视
+grep "loss:" ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log | awk -F "loss:" '{print $2}'| awk '{print $1}' >${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${CaseName}_loss.txt
+#最后一个迭代loss值，不需要修改
+ActualLoss=$(awk 'END {print}' ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${CaseName}_loss.txt)
+
+#关键信息打印到${CaseName}.log中，不需要修改
+echo "Network = ${Network}" >${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+echo "RankSize = ${WORLD_SIZE}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+echo "BatchSize = ${BatchSize}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+echo "DeviceType = ${DeviceType}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+echo "CaseName = ${CaseName}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+echo "ActualFPS = ${ActualFPS}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+echo "TrainingTime = ${TrainingTime}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+echo "ActualLoss = ${ActualLoss}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+echo "E2ETrainingTime = ${e2e_time}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
+echo "TrainAccuracy = ${train_accuracy}" >>${test_path_dir}/output/${ASCEND_DEVICE_ID}/${CaseName}.log
