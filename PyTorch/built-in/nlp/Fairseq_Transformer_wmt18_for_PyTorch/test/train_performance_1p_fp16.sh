1349b31184013aa93492cac24432101bbfe37bdc
chensida <chensida@huawei.com>
2023-06-20 06:28:02 +0000

    !4962 [自研][PyTorch][Fairseq Transformer wmt18 for PyTorch]1p脚本添加日志重定向 * [自研][PyTorch][Fairseq Transformer wmt18 for PyTorch]1p脚本添加日志重定向

--- PyTorch/built-in/nlp/Fairseq_Transformer_wmt18_for_PyTorch/test/train_performance_1p_fp16.sh
+++ PyTorch/built-in/nlp/Fairseq_Transformer_wmt18_for_PyTorch/test/train_performance_1p_fp16.sh
@@ -86,24 +86,23 @@ if [ x"${etp_flag}" != x"true" ];then
     source ${test_path_dir}/env_npu.sh
 fi
 
-RANK_ID=0
-
-fairseq-train $data_path \
-            --fp16 --distributed-world-size 1 \
-            --arch transformer_wmt_en_de --share-decoder-input-output-embed \
-            --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
-            --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
-            --dropout 0.3 --weight-decay 0.0001 \
-            --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
-            --max-tokens 4000 \
-            --eval-bleu \
-            --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
-            --eval-bleu-detok moses \
-            --eval-bleu-remove-bpe \
-            --eval-bleu-print-samples \
-            --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
-            --no-save \
-            --max-epoch 1
+nohup fairseq-train $data_path \
+                    --fp16 --distributed-world-size 1 \
+                    --arch transformer_wmt_en_de --share-decoder-input-output-embed \
+                    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+                    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
+                    --dropout 0.3 --weight-decay 0.0001 \
+                    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+                    --max-tokens 4000 \
+                    --eval-bleu \
+                    --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+                    --eval-bleu-detok moses \
+                    --eval-bleu-remove-bpe \
+                    --eval-bleu-print-samples \
+                    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
+                    --no-save \
+                    --max-epoch 1 \
+                    > ${test_path_dir}/output/${ASCEND_DEVICE_ID}/train_${ASCEND_DEVICE_ID}.log 2>&1 &
 
 wait
 #训练结束时间，不需要修改
