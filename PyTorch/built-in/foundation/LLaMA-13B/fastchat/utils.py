fc87e2e0d859778384a07b0efe943118196ad8d8
baoxiang <baoxiang5@huawei.com>
2023-06-26 20:15:50 +0800

    update licenses.

--- PyTorch/built-in/foundation/LLaMA-13B/fastchat/utils.py
+++ PyTorch/built-in/foundation/LLaMA-13B/fastchat/utils.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-# Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
 # Copyright 2023 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
