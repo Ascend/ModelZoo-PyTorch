b5656c3909c6e2c3217ad97c884942a8afce753b
tiansongxue <tiansongxue@huawei.com>
2023-07-04 08:10:11 +0000

    !5113 [torch-aie][resnet50样例]readme修复 * [torch-aie][resnet50]样例readme修复

--- AscendIE/TorchAIE/built-in/cv/classification/ResNet50/run.sh
+++ AscendIE/TorchAIE/built-in/cv/classification/ResNet50/run.sh
@@ -1,6 +1,7 @@
 rm -rf build
 mkdir build
 cd build
+# 请根据实际运行环境修改DTORCH和DTORCH_AIE的路径
 cmake .. -DTORCH=/usr/lib/python3.9.0/lib/python3.9/site-packages/torch -DTORCH_AIE=/usr/lib/python3.9.0/lib/python3.9/site-packages/torch_aie
 make -j 32
 ./sample "../resnet50.ts"
