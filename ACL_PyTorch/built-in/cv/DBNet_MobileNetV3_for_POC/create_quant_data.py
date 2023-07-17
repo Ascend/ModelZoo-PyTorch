651303c3dad032ac3d81318e735e592240d7d589
wwfu <wangwanfu@huawei.com>
2023-07-17 01:53:04 +0000

    add ACL_PyTorch/built-in/cv/DBNet_MobileNetV3_for_POC/create_quant_data.py. first commit
    Signed-off-by: wwfu <wangwanfu@huawei.com>

---
+++ ACL_PyTorch/built-in/cv/DBNet_MobileNetV3_for_POC/create_quant_data.py
@@ -0,0 +1,62 @@
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+from pathlib import Path
+import platform
+import sys
+import numpy as np
+
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(__dir__)
+sys.path.insert(0, os.path.abspath(os.path.join(__dir__, 'PaddleOCR')))
+
+from ppocr.data import build_dataloader
+import tools.program as program
+
+
+def eval(config, logger):
+    save_dir = Path(config['save_dir'])
+    save_dir.mkdir(parents=True, exist_ok=True)
+    batch_size = config['batch_size']
+    valid_dataloader = build_dataloader(config, 'Eval', None, logger)
+    max_iter = len(valid_dataloader)
+    if platform.system() == "Windows":
+        max_iter -= 1
+
+    logger.info('Processing, please wait a moment.')
+    cnt = 0
+    inputs = []
+    for i, batch in enumerate(valid_dataloader):
+        if i >= max_iter:
+            break
+        inputs.append(batch[0].numpy())
+        if len(inputs) < batch_size:
+            continue
+        np.concatenate(inputs, axis=0).tofile(str(save_dir / f'x-{cnt:0>2}.bin'))
+        inputs = []
+        cnt += 1
+    logger.info(f'Done. Quantization data are saved in {save_dir}.')
+
+def main():
+    config, _, logger, _ = program.preprocess()
+    config['Eval']['dataset']['data_dir'] = config['data_dir']
+    label_file = Path(config['data_dir'])/'test_icdar2015_label.txt'
+    config['Eval']['dataset']['label_file_list'] = [str(label_file)]
+    eval(config, logger)
+
+
+if __name__ == '__main__':
+    main()
