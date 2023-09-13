1378f0a1a7d8c6656f91184d0004f76e124589c8
zyz <zhangyanzhe1@huawei.com>
2023-09-13 11:28:13 +0800

    update readme and add inference function

--- PyTorch/built-in/foundation/FlagAI/examples/Aquila/Aquila-pretrain/generate.py
+++ PyTorch/built-in/foundation/FlagAI/examples/Aquila/Aquila-pretrain/generate.py
@@ -3,7 +3,9 @@ import torch
 from flagai.auto_model.auto_loader import AutoLoader
 from flagai.data.tokenizer import Tokenizer
 from flagai.model.predictor.predictor import Predictor
-import bminf
+
+import torch_npu
+from torch_npu.contrib import transfer_to_npu
 
 state_dict = "./checkpoints_in/"
 model_name = 'aquila-7b'
@@ -11,27 +13,21 @@ model_name = 'aquila-7b'
 loader = AutoLoader("lm",
                     model_dir=state_dict,
                     model_name=model_name,
-                    use_cache=True,
-                    device='cuda',
-                    fp16=True)
-model = loader.get_model()
+                    use_cache=True)
+
+model = loader.get_model().half().npu()
 tokenizer = loader.get_tokenizer()
 
 model.eval()
-model.cuda()
-
-texts = [
-    "汽车EDR是什么",
-]
-
 predictor = Predictor(model, tokenizer)
 
-for text in texts:
-    print('-' * 80)
-    text = f'{text}'
-    print(f"text is {text}")
+while True:
+    user_input = input("please input ")
+    if user_input.lower() == "false":
+        break
+    print("input is: ", user_input)
     with torch.no_grad():
-        out = predictor.predict_generate_randomsample(text,
+        out = predictor.predict_generate_randomsample(user_input,
                                                       out_max_length=200,
                                                       top_p=0.95)
         print(f"pred is {out}")
