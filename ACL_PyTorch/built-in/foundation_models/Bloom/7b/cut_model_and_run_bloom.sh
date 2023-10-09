9d7cb57bcf92c551124f3fdf4b6f3f20d65fa3da
马宁悦 <maningyue@huawei.com>
2023-10-09 08:03:20 +0000

    bugfix:fix script for bloom
    Signed-off-by: 马宁悦 <maningyue@huawei.com>

--- ACL_PyTorch/built-in/foundation_models/Bloom/7b/cut_model_and_run_bloom.sh
+++ ACL_PyTorch/built-in/foundation_models/Bloom/7b/cut_model_and_run_bloom.sh
@@ -10,21 +10,21 @@ cp $UTILS_PATH/utils.py $UTILS_PATH/utils.py.bak
 cp $MODELING_PATH/modeling_bloom.py $MODELING_PATH/modeling_bloom.py.bak
 cp .patches/utils.patch $UTILS_PATH/
 cd $UTILS_PATH
-patch < -p1 utils.patch
+patch -p1 < utils.patch
 cd $CODE_PATH
 
 if [ ! -d "$output_dir" ];
 then
     cp patches/parallel.patch $MODELING_PATH/
     cd $MODELING_PATH
-    patch < -p1 parallel.patch
+    patch -p1 < parallel.patch
     cd $CODE_PATH
     echo "Cutted Weight directory does not exist, cuting the weight......"
     python ./cut_model_util.py --input_path $input_dir --output_path $output_dir --world_size $world_size_
 fi 
 cp patches/performance.patch $MODELING_PATH/
 cd $MODELING_PATH
-patch < -p1 performance.patch
+patch -p1 < performance.patch
 cd $CODE_PATH
 echo "Weight directory exists, runing......"
 torchrun --nproc_per_node 2 run_bloom_half_parallel_loadPartModel.py --load_path $output_dir
