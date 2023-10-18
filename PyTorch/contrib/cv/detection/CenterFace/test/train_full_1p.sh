e1acbabf497f11e7e5e8962fd1dd427a7963f1af
libin <1581602217@qq.com>
2023-10-18 08:39:32 +0000

    update PyTorch/contrib/cv/detection/CenterFace/test/train_full_1p.sh.
    Signed-off-by: libin <1581602217@qq.com>

--- PyTorch/contrib/cv/detection/CenterFace/test/train_full_1p.sh
+++ PyTorch/contrib/cv/detection/CenterFace/test/train_full_1p.sh
@@ -12,7 +12,8 @@ Network="CenterFace_ID4089_for_PyTorch"
 
 #训练batch_size,,需要模型审视修改
 batch_size=32
-
+#关闭INF/NAN
+export INF_NAN_MODE_ENABLE=0
 # 指定训练所使用的npu device卡id
 device_id=0
 # 参数校验，data_path为必传参数，其他参数的增删由模型自身决定；此处新增参数需在上面有定义并赋值
