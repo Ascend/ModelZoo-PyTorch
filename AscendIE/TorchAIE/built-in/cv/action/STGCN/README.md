8b658e989a15d45cad50c70487b2d44ca1eb92c9
Zheng Yong <zhengyong22@huawei.com>
2023-11-20 17:04:57 +0800

    torch_aie support of sg-gcn

---
+++ AscendIE/TorchAIE/built-in/cv/action/STGCN/README.md
@@ -0,0 +1,133 @@
+# STGCN(ST-GCN) 模型推理指导
+
+- [概述](#概述)
+    - [输入输出数据](#输入输出数据)
+- [推理环境](#推理环境)
+- [快速上手](#快速上手)
+    - [获取源码](#获取源码)
+    - [准备数据集](#准备数据集)
+    - [推理验证](#推理验证)
+- [性能&精度](#性能精度)
+
+----
+# 概述
+
+ST-GCN是一种图卷积神经网络，该模型可以实现对人体骨架图做姿态估计，从而实现行为识别的效果。该模型利用了人体骨骼的局部模式和相关特征信息。本项目利用昇腾推理引擎`AscendIE`和框架推理插件`TorchAIE`，基于`pytorch框架`实现该模型在昇腾设备上的高性能推理。
+
++ 论文  
+    Spatial Temporal Graph Convolutional Networks for Skeleton-Based Action Recognition
+
++ 参考实现  
+    ```
+    url = https://github.com/open-mmlab/mmskeleton/blob/master/mmskeleton/models/backbones/st_gcn_aaai18.py
+    branch = master
+    commit_id = b4c076baa9e02e69b5876c49fa7c509866d902c7
+    model_name = ST_GCN_18
+    ```
+## 输入输出数据
++ 模型输入  
+    | input-name | data-type | data-format |input-shape |
+    | ---------- | --------- | ----------- | ---------- |
+    | input1  |  FLOAT32  | ND         | batch_size x 3 x 300 x 18 x 2   |
+
++ 模型输出  
+    | output-name |  data-type | data-format |output-shape |
+    | ----------- | ---------- | ----------- | ----------- |
+    | output1       |  FLOAT32   | ND          | batch_size x 400       |
+
+
+----
+# 推理环境准备<a name="ZH-CN_TOPIC_0000001126281702"></a>
+
+- 该模型需要以下插件与驱动  
+
+  **表 1**  版本配套表
+
+  | 配套  | 版本  | 环境准备指导  |
+  |---------| ------- | ------------------------------------------------------------ |
+  | 固件与驱动 | 23.0.rc1  | [Pytorch框架推理环境准备](https://www.hiascend.com/document/detail/zh/ModelZoo/pytorchframework/pies) |
+  | CANN | 7.0.RC1.alpha003 | - |
+  | Python | 3.9.11 | - |
+  | PyTorch | 2.0.1 | - |
+  | Torch_AIE | 6.3.rc2 | - |
+
+- 安装依赖
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+----
+# 快速上手
+
+## 获取源码
+
+1. 模型导出依赖st-gcn和mmskeleton仓库。
+    ```bash
+    git clone https://github.com/yysijie/st-gcn.git
+    git clone https://github.com/open-mmlab/mmskeleton.git
+    git clone https://gitee.com/ascend/ModelZoo-PyTorch.git
+    cp ModelZoo-PyTorch/ACL_PyTorch/built-in/cv/STGCN_for_Pytorch/stgcn_postprocess.py .
+    ```
+
+
+## 准备数据集
+
+1. 获取原始数据集  
+    参考[STGCN-Pytorch在昇腾设备上离线推理指导](https://gitee.com/ascend/ModelZoo-PyTorch/tree/master/ACL_PyTorch/built-in/cv/STGCN_for_Pytorch)预处理数据，得到如下目录结构：
+    ```
+    data/
+    |—— Kinetics/
+        |—— kinetics-skeleton
+            |—— val_label.pkl
+            |—— ...
+    |—— kinetics-skeleton
+            |—— val_data
+                |—— 0.bin
+                |—— 1.bin
+                ...
+            |—— val_label
+                |—— 0.bin
+                |—— 1.bin
+                ...
+    ```
+
+## 推理验证
+
+1. 执行推理。
+
+    ```
+    python inference.py
+    ```
+    执行推理，并将计算结果保存在./result文件夹中。同时，推理结束后打印出性能数据。
+
+
+2. 精度验证。
+
+    调用脚本stgcn_postprocess.py，可以获得Accuracy数据。
+
+    ```
+    python stgcn_postprocess.py \
+        --result_dir ./result \
+        --label_path ./data/Kinetics/kinetics-skeleton/val_label.pkl
+    ```
+
+    - 参数说明：
+
+    - result_dir：推理结果保存地址
+
+
+----
+# 性能&精度
+
+在310P设备上，OM模型的精度与目标精的相对误差低于 1%，batchsize为1时模型性能最优，达 381.10 fps。
+
+| 芯片型号   | BatchSize | 数据集      | 精度            | 性能       |
+| --------- |---------- | ----------- | --------------- | --------- |
+|Ascend310P3| 1         | kinetics-skeleton | Top1@Acc=31.61%   Top5@Acc: 53.74% | 4416 fps |
+|Ascend310P3| 4         | kinetics-skeleton | Top1@Acc=31.61%   Top5@Acc: 53.74% | 13669 fps |
+|Ascend310P3| 8         | kinetics-skeleton | Top1@Acc=31.61%   Top5@Acc: 53.74% | 26010 fps |
+|Ascend310P3| 16        | kinetics-skeleton | Top1@Acc=31.61%   Top5@Acc: 53.74% | 33125 fps |
+|Ascend310P3| 32        | kinetics-skeleton | Top1@Acc=31.61%   Top5@Acc: 53.74% | 93595 fps |
+|Ascend310P3| 64        | kinetics-skeleton | Top1@Acc=31.61%   Top5@Acc: 53.74% | 120215 fps |
+
---
+++ AscendIE/TorchAIE/built-in/cv/action/STGCN/inference.py
@@ -0,0 +1,154 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import copy
+import numpy as np
+import os
+import time
+from tqdm import tqdm
+import sys
+import torch
+import torch_npu
+import torch_aie
+
+sys.path.append("./st-gcn")
+from net.st_gcn import Model
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="inference")
+    parser.add_argument("--input_path", default="data/kinetics-skeleton/val_data")
+    parser.add_argument("--output_path", default="result")
+    parser.add_argument("--save_output", type=bool, default=True)
+    parser.add_argument(
+        "--input_shape", nargs="+", type=int, default=[1, 3, 300, 18, 2]
+    )
+    parser.add_argument("--device", default="npu:0")
+    parser.add_argument("--warmup_num", type=int, default=5)
+    parser.add_argument("--in_channels", type=int, default=3)
+    parser.add_argument("--num_class", type=int, default=400)
+    parser.add_argument(
+        "--ckpt_path",
+        default="mmskeleton/checkpoints/st_gcn.kinetics-6fa43f73.pth",
+    )
+    args = parser.parse_args()
+    return args
+
+
+def compile_model(args):
+    model = Model(
+        args.in_channels,
+        args.num_class,
+        {"layout": "openpose", "strategy": "spatial"},
+        True,
+    )
+    model.eval()
+    state_dict = torch.load(args.ckpt_path)
+    model.load_state_dict(state_dict, strict=True)
+
+    net_input = torch.randn(args.input_shape)
+    ts_model = torch.jit.trace(model.eval(), net_input)
+
+    compile_input = [torch_aie.Input(args.input_shape, dtype=torch.float)]
+    torch_aie_model = torch_aie.compile(
+        ts_model,
+        inputs=compile_input,
+        precision_policy=torch_aie.PrecisionPolicy.FP16,
+    )
+    return torch_aie_model
+
+
+def get_input_info(args):
+    batch = args.input_shape[0]
+    file_path_list = []
+    file_name_list = []
+    for dir_path, _, file_names in os.walk(args.input_path):
+        for file_name in file_names:
+            file_path_list.append(os.path.join(dir_path, file_name))
+            file_name_list.append(file_name)
+
+    file_nums = len(file_path_list)
+    iters = (file_nums + batch - 1) // batch
+    pad = iters * batch - file_nums
+    return file_path_list, file_name_list, iters, pad
+
+
+def create_data_generator(args, file_path_list, file_name_list, iters, pad):
+    batch = args.input_shape[0]
+    file_nums = len(file_path_list)
+
+    for i in range(iters):
+        batch_data = []
+        batch_names = []
+        for j in range(batch):
+            idx = batch * i + j
+            if idx == file_nums:
+                break
+            batch_data.append(
+                torch.from_numpy(np.fromfile(file_path_list[idx], np.float32)).view(
+                    [1, *args.input_shape[1:]]
+                )
+            )
+            batch_names.append(file_name_list[idx])
+            idx += 1
+        if (i == (iters - 1)) and (pad > 0):
+            for _ in range(pad):
+                batch_data.append(copy.deepcopy(batch_data[-1]))
+                batch_names.append(file_name_list[-1])
+        yield torch.cat(batch_data).to(args.device), batch_names
+
+
+def inference(args, torch_aie_model):
+    output_path = args.output_path
+    batch = args.input_shape[0]
+    if args.save_output and (not os.path.exists(output_path)):
+        os.makedirs(output_path, mode=640)
+
+    file_path_list, file_name_list, iters, pad = get_input_info(args)
+    g = create_data_generator(args, file_path_list, file_name_list, iters, pad)
+
+    cumulated_time = 0
+    counted_num = 0
+    for x, x_names in tqdm(g, total=iters):
+        counted_num += 1
+        stream = torch_aie.npu.Stream(args.device)
+        with torch_aie.npu.stream(stream):
+            stream.synchronize()
+            ts = time.time()
+            ret = torch_aie_model.forward(x)
+            stream.synchronize()
+            duration = time.time() - ts
+            if counted_num > args.warmup_num:
+                cumulated_time += duration
+
+        ret = ret.detach().to("cpu").numpy()
+        if args.save_output:
+            for i in range(batch):
+                ret[i, ...].tofile(os.path.join(output_path, x_names[i].replace(".bin", "_0.bin")))
+
+    print(
+        f"Pure inference performance: {batch * (counted_num - args.warmup_num) / cumulated_time} FPS"
+    )
+
+
+def main():
+    args = parse_args()
+    torch_npu.npu.set_device(args.device)
+    torch_aie_model = compile_model(args)
+    inference(args, torch_aie_model)
+
+
+if __name__ == "__main__":
+    main()
